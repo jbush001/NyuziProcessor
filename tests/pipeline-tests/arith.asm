86b164269073794b0f5f7ca004226023d39f8954
Jeff Bush <jeffbush001@gmail.com>
2011-10-31 22:58:39 -0700

    Rearrange test dir

--- tests/pipeline-tests/arith.asm
+++
@@ -1,5 +0,0 @@
-_start			s1 = s1 + 0x13
-				s2 = s2 + 0x27
-				s3 = s1 + s2
-						
-done			goto done
--- tests/pipeline-tests/branch-test.asm
+++
@@ -1,8 +0,0 @@
-_start				
-loop0				s0 = s0 + 1
-					s1 = s0 < 10
-					btrue s1, loop0
-					nop
-					nop
-					nop
-done				goto	done
