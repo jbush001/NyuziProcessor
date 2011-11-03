cb3501549d68baf560f8535d0405942e7f82dc1f
Jeff Bush <jeffbush001@gmail.com>
2011-11-02 22:37:25 -0700

    minor pedantism

--- tests/hardware-test/mixed.py
+++ tests/hardware-test/mixed.py
@@ -1,3 +1,3 @@
 from runcase import *
 
-runTestWithFile({}, 'bubble-sort.asm', None, 4, [1, 2, 3, 4, 5, 6, 7, 8], 500)
+runTestWithFile({}, 'selection-sort.asm', None, 4, [1, 2, 3, 4, 5, 6, 7, 8], 500)
