529d5d99c46d7efb73984c5906edad52336ad011
Jeff Bush <jeffbush001@gmail.com>
2012-04-26 22:47:09 -0700

    Fix combinational loop

--- verilog/rtl/l2_cache_arb.v
+++ verilog/rtl/l2_cache_arb.v
@@ -8,7 +8,7 @@ module l2_cache_arb(
 	input						clk,
 	input						stall_pipeline,
 	input						pci_valid,
-	output 						pci_ack,
+	output reg					pci_ack,
 	input [1:0]					pci_unit,
 	input [1:0]					pci_strand,
 	input [2:0]					pci_op,
@@ -38,36 +38,42 @@ module l2_cache_arb(
 	output reg[511:0]			arb_sm_data = 0,
 	output reg[1:0]				arb_sm_fill_way = 0);
 
-	assign pci_ack = pci_valid && !stall_pipeline && !smi_data_ready;	
 
 	always @(posedge clk)
 	begin
-		if (smi_data_ready)	
+		if (!stall_pipeline)
 		begin
-			arb_pci_valid <= #1 1'b1;
-			arb_pci_unit <= #1 smi_pci_unit;
-			arb_pci_strand <= #1 smi_pci_strand;
-			arb_pci_op <= #1 smi_pci_op;
-			arb_pci_way <= #1 smi_pci_way;
-			arb_pci_address <= #1 smi_pci_address;
-			arb_pci_data <= #1 smi_pci_data;
-			arb_pci_mask <= #1 smi_pci_mask;
-			arb_has_sm_data <= #1 1'b1;
-			arb_sm_data <= #1 smi_load_buffer_vec;
-			arb_sm_fill_way <= #1 smi_fill_way;
+			if (smi_data_ready)	
+			begin
+				pci_ack <= #1 0;
+				arb_pci_valid <= #1 1'b1;
+				arb_pci_unit <= #1 smi_pci_unit;
+				arb_pci_strand <= #1 smi_pci_strand;
+				arb_pci_op <= #1 smi_pci_op;
+				arb_pci_way <= #1 smi_pci_way;
+				arb_pci_address <= #1 smi_pci_address;
+				arb_pci_data <= #1 smi_pci_data;
+				arb_pci_mask <= #1 smi_pci_mask;
+				arb_has_sm_data <= #1 1'b1;
+				arb_sm_data <= #1 smi_load_buffer_vec;
+				arb_sm_fill_way <= #1 smi_fill_way;
+			end
+			else
+			begin
+				pci_ack <= #1 pci_valid;	
+				arb_pci_valid <= #1 pci_valid;
+				arb_pci_unit <= #1 pci_unit;
+				arb_pci_strand <= #1 pci_strand;
+				arb_pci_op <= #1 pci_op;
+				arb_pci_way <= #1 pci_way;
+				arb_pci_address <= #1 pci_address;
+				arb_pci_data <= #1 pci_data;
+				arb_pci_mask <= #1 pci_mask;
+				arb_has_sm_data <= #1 0;
+				arb_sm_data <= #1 0;
+			end
 		end
 		else
-		begin
-			arb_pci_valid <= #1 pci_valid;
-			arb_pci_unit <= #1 pci_unit;
-			arb_pci_strand <= #1 pci_strand;
-			arb_pci_op <= #1 pci_op;
-			arb_pci_way <= #1 pci_way;
-			arb_pci_address <= #1 pci_address;
-			arb_pci_data <= #1 pci_data;
-			arb_pci_mask <= #1 pci_mask;
-			arb_has_sm_data <= #1 0;
-			arb_sm_data <= #1 0;
-		end
+			pci_ack <= #1 0;
 	end
 endmodule
