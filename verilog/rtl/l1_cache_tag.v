e7350396f87ea60c2afc3131dc492a9ee8bb4420
Jeff Bush <jeffbush001@gmail.com>
2012-06-16 12:04:53 -0700

    Use an explicit SRAM module for tag/valid memory.

--- verilog/rtl/l1_cache_tag.v
+++ verilog/rtl/l1_cache_tag.v
@@ -18,60 +18,89 @@ module l1_cache_tag
 	input							update_i,
 	input							invalidate_i,
 	input[1:0]						update_way_i,
-	input[`L1_TAG_WIDTH - 1:0]			update_tag_i,
-	input[`L1_SET_INDEX_WIDTH - 1:0]	update_set_i);
+	input[`L1_TAG_WIDTH - 1:0]		update_tag_i,
+	input[`L1_SET_INDEX_WIDTH - 1:0] update_set_i);
 
-	localparam					NUM_WAYS = 4;
-	localparam					WAY_INDEX_WIDTH = 2;
-
-	reg[`L1_TAG_WIDTH - 1:0]	tag_mem0[0:`L1_NUM_SETS - 1];
-	reg							valid_mem0[0:`L1_NUM_SETS - 1];
-	reg[`L1_TAG_WIDTH - 1:0]	tag_mem1[0:`L1_NUM_SETS - 1];
-	reg							valid_mem1[0:`L1_NUM_SETS - 1];
-	reg[`L1_TAG_WIDTH - 1:0]	tag_mem2[0:`L1_NUM_SETS - 1];
-	reg							valid_mem2[0:`L1_NUM_SETS - 1];
-	reg[`L1_TAG_WIDTH - 1:0]	tag_mem3[0:`L1_NUM_SETS - 1];
-	reg							valid_mem3[0:`L1_NUM_SETS - 1];
-	reg[`L1_TAG_WIDTH - 1:0]	tag0 = 0;
-	reg[`L1_TAG_WIDTH - 1:0]	tag1 = 0;
-	reg[`L1_TAG_WIDTH - 1:0]	tag2 = 0;
-	reg[`L1_TAG_WIDTH - 1:0]	tag3 = 0;
-	reg							valid0 = 0;
-	reg							valid1 = 0;
-	reg							valid2 = 0;
-	reg							valid3 = 0;
-	reg							access_latched = 0;
-	reg[`L1_TAG_WIDTH - 1:0]	request_tag_latched = 0;
-	integer						i;
-
-	initial
-	begin
-		for (i = 0; i < `L1_NUM_SETS; i = i + 1)
-		begin
-			tag_mem0[i] = 0;
-			tag_mem1[i] = 0;
-			tag_mem2[i] = 0;
-			tag_mem3[i] = 0;
-			valid_mem0[i] = 0;
-			valid_mem1[i] = 0;
-			valid_mem2[i] = 0;
-			valid_mem3[i] = 0;
-		end	
-	end
+	wire[`L1_TAG_WIDTH - 1:0]		tag0;
+	wire[`L1_TAG_WIDTH - 1:0]		tag1;
+	wire[`L1_TAG_WIDTH - 1:0]		tag2;
+	wire[`L1_TAG_WIDTH - 1:0]		tag3;
+	wire							valid0;
+	wire							valid1;
+	wire							valid2;
+	wire							valid3;
+	reg								access_latched = 0;
+	reg[`L1_TAG_WIDTH - 1:0]		request_tag_latched = 0;
 
 	wire[`L1_SET_INDEX_WIDTH - 1:0]	requested_set_index = address_i[10:6];
-	wire[`L1_TAG_WIDTH - 1:0] requested_tag = address_i[31:11];
+	wire[`L1_TAG_WIDTH - 1:0] 		requested_tag = address_i[31:11];
+
+	sram_1r1w #(`L1_TAG_WIDTH, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) tag_mem0(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(tag0),
+		.wr_addr(update_set_i),
+		.wr_data(update_tag_i),
+		.wr_enable(update_i && update_way_i == 0));
+
+	sram_1r1w #(`L1_TAG_WIDTH, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) tag_mem1(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(tag1),
+		.wr_addr(update_set_i),
+		.wr_data(update_tag_i),
+		.wr_enable(update_i && update_way_i == 1));
+
+	sram_1r1w #(`L1_TAG_WIDTH, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) tag_mem2(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(tag2),
+		.wr_addr(update_set_i),
+		.wr_data(update_tag_i),
+		.wr_enable(update_i && update_way_i == 2));
+
+	sram_1r1w #(`L1_TAG_WIDTH, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) tag_mem3(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(tag3),
+		.wr_addr(update_set_i),
+		.wr_data(update_tag_i),
+		.wr_enable(update_i && update_way_i == 3));
+
+	sram_1r1w #(1, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) valid_mem0(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(valid0),
+		.wr_addr(update_set_i),
+		.wr_data(update_i),
+		.wr_enable((invalidate_i || update_i) && update_way_i == 0));
+
+	sram_1r1w #(1, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) valid_mem1(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(valid1),
+		.wr_addr(update_set_i),
+		.wr_data(update_i),
+		.wr_enable((invalidate_i || update_i) && update_way_i == 1));
+
+	sram_1r1w #(1, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) valid_mem2(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(valid2),
+		.wr_addr(update_set_i),
+		.wr_data(update_i),
+		.wr_enable((invalidate_i || update_i) && update_way_i == 2));
+
+	sram_1r1w #(1, `L1_NUM_SETS, `L1_SET_INDEX_WIDTH, 1) valid_mem3(
+		.clk(clk),
+		.rd_addr(requested_set_index),
+		.rd_data(valid3),
+		.wr_addr(update_set_i),
+		.wr_data(update_i),
+		.wr_enable((invalidate_i || update_i) && update_way_i == 3));
 
 	always @(posedge clk)
 	begin
-		tag0 				<= #1 tag_mem0[requested_set_index];
-		valid0 				<= #1 valid_mem0[requested_set_index];
-		tag1 				<= #1 tag_mem1[requested_set_index];
-		valid1 				<= #1 valid_mem1[requested_set_index];
-		tag2 				<= #1 tag_mem2[requested_set_index];
-		valid2 				<= #1 valid_mem2[requested_set_index];
-		tag3 				<= #1 tag_mem3[requested_set_index];
-		valid3 				<= #1 valid_mem3[requested_set_index];
 		access_latched 		<= #1 access_i;
 		request_tag_latched	<= #1 requested_tag;
 	end
@@ -83,61 +112,18 @@ module l1_cache_tag
 
 	always @*
 	begin
-		if (hit0)
-			hit_way_o = 0;
-		else if (hit1)
-			hit_way_o = 1;
-		else if (hit2)
-			hit_way_o = 2;
-		else
-			hit_way_o = 3;
+		case ({hit0, hit1, hit2, hit3})
+			4'b1000: hit_way_o = 0;
+			4'b0100: hit_way_o = 1;
+			4'b0010: hit_way_o = 2;
+			4'b0001: hit_way_o = 3;
+			4'b0000: hit_way_o = 0;
+			default: hit_way_o = 2'hx;	// This will assert in simulation
+		endcase
 	end
 
 	assign cache_hit_o = (hit0 || hit1 || hit2 || hit3) && access_latched;
 
-	always @(posedge clk)
-	begin
-		if (update_i)
-		begin
-			// When we finish loading a line, we mark it as valid and
-			// update tag RAM
-			case (update_way_i)
-				0:
-				begin
-					valid_mem0[update_set_i] <= #1 1;
-					tag_mem0[update_set_i] <= #1 update_tag_i;
-				end
-				
-				1: 
-				begin
-					valid_mem1[update_set_i] <= #1 1; 
-					tag_mem1[update_set_i] <= #1 update_tag_i;
-				end
-				
-				2:
-				begin
-					valid_mem2[update_set_i] <= #1 1;
-					tag_mem2[update_set_i] <= #1 update_tag_i;
-				end
-
-				3:
-				begin
-					valid_mem3[update_set_i] <= #1 1;
-					tag_mem3[update_set_i] <= #1 update_tag_i;
-				end
-			endcase
-		end
-		else if (invalidate_i)
-		begin
-			case (update_way_i)
-				0: valid_mem0[update_set_i] <= #1 0;
-				1: valid_mem1[update_set_i] <= #1 0; 
-				2: valid_mem2[update_set_i] <= #1 0;
-				3: valid_mem3[update_set_i] <= #1 0;
-			endcase
-		end
-	end
-
 	assertion #("l1_cache_tag: more than one way was a hit") a(.clk(clk), 
-		.test(hit0 + hit1 + hit2 + hit3 > 1));
+		.test(access_latched && (hit0 + hit1 + hit2 + hit3 > 1)));
 endmodule
---
+++ verilog/rtl/sram_1r1w.v
@@ -0,0 +1,38 @@
+//
+// Block SRAM with 1 read port and 1 write port
+//
+
+module sram_1r1w
+	#(parameter WIDTH = 32,
+	parameter SIZE = 1024,
+	parameter ADDR_WIDTH = 10,
+	parameter READ_BEFORE_WRITE = 0)	// If true, return old data when there is a
+										// simultaneous read and write to the same
+										// location
+	(input						clk,
+	input [ADDR_WIDTH - 1:0]	rd_addr,
+	output reg[WIDTH - 1:0]		rd_data = 0,
+	input [ADDR_WIDTH - 1:0]	wr_addr,
+	input [WIDTH - 1:0]			wr_data,
+	input						wr_enable);
+
+	reg[WIDTH - 1:0]			data[0:SIZE - 1];
+	integer						i;
+
+	initial
+	begin
+		for (i = 0; i < SIZE; i = i + 1)
+			data[i] = 0;
+	end
+
+	always @(posedge clk)
+	begin
+		if (wr_enable)
+			data[wr_addr] <= #1 wr_data;	
+			
+		if (!READ_BEFORE_WRITE && wr_enable && wr_addr == rd_addr)
+			rd_data <= #1 wr_data;
+		else
+			rd_data <= #1 data[rd_addr];
+	end
+endmodule
