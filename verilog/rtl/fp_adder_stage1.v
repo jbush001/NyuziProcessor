1cecfc805b12adf3c93e754ae087da3566377a8b
Jeff Bush <jeffbush001@gmail.com>
2012-08-17 21:49:27 -0700

    Add license boilerplate

--- verilog/rtl/fp_adder_stage1.v
+++ verilog/rtl/fp_adder_stage1.v
@@ -1,4 +1,20 @@
 // 
+// Copyright 2011-2012 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+// 
 // Stage 1 of the floating point addition pipeline
 // - Compute the amount to shift the exponents to align the significands
 // - Swap significands if needed so the smaller one is in the second slot
--- verilog/rtl/pci_arbiter_mux.v
+++ verilog/rtl/pci_arbiter_mux.v
@@ -1,3 +1,19 @@
+// 
+// Copyright 2011-2012 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
 //
 // Arbitrates for the PCI interface between store buffer, L1 instruction cache,
 // and L1 data cache and muxes control signals.  This currently uses a very simple fixed 
