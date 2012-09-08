92cd1bf5d722a794b93814976be88231d3195016
Jeff Bush <jeffbush001@gmail.com>
2012-09-08 09:33:46 -0700

    If multiple hex filenames are passed, run them all in sequence

--- tests/randomizer/runtest.py
+++ tests/randomizer/runtest.py
@@ -103,6 +103,84 @@ class CEmulatorWrapper:
 
 		return parseRegisterTraces(output.split('\n'))
 
+SIMULATOR_MEM_DUMP = 'WORK/sim-memory.bin'
+REFERENCE_MEM_DUMP = 'WORK/reference-memory.bin'
+
+def runTest(hexFilename):
+	print 'running test', hexFilename,
+
+	reference = CEmulatorWrapper()
+	referencetraces = reference.runTest(hexFilename, REFERENCE_MEM_DUMP)
+	
+	sim = VerilogSimulatorWrapper()
+	simtraces = sim.runTest(hexFilename, SIMULATOR_MEM_DUMP)
+	
+	try:
+		for strandid, (referencestrand, simstrand) in enumerate(zip(referencetraces, simtraces)):
+			for referencetransfer, simtransfer in zip(referencestrand, simstrand):
+				try:
+					if len(simtransfer) != len(referencetransfer):
+						raise Exception()
+		
+					if len(referencetransfer) == 4:
+						# Vector transfer
+						mpc, mreg, mmask, mvalue = referencetransfer
+						spc, sreg, smask, svalue = simtransfer
+						if mpc != spc or mreg != sreg or mmask != smask:
+							raise Exception()
+						
+						for lane in range(16):
+							# Check each lane individually
+							if (mmask & (1 << (15 - lane))) != 0:
+								if mvalue[lane] != svalue[lane]:
+									print 'mismatch lane', lane
+									raise Exception()
+					else:
+						# Scalar transfer
+						if referencetransfer != simtransfer:
+							raise Exception()
+				except:
+					print 'mismatch, strand', strandid
+					print '  reference @', hex(referencetransfer[0]), referencetransfer[1:]
+					print '  simulation @', hex(simtransfer[0]), simtransfer[1:]
+					raise
+	
+			# If there are left over events, bail now
+			if len(referencestrand) != len(simstrand):
+				print 'number of events does not match, strand ', strandid, len(referencestrand), len(simstrand)
+				raise Exception()
+	
+	
+		f1 = open(REFERENCE_MEM_DUMP)
+		f2 = open(SIMULATOR_MEM_DUMP)
+		
+		offset = 0
+		while True:
+			b1 = f1.read(1)
+			b2 = f2.read(1)
+			if b1 == '':
+				if b2 != '':
+					print 'length mismatch'
+					raise Exception()
+		
+				break
+				
+			if b1 != b2:
+				print 'mismatch @', hex(offset), 'reference', ord(b1), 'sim', ord(b2)
+				raise Exception()
+				
+			offset += 1
+		
+		f1.close()
+		f2.close()
+	
+		print 'PASS'
+	except:
+		print 'FAIL'
+
+if 'SHOWREGS' in os.environ:
+	showRegs = True
+
 # A, B, C, D, (e is remainder)
 profiles = [
 	[ 0, 0, 100, 0 ],		# Only memory accesses
@@ -112,92 +190,19 @@ profiles = [
 ]
 
 if len(sys.argv) > 1:
-	# Run on an existing file
-	hexFilename = sys.argv[1]
+	# Run on an existing file(s)
+	for hexFilename in sys.argv[1:]:
+		runTest(hexFilename)
 else:
 	# Generate a new random test file
 	hexFilename = 'WORK/test.hex'
 	profileIndex = random.randint(0, 3)
 	print 'using profile', profileIndex
 	Generator(profiles[profileIndex]).generate(hexFilename)
-
-if 'SHOWREGS' in os.environ:
-	showRegs = True
-
-SIMULATOR_MEM_DUMP = 'WORK/sim-memory.bin'
-REFERENCE_MEM_DUMP = 'WORK/reference-memory.bin'
-
-print "generating reference trace"
-reference = CEmulatorWrapper()
-referencetraces = reference.runTest(hexFilename, REFERENCE_MEM_DUMP)
-
-print "running simulation"
-sim = VerilogSimulatorWrapper()
-simtraces = sim.runTest(hexFilename, SIMULATOR_MEM_DUMP)
-
-print "comparing results"
-
-try:
-	for strandid, (referencestrand, simstrand) in enumerate(zip(referencetraces, simtraces)):
-		for referencetransfer, simtransfer in zip(referencestrand, simstrand):
-			try:
-				if len(simtransfer) != len(referencetransfer):
-					raise Exception()
-	
-				if len(referencetransfer) == 4:
-					# Vector transfer
-					mpc, mreg, mmask, mvalue = referencetransfer
-					spc, sreg, smask, svalue = simtransfer
-					if mpc != spc or mreg != sreg or mmask != smask:
-						raise Exception()
-					
-					for lane in range(16):
-						# Check each lane individually
-						if (mmask & (1 << (15 - lane))) != 0:
-							if mvalue[lane] != svalue[lane]:
-								print 'mismatch lane', lane
-								raise Exception()
-				else:
-					# Scalar transfer
-					if referencetransfer != simtransfer:
-						raise Exception()
-			except:
-				print 'mismatch, strand', strandid
-				print '  reference @', hex(referencetransfer[0]), referencetransfer[1:]
-				print '  simulation @', hex(simtransfer[0]), simtransfer[1:]
-				raise
-
-		# If there are left over events, bail now
-		if len(referencestrand) != len(simstrand):
-			print 'number of events does not match, strand ', strandid, len(referencestrand), len(simstrand)
-			raise Exception()
+	runTest(hexFilename)
 
 
-	f1 = open(REFERENCE_MEM_DUMP)
-	f2 = open(SIMULATOR_MEM_DUMP)
-	
-	offset = 0
-	while True:
-		b1 = f1.read(1)
-		b2 = f2.read(1)
-		if b1 == '':
-			if b2 != '':
-				print 'length mismatch'
-				raise Exception()
-	
-			break
-			
-		if b1 != b2:
-			print 'mismatch @', hex(offset), 'reference', ord(b1), 'sim', ord(b2)
-			raise Exception()
-			
-		offset += 1
-	
-	f1.close()
-	f2.close()
 
-	print 'PASS'
-except:
-	pass
+
 
 
