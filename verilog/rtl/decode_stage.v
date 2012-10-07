jbush001/NyuziProcessor

commit 74ad622d9dcae21d787c7d525246c7245b340c5e

Eliminate 'sftoi' instruction in favor of 'ftoi' which takes only one argument.

verilog/rtl/decode_stage.v
@@ -274,15 +274,19 @@ module decode_stage(
 	always @*
 	begin
 		if (is_fmt_a)
-		begin	
-			if (a_opcode[5:4] == 2'b01 || a_opcode[5:2] == 4'b1011)
+		begin
+			if ((a_opcode >= `OP_SIGTR && a_opcode <= `OP_UILTE) 
+				|| (a_opcode >= `OP_FGTR && a_opcode <= `OP_FLTE)
+				|| a_opcode == `OP_GETLANE)
 				writeback_is_vector_nxt = 0;	// compare op or getlane
 			else
 				writeback_is_vector_nxt = a_fmt != `FMTA_S;
 		end
 		else if (is_fmt_b)
 		begin
-			if (b_opcode[4] == 1'b1)
+			if ((b_opcode >= `OP_SIGTR && b_opcode <= `OP_UILTE) 
+				|| (b_opcode >= `OP_FGTR && b_opcode <= `OP_FLTE)
+				|| b_opcode == `OP_GETLANE)
 				writeback_is_vector_nxt = 0;	// compare op or getlane (a bit special)
 			else
 				writeback_is_vector_nxt = b_fmt != `FMTB_S_S;

tests/assembler_test/assembler-test.asm
@@ -45,7 +45,7 @@ sf20 = sf9 + sf11
 sf21 = sf9 - sf11
 sf22 = sf9 * sf11
 sf23 = sf9 / sf11
-si24 = sftoi(sf9, sf10)
+si24 = ftoi(sf9)
 sf25 = sitof(si9, sf10)
 sf26 = floor(sf10)
 sf27 = frac(sf10)
@@ -122,10 +122,10 @@ vi13 = clz(si11)
 ;
 vf21 = vf9 - sf11					; format 1
 vf20 = vf9 + vf11					; format 2
-vi24 = sftoi(vf9, sf10)				; use functional style with vector (2 args)
+vi24 = ftoi(vf9)					; use functional style with vector (1 arg)
 vf26 = floor(vf10)					; single function argument (1 arg)
-vi24{si11} = sftoi(vf9, sf10)		; sftoi is special
-vi24{~si12} = sftoi(vf9, sf10)		; ditto
+vi24{si11} = ftoi(vf9)				; ftoi is special
+vi24{~si12} = ftoi(sf10)			; ditto
 
 ;
 ; Memory accesses

tests/directed_verification/fp_test.py
@@ -234,28 +234,26 @@ class FloatingPointTests(TestGroup):
 			}, None, None, None)
 
 	def test_ftoi():
-		return ({ 'u1' : 12.0, 
-				'u2' : 1.0, 
+		return ({ 'u1' : 12.981, 
 				'u5' : -123.0, 
-				'u7' : 23.0, 
-				'u8' : 4.0 },
+				'u7' : 23.0 },
 			'''
-				u3 = sftoi(f1, f2)	
-				u4 = sftoi(f5, f2)
-				u6 = sftoi(f7, f8)
+				u3 = ftoi(f1)	
+				u4 = ftoi(f5)
+				u6 = ftoi(f7)
 			''',
 			{ 't0u3' : 12,
 			 	't0u4' : -123,
-			 	't0u6' : 92
+			 	't0u6' : 23
 			}, None, None, None)
 			
 	# Result will be zero because of very small exponent.  Make sure
 	# we shift in zeros properly (regression test).
 	def test_ftoi2():
-		return ({ 'u1': -0.9, 'u2' : 32 },
+		return ({ 'u1': 0.00009 },
 		'''
-			u2 = sftoi(f1, f2)
-		''', { 't0u2' : 0xffffffff }, None, None, None)
+			u2 = ftoi(f1)
+		''', { 't0u2' : 0 }, None, None, None)
 			
 	def test_reciprocal():
 		return ({ 'u1' : 12345.0 }, '''

tools/assembler/code_output.c
@@ -199,7 +199,7 @@ int abOpcodeTable[][4] = {
 	{ -1, 19, -1, 45 }, // OP_GREATER_EQUAL
 	{ -1, 20, -1, 46 }, // OP_LESS
 	{ -1, 21, -1, 47 }, // OP_LESS_EQUAL
-	{ -1, -1, -1, -1 },	// OP_SFTOI *unused, we have a conditional to handle this*
+	{ -1, -1, -1, -1 },	// OP_FTOI *unused, we have a conditional to handle this*
 	{ -1, -1, -1, -1 }, // OP_SITOF *unused, we have a conditional to handle this*
 	{ -1, -1, 38, -1 }, // OP_FLOOR
 	{ -1, -1, 39, -1 }, // OP_FRAC
@@ -263,7 +263,7 @@ int bFormatTable[] = {
 // True if instruction translates between integer <-> float
 int isTypeConversion(enum OpType operation)
 {
-	return operation == OP_SFTOI || operation == OP_SITOF;
+	return operation == OP_FTOI || operation == OP_SITOF;
 }
 
 int isCompareOperation(enum OpType operation)
@@ -323,11 +323,11 @@ int emitAInstruction(const struct RegisterInfo *dest,
 		return 0;
 	}
 
-	if (operation == OP_SFTOI)
+	if (operation == OP_FTOI)
 	{
-		// Two float arguments
-		opcode = 48;
-		if (!src1 || src1->type != TYPE_FLOAT || src2->type != TYPE_FLOAT)
+		// One float argument
+		opcode = 27;
+		if (src1 || !src2 || src2->type != TYPE_FLOAT)
 		{
 			printAssembleError(currentSourceFile, lineno, "invalid operand types\n");
 			return 0;

tools/assembler/code_output.h
@@ -43,7 +43,7 @@ enum OpType
 	OP_GREATER_EQUAL,
 	OP_LESS,
 	OP_LESS_EQUAL,
-	OP_SFTOI,
+	OP_FTOI,
 	OP_SITOF,
 	OP_FLOOR,
 	OP_FRAC,

tools/assembler/main.c
@@ -77,7 +77,7 @@ int main(int argc, char *argv[])
 	enterScope();
 	createSymbol("clz", SYM_KEYWORD, OP_CLZ, 1);
 	createSymbol("ctz", SYM_KEYWORD, OP_CTZ, 1);
-	createSymbol("sftoi", SYM_KEYWORD, OP_SFTOI, 1);
+	createSymbol("ftoi", SYM_KEYWORD, OP_SFTOI, 1);
 	createSymbol("sitof", SYM_KEYWORD, OP_SITOF, 1);
 	createSymbol("floor", SYM_KEYWORD, OP_FLOOR, 1);
 	createSymbol("frac", SYM_KEYWORD, OP_FRAC, 1);

tools/disassembler/disassemble.c
@@ -20,7 +20,7 @@
 #include <getopt.h>
 
 #define OP_SHUFFLE 13
-#define OP_SFTOI 48
+#define OP_FTOI 27
 #define OP_SITOF 42
 #define OP_GETLANE 26
 
@@ -58,7 +58,7 @@ struct ABOpInfo
 	{ 1, 2, 0, "<" },	//24
 	{ 1, 2, 0, "<=" },	// 25
 	{ 0, 2, 0, "getlane" }, // 26
-	{ 0, 0, 0, "" }, // 27
+	{ 0, 1, 1, "ftoi" }, // 27
 	{ 0, 0, 0, "" }, // 28
 	{ 0, 0, 0, "" }, // 29
 	{ 0, 0, 0, "" }, // 30
@@ -79,7 +79,7 @@ struct ABOpInfo
 	{ 1, 2, 1, ">=" },	// 45
 	{ 1, 2, 1, "<" },	// 46
 	{ 1, 2, 1, "<=" },	// 47
-	{ 0, 2, 1, "sftoi" },// 48
+	{ 0, 0, 0, "" } // 48
 };
 
 struct AFmtInfo
@@ -127,23 +127,23 @@ void disassembleAOp(unsigned int instr)
 	int opcode = (instr >> 23) & 0x3f;
 	const struct ABOpInfo *opInfo = &abOpcodeTable[opcode];
 	const struct AFmtInfo *fmtInfo = &aFormatTab[(instr >> 20) & 7];
-	char vecSpec;
-	char typeSpec;
-	char optype;
+	char destVectorPrefix;
+	char destFormatPrefix;
+	char operandFormatPrefix;
 	int isCompare = isCompareInstruction(opcode);
 	
 	if (isCompare || opcode == OP_GETLANE)
 	{
-		vecSpec = 's';
-		typeSpec = 'i';	
+		destVectorPrefix = 's';
+		destFormatPrefix = 'i';	
 	}
 	else
 	{
-		vecSpec = fmtInfo->op1IsScalar ? 's' : 'v';
-		typeSpec = opInfo->isFloat && opcode != OP_SFTOI ? 'f' : 'i';
+		destVectorPrefix = fmtInfo->op1IsScalar ? 's' : 'v';
+		destFormatPrefix = opInfo->isFloat && opcode != OP_FTOI ? 'f' : 'i';
 	}
 
-	printf("%c%c%d", vecSpec, typeSpec, (instr >> 5) & 0x1f);
+	printf("%c%c%d", destVectorPrefix, destFormatPrefix, (instr >> 5) & 0x1f);
 
 	if (fmtInfo->masked && !isCompare)
 	{
@@ -167,19 +167,19 @@ void disassembleAOp(unsigned int instr)
 		else
 		{
 			if ((opcode >= 22 && opcode <= 25) || (opcode == 10))
-				optype = 'u';	// special case for unsigned compares and shifts
+				operandFormatPrefix = 'u';	// special case for unsigned compares and shifts
 			else if (opInfo->isFloat)
-				optype = 'f';
+				operandFormatPrefix = 'f';
 			else
-				optype = 'i';
+				operandFormatPrefix = 'i';
 
 			printf("%c%c%d %s %c%c%d\n", 
 				fmtInfo->op1IsScalar ? 's' : 'v',
-				optype,
+				operandFormatPrefix,
 				instr & 0x1f,
 				opInfo->name, 
 				fmtInfo->op2IsScalar ? 's' : 'v',
-				optype,
+				operandFormatPrefix,
 				(instr >> 15) & 0x1f);
 		}
 	}
@@ -215,18 +215,18 @@ void disassembleBOp(unsigned int instr)
 	int opcode = (instr >> 26) & 0x1f;
 	const struct ABOpInfo *opInfo = &abOpcodeTable[opcode];
 	const struct BFmtInfo *fmtInfo = &bFormatTab[(instr >> 23) & 7];
-	char vecSpec;
+	char destVectorPrefix;
 	int immValue;
-	char optype;
+	char operandFormatPrefix;
 	int isCompare = isCompareInstruction(opcode);
 	int isUnsigned = (opcode >= 22 && opcode <= 25) || (opcode == 10);
 
 	if (isCompare || opcode == OP_GETLANE)
-		vecSpec = 's';
+		destVectorPrefix = 's';
 	else
-		vecSpec = fmtInfo->destIsScalar ? 's' : 'v';
+		destVectorPrefix = fmtInfo->destIsScalar ? 's' : 'v';
 	
-	printf("%c%c%d", vecSpec, opcode == OP_SITOF ? 'f' : 'i', (instr >> 5) & 0x1f);
+	printf("%c%c%d", destVectorPrefix, opcode == OP_SITOF ? 'f' : 'i', (instr >> 5) & 0x1f);
 
 	if (fmtInfo->masked && !isCompare)
 	{
@@ -271,15 +271,15 @@ void disassembleBOp(unsigned int instr)
 		else
 		{
 			if (isUnsigned)
-				optype = 'u';	// special case for unsigned compares and shifts
+				operandFormatPrefix = 'u';	// special case for unsigned compares and shifts
 			else if (opInfo->isFloat)
-				optype = 'f';
+				operandFormatPrefix = 'f';
 			else
-				optype = 'i';
+				operandFormatPrefix = 'i';
 
 			printf("%c%c%d %s ", 
 				fmtInfo->op1IsScalar ? 's' : 'v',
-				optype,
+				operandFormatPrefix,
 				instr & 0x1f,
 				opInfo->name);
 				

tools/emulator/core.c
@@ -362,6 +362,7 @@ unsigned int doOp(int operation, unsigned int value1, unsigned int value2)
 		case 23: return value1 >= value2;
 		case 24: return value1 < value2;
 		case 25: return value1 <= value2;
+		case 27: return (int) valueAsFloat(value2); // ftoi
 		case 32: return valueAsInt(valueAsFloat(value1) + valueAsFloat(value2));
 		case 33: return valueAsInt(valueAsFloat(value1) - valueAsFloat(value2));
 		case 34: return valueAsInt(valueAsFloat(value1) * valueAsFloat(value2));
@@ -374,7 +375,6 @@ unsigned int doOp(int operation, unsigned int value1, unsigned int value2)
 		case 45: return valueAsFloat(value1) >= valueAsFloat(value2);
 		case 46: return valueAsFloat(value1) < valueAsFloat(value2);
 		case 47: return valueAsFloat(value1) <= valueAsFloat(value2);
-		case 48: return (int) (valueAsFloat(value1) * valueAsFloat(value2)); // sftoi
 		default: return 0;
 	}
 }

commit 0631d219b115441289602c07dffbd27debd701f1

Commit 74ad622d9dcae21d787c7d525246c7245b340c5e broke vector equal/not equal operators.  It had changed the way vector comparisons were detected, but was not including those opcodes.

verilog/rtl/decode_stage.v
@@ -275,7 +275,7 @@ module decode_stage(
 	begin
 		if (is_fmt_a)
 		begin
-			if ((a_opcode >= `OP_SIGTR && a_opcode <= `OP_UILTE) 
+			if ((a_opcode >= `OP_EQUAL && a_opcode <= `OP_UILTE) 
 				|| (a_opcode >= `OP_FGTR && a_opcode <= `OP_FLTE)
 				|| a_opcode == `OP_GETLANE)
 				writeback_is_vector_nxt = 0;	// compare op or getlane
@@ -284,7 +284,7 @@ module decode_stage(
 		end
 		else if (is_fmt_b)
 		begin
-			if ((b_opcode >= `OP_SIGTR && b_opcode <= `OP_UILTE) 
+			if ((b_opcode >= `OP_EQUAL && b_opcode <= `OP_UILTE) 
 				|| (b_opcode >= `OP_FGTR && b_opcode <= `OP_FLTE)
 				|| b_opcode == `OP_GETLANE)
 				writeback_is_vector_nxt = 0;	// compare op or getlane (a bit special)