jbush001/NyuziProcessor

commit 7ced087b9f45ff008dbac36a998b56c243df7f73

Properly handle exponent overflow/underflow during multiplication. These will now return inf.

rtl/core/multi_cycle_scalar_alu.v
@@ -36,6 +36,25 @@ module multi_cycle_scalar_alu
 	input [TOTAL_WIDTH - 1:0]				operand2,
 	output reg [TOTAL_WIDTH - 1:0]			multi_cycle_result);
 
+	/*AUTOWIRE*/
+	// Beginning of automatic wires (for undeclared instantiated-module outputs)
+	wire [EXPONENT_WIDTH-1:0] add1_exponent1;// From fp_adder_stage1 of fp_adder_stage1.v
+	wire [EXPONENT_WIDTH-1:0] add1_exponent2;// From fp_adder_stage1 of fp_adder_stage1.v
+	wire		add1_exponent2_larger;	// From fp_adder_stage1 of fp_adder_stage1.v
+	wire [5:0]	add1_operand_align_shift;// From fp_adder_stage1 of fp_adder_stage1.v
+	wire [SIGNIFICAND_WIDTH+2:0] add1_significand1;// From fp_adder_stage1 of fp_adder_stage1.v
+	wire [SIGNIFICAND_WIDTH+2:0] add1_significand2;// From fp_adder_stage1 of fp_adder_stage1.v
+	wire [EXPONENT_WIDTH-1:0] add2_exponent;// From add2 of fp_adder_stage2.v
+	wire [SIGNIFICAND_WIDTH+2:0] add2_significand1;// From add2 of fp_adder_stage2.v
+	wire [SIGNIFICAND_WIDTH+2:0] add2_significand2;// From add2 of fp_adder_stage2.v
+	wire [EXPONENT_WIDTH-1:0] add3_exponent;// From add3 of fp_adder_stage3.v
+	wire		add3_sign;		// From add3 of fp_adder_stage3.v
+	wire [SIGNIFICAND_WIDTH+2:0] add3_significand;// From add3 of fp_adder_stage3.v
+	wire [7:0]	mul1_exponent;		// From mul1 of fp_multiplier_stage1.v
+	wire		mul1_sign;		// From mul1 of fp_multiplier_stage1.v
+	wire		mul_overflow_stage2;	// From mul1 of fp_multiplier_stage1.v
+	// End of automatics
+
 	reg[5:0] 								operation2;
 	reg[5:0] 								operation3;
 	reg[5:0] 								operation4;
@@ -58,24 +77,8 @@ module multi_cycle_scalar_alu
 	wire[47:0]								mult_product;
 	wire[31:0]								mul1_muliplicand;
 	wire[31:0]								mul1_multiplier;
-
-	/*AUTOWIRE*/
-	// Beginning of automatic wires (for undeclared instantiated-module outputs)
-	wire [EXPONENT_WIDTH-1:0] add1_exponent1;// From fp_adder_stage1 of fp_adder_stage1.v
-	wire [EXPONENT_WIDTH-1:0] add1_exponent2;// From fp_adder_stage1 of fp_adder_stage1.v
-	wire		add1_exponent2_larger;	// From fp_adder_stage1 of fp_adder_stage1.v
-	wire [5:0]	add1_operand_align_shift;// From fp_adder_stage1 of fp_adder_stage1.v
-	wire [SIGNIFICAND_WIDTH+2:0] add1_significand1;// From fp_adder_stage1 of fp_adder_stage1.v
-	wire [SIGNIFICAND_WIDTH+2:0] add1_significand2;// From fp_adder_stage1 of fp_adder_stage1.v
-	wire [EXPONENT_WIDTH-1:0] add2_exponent;// From add2 of fp_adder_stage2.v
-	wire [SIGNIFICAND_WIDTH+2:0] add2_significand1;// From add2 of fp_adder_stage2.v
-	wire [SIGNIFICAND_WIDTH+2:0] add2_significand2;// From add2 of fp_adder_stage2.v
-	wire [EXPONENT_WIDTH-1:0] add3_exponent;// From add3 of fp_adder_stage3.v
-	wire		add3_sign;		// From add3 of fp_adder_stage3.v
-	wire [SIGNIFICAND_WIDTH+2:0] add3_significand;// From add3 of fp_adder_stage3.v
-	wire [EXPONENT_WIDTH-1:0] mul1_exponent;// From mul1 of fp_multiplier_stage1.v
-	wire		mul1_sign;		// From mul1 of fp_multiplier_stage1.v
-	// End of automatics
+	reg										mul_overflow_stage3;
+	reg										mul_overflow_stage4;
 
 	// Check for inf/nan
 	wire op1_is_special = operand1[30:23] == {EXPONENT_WIDTH{1'b1}};
@@ -181,8 +184,9 @@ module multi_cycle_scalar_alu
 				  // Outputs
 				  .mul1_muliplicand	(mul1_muliplicand[31:0]),
 				  .mul1_multiplier	(mul1_multiplier[31:0]),
-				  .mul1_exponent	(mul1_exponent[EXPONENT_WIDTH-1:0]),
+				  .mul1_exponent	(mul1_exponent[7:0]),
 				  .mul1_sign		(mul1_sign),
+				  .mul_overflow_stage2	(mul_overflow_stage2),
 				  // Inputs
 				  .clk			(clk),
 				  .reset		(reset),
@@ -288,6 +292,14 @@ module multi_cycle_scalar_alu
 				else
 					multi_cycle_result = result_equal || result_negative;
 			end
+			
+			`OP_FMUL:
+			begin
+				if (mul_overflow_stage4)
+					multi_cycle_result = { norm_sign, 8'hff, 23'd0  };	// INF
+				else
+					multi_cycle_result = { norm_sign, norm_exponent, norm_significand };
+			end
 
 			default:
 			begin
@@ -312,6 +324,8 @@ module multi_cycle_scalar_alu
 			mul2_sign <= 1'h0;
 			mul3_exponent <= {EXPONENT_WIDTH{1'b0}};
 			mul3_sign <= 1'h0;
+			mul_overflow_stage3 <= 1'h0;
+			mul_overflow_stage4 <= 1'h0;
 			operation2 <= 6'h0;
 			operation3 <= 6'h0;
 			operation4 <= 6'h0;
@@ -344,6 +358,8 @@ module multi_cycle_scalar_alu
 			special_is_neg_stage2 <= special_is_neg_stage1;
 			special_is_neg_stage3 <= special_is_neg_stage2;
 			special_is_neg_stage4 <= special_is_neg_stage3;
+			mul_overflow_stage3 <= mul_overflow_stage2;
+			mul_overflow_stage4 <= mul_overflow_stage3;
 		end
 	end
 endmodule

rtl/core/fp_multiplier_stage1.v
@@ -35,13 +35,14 @@ module fp_multiplier_stage1
 	input [TOTAL_WIDTH - 1:0]					operand2,
 	output reg[31:0]							mul1_muliplicand,
 	output reg[31:0]							mul1_multiplier,
-	output reg[EXPONENT_WIDTH - 1:0] 			mul1_exponent,
-	output reg									mul1_sign);
+	output reg[7:0] 							mul1_exponent,
+	output reg									mul1_sign,
+	output reg									mul_overflow_stage2);
 
-	reg 										sign1;
-	reg[EXPONENT_WIDTH - 1:0] 					exponent1;
-	reg 										sign2;
-	reg[EXPONENT_WIDTH - 1:0] 					exponent2;
+	reg sign1;
+	reg[7:0] exponent1;
+	reg sign2;
+	reg[7:0] exponent2;
 
 	// Multiplicand
 	always @*
@@ -84,17 +85,18 @@ module fp_multiplier_stage1
 	wire result_sign = sign1 ^ sign2;
 
 	// Unbias the exponents so we can add them in two's complement.
-	wire[EXPONENT_WIDTH - 1:0] unbiased_exponent1 = { ~exponent1[EXPONENT_WIDTH - 1], 
-			exponent1[EXPONENT_WIDTH - 2:0] } + 1;
-	wire[EXPONENT_WIDTH - 1:0] unbiased_exponent2 = { ~exponent2[EXPONENT_WIDTH - 1], 
-			exponent2[EXPONENT_WIDTH - 2:0] } + 1;
+	wire[7:0] unbiased_exponent1 = { ~exponent1[7], exponent1[6:0] } + 1;
+	wire[7:0] unbiased_exponent2 = { ~exponent2[7], exponent2[6:0] } + 1;
 
 	// The result exponent is simply the sum of the two exponents.
-	wire[EXPONENT_WIDTH - 1:0] unbiased_result_exponent = unbiased_exponent1 + unbiased_exponent2;
+	wire[7:0] unbiased_result_exponent = unbiased_exponent1 + unbiased_exponent2;
+
+	wire exponent_overflow = unbiased_exponent1[7] == unbiased_exponent2[7]
+		&& unbiased_result_exponent[7] != unbiased_exponent1[7];
 
 	// Re-bias the result exponent.
-	wire[EXPONENT_WIDTH - 1:0] result_exponent = { ~unbiased_result_exponent[EXPONENT_WIDTH - 1], 
-			unbiased_result_exponent[EXPONENT_WIDTH - 2:0] } - 1;
+	wire[7:0] result_exponent = { ~unbiased_result_exponent[7], 
+		unbiased_result_exponent[6:0] } - 1;
 	
 	always @(posedge clk, posedge reset)
 	begin
@@ -102,14 +104,16 @@ module fp_multiplier_stage1
 		begin
 			/*AUTORESET*/
 			// Beginning of autoreset for uninitialized flops
-			mul1_exponent <= {EXPONENT_WIDTH{1'b0}};
+			mul1_exponent <= 8'h0;
 			mul1_sign <= 1'h0;
+			mul_overflow_stage2 <= 1'h0;
 			// End of automatics
 		end
 		else
 		begin
-			mul1_exponent				<= result_exponent;
-			mul1_sign 					<= result_sign;
+			mul1_exponent <= result_exponent;
+			mul1_sign <= result_sign;
+			mul_overflow_stage2 <= exponent_overflow;
 		end
 	end
 endmodule

rtl/core/single_cycle_scalar_alu.v
@@ -41,7 +41,7 @@ module single_cycle_scalar_alu(
 
 	// These flags are only valid if do_subtract is true.  Otherwise ignored.
 	wire negative = sum_difference[31]; 
-	wire overflow =	 operand2[31] == negative && operand1[31] != operand2[31];
+	wire overflow =	operand2[31] == negative && operand1[31] != operand2[31];
 	wire zero = sum_difference == 0;
 	wire signed_gtr = overflow == negative;
 

tests/directed_verification/fp_test.py
@@ -281,7 +281,8 @@ class FloatingPointTests(TestGroup):
 			f13 = f14 * f15
 			f16 = f17 * f18		; zero identity
 			f19 = f18 * f17		; zero identity (zero in second position)
-		''', { 't0u3' : 8.0, 
+		''', { 
+			't0u3' : 8.0, 
 			't0u4' : 2722.5003226,
 			't0u7' : -8.53947662,
 			't0u10' : 2.76,
@@ -317,7 +318,6 @@ class FloatingPointTests(TestGroup):
 			 	't0u4' : -123,
 			 	't0u6' : 23
 			}, None, None, None)
-			
 	
 	def test_ftoi2():
 		return ({ 'u1': 0.00009, 'u2' : 0.0 },
@@ -350,5 +350,13 @@ class FloatingPointTests(TestGroup):
 		
 		''', { 't0u2' : 0x3c053407, 't0u3' : None }, None, None, None )
 			
-
-			
+	def test_mulOverflow():
+		return ({ 'u1' : float('1e20'), 'u2' : float('1e-20') },
+			'''
+				f3 = f1 * f1
+				f4 = f2 * f2
+			''',
+			{ 	
+				't0u3' : float('inf'),
+				't0u4' : float('inf')
+			}, None, None, None)

commit 81173943ead68cdbbda4a8158db34a7f1c9b427d

Change 7ced087b9f4 broke handling of inf/NaN for multiplication. In many cases (including the tests, which all passed), the overflow logic would do the right thing, but not all.

rtl/core/multi_cycle_scalar_alu.v
@@ -292,19 +292,13 @@ module multi_cycle_scalar_alu
 				else
 					multi_cycle_result = result_equal || result_negative;
 			end
-			
-			`OP_FMUL:
-			begin
-				if (mul_overflow_stage4)
-					multi_cycle_result = { norm_sign, 8'hff, 23'd0  };	// INF
-				else
-					multi_cycle_result = { norm_sign, norm_exponent, norm_significand };
-			end
 
 			default:
 			begin
 				// Not a comparison, take the result as is.
-				if (result_is_nan_stage4)	// Quiet NaN
+				if (operation4 == `OP_FMUL && mul_overflow_stage4)
+					multi_cycle_result = { norm_sign, 8'hff, 23'd0  };	// INF
+				else if (result_is_nan_stage4)	// Quiet NaN
 					multi_cycle_result = { 1'b0, {EXPONENT_WIDTH{1'b1}}, 1'b1, {SIGNIFICAND_WIDTH - 1{1'b0}}  }; // nan
 				else if (result_is_inf_stage4)
 					multi_cycle_result = { special_is_neg_stage4, {EXPONENT_WIDTH{1'b1}}, {SIGNIFICAND_WIDTH{1'b0}} };	// inf