fe2f5a5a4dcaff83fc37fb3a32c616096b3723f3
Jeff Bush <jeffbush001@gmail.com>
2013-01-17 20:08:32 -0800

    Update boilerplate

--- firmware/3d-engine/ref_impl/Rasterizer.cpp
+++ firmware/3d-engine/ref_impl/Rasterizer.cpp
@@ -1,5 +1,5 @@
 // 
-// Copyright 2011-2012 Jeff Bush
+// Copyright 2011-2013 Jeff Bush
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
