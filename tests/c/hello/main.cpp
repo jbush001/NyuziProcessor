9df28665fc4eee036faa1c79aee748217c26f4fb
Jeff Bush <jeffbush001@gmail.com>
2013-07-20 13:40:31 -0700

    More advanced hello world.

--- tests/c/hello/main.cpp
+++ tests/c/hello/main.cpp
@@ -1,12 +1,44 @@
 
-void printstr(const char *string)
-{	
-	for (const char *c = string; *c; c++)
-		*((volatile unsigned int*) 0xFFFF0004) = *c;
-}
+class DebugPrinter
+{
+public:
+	DebugPrinter &operator<<(const char *str)
+	{
+		for (const char *c = str; *c; c++)
+			writeChar(*c);
+			
+		return *this;
+	}
+
+	DebugPrinter &operator<<(unsigned int value)
+	{
+		writeChar('0');
+		writeChar('x');
+		for (int i = 0; i < 8; i++)
+		{
+			int digitValue = value >> 28;
+			value <<= 4;
+			if (digitValue < 10)
+				writeChar(digitValue + '0');
+			else 
+				writeChar(digitValue - 10 + 'A');
+		}
+		
+		return *this;
+	}
+
+private:
+	void writeChar(char c)
+	{
+		*((volatile unsigned int*) 0xFFFF0004) = c;
+	}
+};
+
+DebugPrinter debug;
 
 int main()
 {
-	printstr("Hello World\n");
+	debug << "Hello World: " << 0x1234abcd << ".\n";
+	
 	return 0;
 }
