220e627d9cd2fc3d6dcc574e6fa9fbc01c3392c7
Jeff Bush <jeffbush001@gmail.com>
2013-07-21 21:26:56 -0700

    Add some comments

--- tests/fpga/whack_c/main.cpp
+++ tests/fpga/whack_c/main.cpp
@@ -1,3 +1,19 @@
+// 
+// Copyright 2011-2013 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
 typedef int veci16 __attribute__((__vector_size__(16 * sizeof(int))));
 
 veci16* const kFrameBufferAddress = (veci16*) 0x10000000;
@@ -5,6 +21,8 @@ const veci16 kXOffsets = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 
 
 int main()
 {
+	// Strands work on interleaved chunks of pixels.  The strand ID determines
+	// the starting point.
 	int myStrandId = __builtin_vp_get_current_strand();
 	for (int frameNum = 0; ; frameNum++)
 	{
@@ -18,7 +36,7 @@ int main()
 				veci16 fv = __builtin_vp_makevectori(frameNum);
 
 				*ptr = (xv+fv)+xv+(xv^(yv+fv))+fv;
-				ptr += 4;
+				ptr += 4;	// Skip over four chunks before there are four threads.
 			}
 		}
 	}
