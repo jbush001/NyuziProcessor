61478d86a745125d915b85db31591fddc43b411b
Jeff Bush <jeffbush001@gmail.com>
2013-07-26 19:47:26 -0700

    Texture demo in C

---
+++ tests/fpga/texture_c/main.cpp
@@ -0,0 +1,332 @@
+// 
+// Copyright 2011-2013 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+typedef int veci16 __attribute__((ext_vector_type(16)));
+typedef float vecf16 __attribute__((ext_vector_type(16)));
+
+veci16* const kFrameBufferAddress = (veci16*) 0x10000000;
+const vecf16 kXOffsets = { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 
+	8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f };
+extern unsigned int kImage[];
+
+struct Matrix
+{
+	float a;
+	float b;
+	float c;
+	float d;
+};
+
+int main()
+{
+	Matrix rotationMatrix = { 1.0f, 0.0f, 0.0f, 1.0f };
+
+	// Strands work on interleaved chunks of pixels.  The strand ID determines
+	// the starting point.
+	int myStrandId = __builtin_vp_get_current_strand();
+	while (true)
+	{
+		unsigned int imageBase = (unsigned int) kImage;
+		veci16 *outputPtr = kFrameBufferAddress + myStrandId;
+		for (int y = 0; y < 480; y++)
+		{
+			for (int x = myStrandId * 16; x < 640; x += 64)
+			{
+				vecf16 xv = kXOffsets + __builtin_vp_makevectorf((float) x);
+				vecf16 yv = __builtin_vp_makevectorf((float) y);
+				vecf16 u = xv * __builtin_vp_makevectorf(rotationMatrix.a)
+					 + yv * __builtin_vp_makevectorf(rotationMatrix.b);
+				vecf16 v = xv * __builtin_vp_makevectorf(rotationMatrix.c) 
+					+ yv * __builtin_vp_makevectorf(rotationMatrix.d);
+				
+				veci16 tx = (__builtin_vp_vftoi(u) & __builtin_vp_makevectori(15)) 
+					* __builtin_vp_makevectori(4);
+				veci16 ty = (__builtin_vp_vftoi(v) & __builtin_vp_makevectori(15)) 
+					* __builtin_vp_makevectori(4);
+				veci16 pixelPtrs = ty * __builtin_vp_makevectori(16) + tx 
+					+ __builtin_vp_makevectori(imageBase);
+				*outputPtr = __builtin_vp_gather_loadi(pixelPtrs);
+				outputPtr += 4;	// Skip over four chunks because there are four threads.
+			}
+		}
+		
+		rotationMatrix.a += 0.01f;
+		rotationMatrix.b -= 0.01f;
+		rotationMatrix.c += 0.01f;
+		rotationMatrix.d -= 0.01f;
+	}
+
+	return 0;
+}
+
+unsigned int kImage[] = {
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xfffff8,
+	0xc8ffeb,
+	0x68ffe3,
+	0x28ffdf,
+	0x7ffdf,
+	0x7ffe3,
+	0x28ffeb,
+	0x68fff8,
+	0xc8ffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffeb,
+	0x68ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68ffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffe7,
+	0x48ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffe7,
+	0x48ffff,
+	0xffffff,
+	0xffffff,
+	0xffffeb,
+	0x68ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68ffff,
+	0xfffff8,
+	0xc8ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xfff8,
+	0xc8ffeb,
+	0x68ffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68ffe3,
+	0x28ffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffe3,
+	0x28ffdf,
+	0x7ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffdf,
+	0x7ffdf,
+	0x7ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffdf,
+	0x7ffe3,
+	0x28ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffe3,
+	0x28ffeb,
+	0x68ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68fff8,
+	0xc8ffde,
+	0xffde,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xfff8,
+	0xc8ffff,
+	0xffffeb,
+	0x68ffde,
+	0xffde,
+	0x0,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68ffff,
+	0xffffff,
+	0xffffff,
+	0xffffe7,
+	0x48ffde,
+	0xffde,
+	0xffde,
+	0x0,
+	0x0,
+	0x0,
+	0x0,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffe7,
+	0x48ffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffeb,
+	0x68ffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffde,
+	0xffeb,
+	0x68ffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xffffff,
+	0xfffff8,
+	0xc8ffeb,
+	0x68ffe3,
+	0x28ffdf,
+	0x7ffdf,
+	0x7ffe3,
+	0x28ffeb,
+	0x68fff8,
+	0xc8ffff,
+	0xffffff,
+	0xffffff,
+	0xffffff
+};
