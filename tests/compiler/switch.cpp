48b7343392e513d82ba48e7684d87fa0b847366c
Jeff Bush <jeffbush001@gmail.com>
2013-08-07 20:55:49 -0700

    Add test for switch statements (this also fails)

---
+++ tests/compiler/switch.cpp
@@ -0,0 +1,52 @@
+// 
+// Copyright 2013 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+#include "output.h"
+
+int lookupSwitch(int x, int y)
+{
+	switch (x)
+	{
+		case 0:	// Falls through
+		case 1:
+			return y + 1;
+
+		case 2:
+			return y & 7;
+
+		case 3:
+			return y ^ 2;
+		
+		case 4:
+		case 5:	
+			return y * y;	
+			
+		default:
+			return y;
+	}
+}
+
+Output output;
+
+int main()
+{
+	int j = 0;
+	
+	for (int i = 0; i < 10; i++)
+		j = lookupSwitch(i, j);
+
+	output << (unsigned) j;
+}
