6e319ff7e9c0e007c90661555fa398eb64b5dced
Jeff Bush <jeffbush001@gmail.com>
2013-08-10 14:40:30 -0700

    Add another problem (currently causes compiler assert)

---
+++ tests/compiler/sumgrid.cpp
@@ -0,0 +1,107 @@
+// 
+// Copyright 2013 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+#include "output.h"
+
+// Project Euler problem #11: Largest product in a grid.
+
+int grid[20][20] = {
+	8,2,22,97,38,15,0,40,0,75,4,5,7,78,52,12,50,77,91,8,
+	49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,4,56,62,0,
+	81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,3,49,13,36,65,
+	52,70,95,23,4,60,11,42,69,24,68,56,1,32,56,71,37,2,36,91,
+	22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80,
+	24,47,32,60,99,3,45,2,44,75,33,53,78,36,84,20,35,17,12,50,
+	32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70,
+	67,26,20,68,2,62,12,20,95,63,94,39,63,8,40,91,66,49,94,21,
+	24,55,58,5,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72,
+	21,36,23,9,75,0,76,44,20,45,35,14,0,61,33,97,34,31,33,95,
+	78,17,53,28,22,75,31,67,15,94,3,80,4,62,16,14,9,53,56,92,
+	16,39,5,42,96,35,31,47,55,58,88,24,0,17,54,24,36,29,85,57,
+	86,56,0,48,35,71,89,7,5,44,44,37,44,60,21,58,51,54,17,58,
+	19,80,81,68,5,94,47,69,28,73,92,13,86,52,17,77,4,89,55,40,
+	4,52,8,83,97,35,99,16,7,97,57,32,16,26,26,79,33,27,98,66,
+	88,36,68,87,57,62,20,72,3,46,33,67,46,55,12,32,63,93,53,69,
+	04,42,16,73,38,25,39,11,24,94,72,18,8,46,29,32,40,62,76,36,
+	20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,4,36,16,
+	20,73,35,29,78,31,90,1,74,31,49,71,48,86,81,16,23,57,5,54,
+	1,70,54,71,83,51,54,69,16,92,33,48,61,43,52,1,89,19,67,48
+};
+
+Output output;
+
+int main()
+{
+	int startx, starty;
+	int index;
+	int prod1;
+	int prod2;
+	int prod3;
+	int prod4;
+	int maxprod = 0;
+
+	for (startx = 0; startx < 20; startx++)
+	{
+		for (starty = 0; starty < 20; starty++)
+		{
+			prod1 = 1;
+			prod2 = 1;
+			prod3 = 1;
+			prod4 = 1;
+		
+			for (index = 0; index < 4; index++)
+			{
+				if (startx < 16)
+				{
+					// Horizontal
+					prod1 *= grid[startx + index][starty];
+				}
+				
+				if (starty < 16)
+				{
+					// Vertical
+					prod2 *= grid[startx][starty + index];
+				}
+				
+				if (startx < 16 && starty < 16)
+				{
+					// right diagonal \
+					prod3 *= grid[startx + index][starty + index];
+				}
+
+				if (startx > 3 && starty < 16)
+				{
+					// left diagonal /
+					prod4 *= grid[startx - index][starty + index];
+				}
+			}
+
+			if (startx < 16 && prod1 > maxprod)
+				maxprod = prod1;
+			
+			if (starty < 16 && prod2 > maxprod)
+				maxprod = prod2;
+			
+			if (startx < 16 && starty < 16 && prod3 > maxprod)
+				maxprod = prod2;
+
+			if (startx > 3 && starty < 16 && prod4 > maxprod)
+				maxprod = prod4;
+		}
+	}
+	
+	output << "maxproduct is " << maxprod << "\n"; // CHECK: 0x043547E2
+}
