3f50f474b99066c52a1abe7b988c6bc164ab9152
Jeff Bush <jeffbush001@gmail.com>
2013-08-11 15:06:53 -0700

    Add another test

---
+++ tests/compiler/lzss.cpp
@@ -0,0 +1,279 @@
+/**************************************************************
+	LZSS.C -- A Data Compression Program
+	(tab = 4 spaces)
+***************************************************************
+	4/6/1989 Haruhiko Okumura
+	Use, distribute, and modify this program freely.
+	Please send me your improved versions.
+		PC-VAN		SCIENCE
+		NIFTY-Serve	PAF01022
+		CompuServe	74050,1022
+**************************************************************/
+
+#include "output.h"
+
+#define N		 4096	/* size of ring buffer */
+#define F		   18	/* upper limit for match_length */
+#define THRESHOLD	2   /* encode string into position and length
+						   if match_length is greater than this */
+#define NIL			N	/* index for root of binary search trees */
+
+unsigned long int
+		textsize = 0,	/* text size counter */
+		codesize = 0,	/* code size counter */
+		printcount = 0;	/* counter for reporting progress every 1K bytes */
+unsigned char
+		text_buf[N + F - 1];	/* ring buffer of size N,
+			with extra F-1 bytes to facilitate string comparison */
+int		match_position, match_length,  /* of longest match.  These are
+			set by the InsertNode() procedure. */
+		lson[N + 1], rson[N + 257], dad[N + 1];  /* left & right children &
+			parents -- These constitute binary search trees. */
+
+extern "C" void memset(void *output, int value, unsigned int len);
+extern "C" void memcpy(void *output, const void *input, unsigned int len);
+
+void memset(void *output, int value, unsigned int len)
+{
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+		((char *)output)[i] = (char)value;
+}
+
+void memcpy(void *output, const void *input, unsigned int len)
+{
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+		((char *)output)[i] = ((char *)input)[i];
+}
+
+unsigned long strlen(const char *str)
+{
+	long len = 0;
+	while (*str++)
+		len++;
+
+	return len;
+}
+
+void InitTree(void)  /* initialize trees */
+{
+	int  i;
+
+	/* For i = 0 to N - 1, rson[i] and lson[i] will be the right and
+	   left children of node i.  These nodes need not be initialized.
+	   Also, dad[i] is the parent of node i.  These are initialized to
+	   NIL (= N), which stands for 'not used.'
+	   For i = 0 to 255, rson[N + i + 1] is the root of the tree
+	   for strings that begin with character i.  These are initialized
+	   to NIL.  Note there are 256 trees. */
+
+	for (i = N + 1; i <= N + 256; i++) rson[i] = NIL;
+	for (i = 0; i < N; i++) dad[i] = NIL;
+}
+
+void InsertNode(int r)
+	/* Inserts string of length F, text_buf[r..r+F-1], into one of the
+	   trees (text_buf[r]'th tree) and returns the longest-match position
+	   and length via the global variables match_position and match_length.
+	   If match_length = F, then removes the old node in favor of the new
+	   one, because the old one will be deleted sooner.
+	   Note r plays double role, as tree node and position in buffer. */
+{
+	int  i, p, cmp;
+	unsigned char  *key;
+
+	cmp = 1;  key = &text_buf[r];  p = N + 1 + key[0];
+	rson[r] = lson[r] = NIL;  match_length = 0;
+	for ( ; ; ) {
+		if (cmp >= 0) {
+			if (rson[p] != NIL) p = rson[p];
+			else {  rson[p] = r;  dad[r] = p;  return;  }
+		} else {
+			if (lson[p] != NIL) p = lson[p];
+			else {  lson[p] = r;  dad[r] = p;  return;  }
+		}
+		for (i = 1; i < F; i++)
+			if ((cmp = key[i] - text_buf[p + i]) != 0)  break;
+		if (i > match_length) {
+			match_position = p;
+			if ((match_length = i) >= F)  break;
+		}
+	}
+	dad[r] = dad[p];  lson[r] = lson[p];  rson[r] = rson[p];
+	dad[lson[p]] = r;  dad[rson[p]] = r;
+	if (rson[dad[p]] == p) rson[dad[p]] = r;
+	else                   lson[dad[p]] = r;
+	dad[p] = NIL;  /* remove p */
+}
+
+void DeleteNode(int p)  /* deletes node p from tree */
+{
+	int  q;
+	
+	if (dad[p] == NIL) return;  /* not in tree */
+	if (rson[p] == NIL) q = lson[p];
+	else if (lson[p] == NIL) q = rson[p];
+	else {
+		q = lson[p];
+		if (rson[q] != NIL) {
+			do {  q = rson[q];  } while (rson[q] != NIL);
+			rson[dad[q]] = lson[q];  dad[lson[q]] = dad[q];
+			lson[q] = lson[p];  dad[lson[p]] = q;
+		}
+		rson[q] = rson[p];  dad[rson[p]] = q;
+	}
+	dad[q] = dad[p];
+	if (rson[dad[p]] == p) rson[dad[p]] = q;  else lson[dad[p]] = q;
+	dad[p] = NIL;
+}
+
+int Encode(unsigned char *output, const unsigned char *input, int inputLength)
+{
+	int  i, c, len, r, s, last_match_length, code_buf_ptr;
+	unsigned char  code_buf[17], mask;
+	int inputOffset = 0;
+	int outputOffset = 0;
+	
+	InitTree();  /* initialize trees */
+	code_buf[0] = 0;  /* code_buf[1..16] saves eight units of code, and
+		code_buf[0] works as eight flags, "1" representing that the unit
+		is an unencoded letter (1 byte), "0" a position-and-length pair
+		(2 bytes).  Thus, eight units require at most 16 bytes of code. */
+	code_buf_ptr = mask = 1;
+	s = 0;  r = N - F;
+	for (i = s; i < r; i++) text_buf[i] = ' ';  /* Clear the buffer with
+		any character that will appear often. */
+	for (len = 0; len < F && inputOffset < inputLength; len++)
+		text_buf[r + len] = input[inputOffset++];  /* Read F bytes into the last F bytes of
+			the buffer */
+	if ((textsize = len) == 0) return 0;  /* text of size zero */
+	for (i = 1; i <= F; i++) InsertNode(r - i);  /* Insert the F strings,
+		each of which begins with one or more 'space' characters.  Note
+		the order in which these strings are inserted.  This way,
+		degenerate trees will be less likely to occur. */
+	InsertNode(r);  /* Finally, insert the whole string just read.  The
+		global variables match_length and match_position are set. */
+	do {
+		if (match_length > len) match_length = len;  /* match_length
+			may be spuriously long near the end of text. */
+		if (match_length <= THRESHOLD) {
+			match_length = 1;  /* Not long enough match.  Send one byte. */
+			code_buf[0] |= mask;  /* 'send one byte' flag */
+			code_buf[code_buf_ptr++] = text_buf[r];  /* Send uncoded. */
+		} else {
+			code_buf[code_buf_ptr++] = (unsigned char) match_position;
+			code_buf[code_buf_ptr++] = (unsigned char)
+				(((match_position >> 4) & 0xf0)
+			  | (match_length - (THRESHOLD + 1)));  /* Send position and
+					length pair. Note match_length > THRESHOLD. */
+		}
+		if ((mask <<= 1) == 0) {  /* Shift mask left one bit. */
+			for (i = 0; i < code_buf_ptr; i++)  /* Send at most 8 units of */
+				output[outputOffset++] = code_buf[i];     /* code together */
+			codesize += code_buf_ptr;
+			code_buf[0] = 0;  code_buf_ptr = mask = 1;
+		}
+		last_match_length = match_length;
+		for (i = 0; i < last_match_length && inputOffset < inputLength; i++) {
+			DeleteNode(s);		/* Delete old strings and */
+			c = input[inputOffset++];
+			text_buf[s] = c;	/* read new bytes */
+			if (s < F - 1) text_buf[s + N] = c;  /* If the position is
+				near the end of buffer, extend the buffer to make
+				string comparison easier. */
+			s = (s + 1) & (N - 1);  r = (r + 1) & (N - 1);
+				/* Since this is a ring buffer, increment the position
+				   modulo N. */
+			InsertNode(r);	/* Register the string in text_buf[r..r+F-1] */
+		}
+		while (i++ < last_match_length) {	/* After the end of text, */
+			DeleteNode(s);					/* no need to read, but */
+			s = (s + 1) & (N - 1);  r = (r + 1) & (N - 1);
+			if (--len) InsertNode(r);		/* buffer may not be empty. */
+		}
+	} while (len > 0);	/* until length of string to be processed is zero */
+	if (code_buf_ptr > 1) {		/* Send remaining code. */
+		for (i = 0; i < code_buf_ptr; i++) output[outputOffset++] = code_buf[i];
+		codesize += code_buf_ptr;
+	}
+	
+	return outputOffset;
+}
+
+/* Just the reverse of Encode(). */
+int Decode(unsigned char *output, const unsigned char *input, int inputLength)
+{
+	int  i, j, k, r, c;
+	unsigned int  flags;
+	int inputOffset = 0;
+	int outputOffset = 0;
+	
+	for (i = 0; i < N - F; i++) text_buf[i] = ' ';
+	r = N - F;  flags = 0;
+	for ( ; ; ) {
+		if (((flags >>= 1) & 256) == 0) {
+			if (inputOffset >= inputLength)
+				break;
+
+			c = input[inputOffset++];
+			flags = c | 0xff00;		/* uses higher byte cleverly */
+		}							/* to count eight */
+		if (flags & 1) {
+			c = input[inputOffset++];
+			output[outputOffset++] = c;
+			if (inputOffset >= inputLength)
+				break;
+			text_buf[r++] = c;  r &= (N - 1);
+		} else {
+			if (inputOffset >= inputLength)
+				break;
+
+			i = input[inputOffset++];
+			if (inputOffset >= inputLength)
+				break;
+
+			j = input[inputOffset++];
+
+			i |= ((j & 0xf0) << 4);  j = (j & 0x0f) + THRESHOLD;
+			for (k = 0; k <= j; k++) {
+				c = text_buf[(i + k) & (N - 1)];
+				output[outputOffset++] = c;  
+				text_buf[r++] = c;  
+				r &= (N - 1);
+			}
+		}
+	}
+	
+	return outputOffset;
+}
+
+char kRawText[] = "Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal."
+"Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this."
+"But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.";
+
+char encodedBuffer[4096];
+char decodedBuffer[4096];
+
+Output output;
+
+int main(int argc, char *argv[])
+{
+	int inLength = strlen(kRawText) + 1;
+	int encodedLength = Encode((unsigned char*) encodedBuffer, (unsigned char*) kRawText, 
+		inLength);
+	int decodedLength = Decode((unsigned char*) decodedBuffer, (unsigned char*) encodedBuffer, 
+		encodedLength);
+
+	output << "decodedLength " << decodedLength << "\n";
+	// CHECK: decodedLength 0x000005c0
+	
+	output << decodedBuffer;
+	
+	// CHECK: Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty
+	// CHECK: of the people, by the people, for the people, shall not perish from the earth.
+
+	return 0;
+}
