8c10c0b46efb753f61e2f5021083c6bf06d19f29
Jeff Bush <jeffbush001@gmail.com>
2013-08-24 08:07:09 -0700

    Turn on optimizations for verilated sources.  Properly toggle clock and deassert reset.

--- rtl/testbench/verilator_main.cpp
+++ rtl/testbench/verilator_main.cpp
@@ -1,17 +1,34 @@
-#include <stdio.h>
+#include <iostream>
 #include "Vgpgpu.h"
 #include "verilated.h"
+
+using namespace std;
+
+vluint64_t currentTime = 0;  
 
 int main(int argc, char **argv, char **env) 
 {
 	Verilated::commandArgs(argc, argv);
 	Vgpgpu* top = new Vgpgpu;
+	top->reset = 1;
+
 	while (!Verilated::gotFinish()) 
 	{
-		printf("cycle\n"); 
+		if (currentTime > 10)
+			top->reset = 0;   // Deassert reset
+
+		// Toggle clock
+		if ((currentTime % 10) == 1)
+			top->clk = 1;
+		else if ((currentTime % 10) == 6)
+			top->clk = 0;
+
 		top->eval(); 
+		cout << (int) top->axi_arvalid << endl; 
+		currentTime++; 
 	}
 	
+	top->final();
 	delete top;
 	exit(0);
 }
