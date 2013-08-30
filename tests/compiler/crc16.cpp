c3bafa19d865bb985f9029eba36de4c19b39badf
Jeff Bush <jeffbush001@gmail.com>
2013-08-30 09:11:11 -0700

    Fix compiler tests on Linux

--- tests/compiler/crc16.cpp
+++ tests/compiler/crc16.cpp
@@ -25,7 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "Output.h"
+#include "output.h"
 
 /* CRC16 implementation acording to CCITT standards */
 
--- tests/compiler/runtest.sh
+++ tests/compiler/runtest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # 
 # Copyright 2011-2013 Jeff Bush
