37b643f442291a2176244563ef1d6c87dc710775
Jeff Bush <jeffbush001@gmail.com>
2013-09-07 22:30:03 -0700

    Fix vertex parameters for color shader test. Add boilerplate.

--- firmware/nuengine/start.s
+++ firmware/nuengine/start.s
@@ -1,3 +1,24 @@
+; 
+; Copyright 2013 Jeff Bush
+; 
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+
+; Stacks:
+; 0x1c000 - 0x1fffc  thread 0
+; 0x18000 - 0x1bffc  thread 1
+; 0x14000 - 0x17ffc  thread 2
+; 0x10000 - 0x13ffc  thread 3
 
 					.text
 					.globl _start
