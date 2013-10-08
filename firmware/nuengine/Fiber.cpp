8cdbe6b65ae7f8aeddc53129c49b76e96e4436e1
Jeff Bush <jeffbush001@gmail.com>
2013-10-07 22:13:36 -0700

    Add boilerplate

--- firmware/nuengine/Fiber.cpp
+++ firmware/nuengine/Fiber.cpp
@@ -1,3 +1,19 @@
+// 
+// Copyright 2013 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #include "Fiber.h"
 #include "utils.h"
 #include "Debug.h"
@@ -19,10 +35,11 @@ Fiber *Fiber::spawnFiber(void (*startFunction)())
 	newFiber->fStackBase = static_cast<unsigned int*>(allocMem(kDefaultStackSize 
 		* sizeof(int)));
 
-	// This assumes the format defined in context_switch.s
+	// This assumes the frame format defined in context_switch.s
 	newFiber->fStackPointer = newFiber->fStackBase + kDefaultStackSize - 272;
+
+	// Set link pointer
 	newFiber->fStackPointer[14] = reinterpret_cast<unsigned int>(startFunction);
-		// Set link pointer
 
 	return newFiber;
 }
