317002b90fe4e3d6bd53a1c02d5fb10e46d9415f
Jeff Bush <jeffbush001@gmail.com>
2013-10-22 07:50:48 -0700

    Add test cases for unsigned int to floating point

--- tests/compiler/fconv.cpp
+++ tests/compiler/fconv.cpp
@@ -5,14 +5,17 @@ Output output;
 float a = 123.0;
 int b = 79;
 unsigned int c = 24;
+unsigned int f = 0x81234000;
 
 int main()
 {
 	float d = b;
 	float e = c;
+	float g = f;
 	
 	output << (int) a;			// CHECK: 0x0000007b
 	output << (unsigned int) a;	// CHECK: 0x0000007b
 	output << (int) d;			// CHECK: 0x0000004f
-//	output << (int) e;			// XXX should be 0x18, but is 0x17 for some reason
+	output << (int) e;			// CHECK: 0x00000018
+	output << (unsigned int) g;	// CHECK: 0x81234000
 }
