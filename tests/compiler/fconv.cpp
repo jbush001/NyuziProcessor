e57034a707843af8cdb451d66e967974850dbd34
Jeff Bush <jeffbush001@gmail.com>
2013-10-21 21:46:50 -0700

    Floating point conversion tests

---
+++ tests/compiler/fconv.cpp
@@ -0,0 +1,17 @@
+#include "output.h"
+
+Output output;
+
+float a = 123.0;
+int b = 79;
+unsigned int c = 24;
+
+int main()
+{
+	float d = b;
+	float e = c;
+	
+	output << (int) a;			// CHECK: 0x0000007b
+	output << (unsigned int) a;	// CHECK: 0x0000007b
+	output << (int) d;			// CHECK: 0x0000004f
+}
