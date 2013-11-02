9cc4ab68566d197809094933ad56537a81f47492
Jeff Bush <jeffbush001@gmail.com>
2013-11-02 07:42:52 -0700

    Remove old copy benchmark (which is replaced by membench)

--- benchmarks/copy/runtest.sh
+++
@@ -1,8 +0,0 @@
-NAME=copy3
-BASEDIR=../..
-
-mkdir -p WORK
-$BASEDIR/tools/assembler/assemble -o WORK/$NAME.hex $NAME.asm
-
-vvp $BASEDIR/rtl/sim.vvp +statetrace=statetrace.txt +bin=WORK/$NAME.hex +simcycles=60000
-
