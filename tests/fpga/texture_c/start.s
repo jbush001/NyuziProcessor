3eeeb7509072c804bf3c7088f73df5feeda8a96c
Jeff Bush <jeffbush001@gmail.com>
2013-11-24 15:22:54 -0800

    More cleanup

--- tests/fpga/texture_c/start.s
+++ tests/fpga/texture_c/start.s
@@ -6,14 +6,14 @@
 _start:				move s0, 15
 					setcr s0, 30		; Start all threads
 
-					load.32 sp, stacks_base
+					load_32 sp, stacks_base
 					getcr s0, 0			; get my strand ID
 					shl s0, s0, 13		; 8192 bytes per stack
-					add.i sp, sp, s0	; Compute stack address
+					add_i sp, sp, s0	; Compute stack address
 
 					call main
 					setcr s0, 29		; Stop thread
 done:				goto done
 
-stacks_base:		.word 0x1012e000	; end of FB + 8192 bytes
+stacks_base:		.long 0x1012e000	; end of FB + 8192 bytes
 
