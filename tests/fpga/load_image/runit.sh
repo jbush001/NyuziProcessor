885472df2323cb3dd962364e0f87c440840a2658
Jeff Bush <jeffbush001@gmail.com>
2013-11-30 17:28:02 -0800

    Update to new assembler format.

--- tests/fpga/load_image/runit.sh
+++ tests/fpga/load_image/runit.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-../../../tools/assembler/assemble -o load_image.hex load_image.asm
+/usr/local/llvm-vectorproc/bin/clang -o load_image.elf load_image.S
+/usr/local/llvm-vectorproc/bin/elf2hex load_image.hex load_image.elf
 jload load_image.hex
