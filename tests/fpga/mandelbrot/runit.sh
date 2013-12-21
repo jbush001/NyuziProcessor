4206d07fd6b11337673d50cb47dd564446047c35
Jeff Bush <jeffbush001@gmail.com>
2013-12-21 08:58:28 -0800

    Convert mandelbrot to new assembly format

--- tests/fpga/mandelbrot/runit.sh
+++ tests/fpga/mandelbrot/runit.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-../../../tools/assembler/assemble -o mandelbrot.hex mandelbrot.asm
+/usr/local/llvm-vectorproc/bin/clang -o mandelbrot.elf mandelbrot.S
+/usr/local/llvm-vectorproc/bin/elf2hex -o mandelbrot.hex mandelbrot.elf
 jload mandelbrot.hex
