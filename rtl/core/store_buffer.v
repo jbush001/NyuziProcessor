78ceb3424b7cd98cc09cf140d59b89c9b500a391
Jeff Bush <jeffbush001@gmail.com>
2013-12-25 20:36:43 -0800

    Address another store buffer race condition. If a store collision occurs, in this case when a regular store completes in the same cycle a synchronized store is queued, sync_store_wait would not be set properly because of the order of parenthesis. This could cause a hang.

--- rtl/core/store_buffer.v
+++ rtl/core/store_buffer.v
@@ -278,13 +278,11 @@ module store_buffer
 			end
 	
 			// Keep track of synchronized stores
-			sync_store_wait <= (sync_store_wait | (sync_req_oh & ~got_sync_store_response)) 
-				& ~l2_ack_oh;
-			got_sync_store_response <= (got_sync_store_response | (sync_store_wait & l2_ack_oh)) 
-				& ~sync_req_oh;
-			if ((l2_ack_oh & sync_store_wait) != 0)
-				sync_store_result[l2rsp_strand] <= l2rsp_status;
-	
+			sync_store_wait <= ((sync_store_wait & ~l2_ack_oh) 
+				| (sync_req_oh & ~got_sync_store_response));
+			got_sync_store_response <= ((got_sync_store_response & ~sync_req_oh) 
+				| (l2_ack_oh & sync_store_wait));
+			sync_store_result[l2rsp_strand] <= l2rsp_status;
 			need_sync_rollback_latched <= need_sync_rollback;
 		end
 	end
