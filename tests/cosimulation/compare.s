146ff184ed45465db72993c68b034afd50c3c8e1
Jeff Bush <jeffbush001@gmail.com>
2014-04-23 19:40:46 -0700

    Add scalar compare tests

--- tests/cosimulation/compare.s
+++ tests/cosimulation/compare.s
@@ -19,17 +19,29 @@ _start:		lea s0, vec1
 			setge_u s8, v0, v1
 			setle_u s9, v0, v1
 
-			load_32 s10, val3
-			seteq_i s0, v0, s10
-			setne_i s1, v0, s10
-			setgt_i s2, v0, s10
-			setlt_i s3, v0, s10
-			setge_i s4, v0, s10
-			setle_i s5, v0, s10
-			setgt_u s6, v0, s10
-			setlt_u s7, v0, s10
-			setge_u s8, v0, s10
-			setle_u s9, v0, s10
+			load_32 s20, val3
+			seteq_i s0, v0, s20
+			setne_i s1, v0, s20
+			setgt_i s2, v0, s20
+			setlt_i s3, v0, s20
+			setge_i s4, v0, s20
+			setle_i s5, v0, s20
+			setgt_u s6, v0, s20
+			setlt_u s7, v0, s20
+			setge_u s8, v0, s20
+			setle_u s9, v0, s20
+			
+			load_32 s21, val4
+			seteq_i s0, s21, s20
+			setne_i s1, s21, s20
+			setgt_i s2, s21, s20
+			setlt_i s3, s21, s20
+			setge_i s4, s21, s20
+			setle_i s5, s21, s20
+			setgt_u s6, s21, s20
+			setlt_u s7, s21, s20
+			setge_u s8, s21, s20
+			setle_u s9, s21, s20
 
 			setcr s0, 29
 done: 		goto done
@@ -38,3 +50,4 @@ done: 		goto done
 vec1: .long BU, BS, BU, BS, BU, SM, BS, SM, BU, BS, BU, BS, BU, SM, BS, SM 
 vec2: .long BU, BS, BS, BU, SM, BU, SM, BS, BU, BS, BS, BU, SM, BU, SM, BS
 val3: .long BU
+val4: .long BS
