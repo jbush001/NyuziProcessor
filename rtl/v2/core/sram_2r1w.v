99f8080f06fa913a37c4f6bfe13c948ed326feba
Jeff Bush <jeffbush001@gmail.com>
2014-05-08 07:16:07 -0700

    Add byte enables to sram_2r1w

--- rtl/v2/core/sram_2r1w.v
+++ rtl/v2/core/sram_2r1w.v
@@ -27,41 +27,84 @@
 module sram_2r1w
 	#(parameter DATA_WIDTH = 32,
 	parameter SIZE = 1024,
-	parameter ADDR_WIDTH = $clog2(SIZE))
-	(input logic                     clk,
-	input logic                      rd1_en,
-	input logic[ADDR_WIDTH - 1:0]    rd1_addr,
+	parameter ENABLE_BYTE_LANES = 0,
+	parameter ADDR_WIDTH = $clog2(SIZE),
+	parameter BYTE_LANES = (DATA_WIDTH / 8))
+	(input                           clk,
+	input                            rd1_en,
+	input [ADDR_WIDTH - 1:0]         rd1_addr,
 	output logic[DATA_WIDTH - 1:0]   rd1_data,
-	input logic                      rd2_en,
-	input logic[ADDR_WIDTH - 1:0]    rd2_addr,
+	input                            rd2_en,
+	input [ADDR_WIDTH - 1:0]         rd2_addr,
 	output logic[DATA_WIDTH - 1:0]   rd2_data,
-	input logic                      wr_en,
-	input logic[ADDR_WIDTH - 1:0]    wr_addr,
-	input logic[DATA_WIDTH - 1:0]    wr_data);
+	input                            wr_en,
+	input [ADDR_WIDTH - 1:0]         wr_addr,
+	input [DATA_WIDTH - 1:0]         wr_data,
+	input [BYTE_LANES - 1:0]         wr_byte_enable);
 
-	logic[DATA_WIDTH - 1:0] data[SIZE];
 `ifdef VENDOR_ALTERA
-	// Note that the use of blocking assignments is not usually proper
-	// in sequential logic, but this is explicitly recommended by 
-	// Altera's "Recommended HDL Coding Styles" document (Example 13-13).
-	// to infer a block RAM with the proper read-after-write behavior. 
-	always_ff @(posedge clk)
-	begin
-		if (wr_en)
-			data[wr_addr] = wr_data;	
+	ALTSYNCRAM #(
+		.OPERATION_MODE("DUAL_PORT"),
+		.WIDTH_A(DATA_WIDTH),
+		.WIDTHAD_A(ADDR_WIDTH),
+		.WIDTH_B(DATA_WIDTH),
+		.WIDTHAD_B(ADDR_WIDTH),
+		.BYTE_SIZE(8),
+		.READ_DURING_WRITE_MODE_MIXED_PORTS("NEW_DATA")
+	) data0(
+		.data_a(wr_data),
+		.address_a(wr_addr),
+		.wren_a(wr_en),
+		.rden_a(1'b0),
+		.byteena_a(wr_byte_enable),
+		.q_a(),
+		.data_b(0),
+		.address_b(rd1_addr),
+		.wren_b(1'b0),
+		.rden_b(rd1_en),
+		.byteena_b(0),
+		.q_b(rd1_data),
+		.clock0(clk));
 
-		if (rd1_en)
-			rd1_data = data[rd1_addr];
-
-		if (rd2_en)
-			rd2_data = data[rd2_addr];
-	end
+	ALTSYNCRAM #(
+		.OPERATION_MODE("DUAL_PORT"),
+		.WIDTH_A(DATA_WIDTH),
+		.WIDTHAD_A(ADDR_WIDTH),
+		.WIDTH_B(DATA_WIDTH),
+		.WIDTHAD_B(ADDR_WIDTH),
+		.BYTE_SIZE(8),
+		.READ_DURING_WRITE_MODE_MIXED_PORTS("NEW_DATA")
+	) data1(
+		.data_a(wr_data),
+		.address_a(wr_addr),
+		.wren_a(wr_en),
+		.rden_a(1'b0),
+		.byteena_a(wr_byte_enable),
+		.q_a(),
+		.data_b(0),
+		.address_b(rd2_addr),
+		.wren_b(1'b0),
+		.rden_b(rd2_en),
+		.byteena_b(0),
+		.q_b(rd2_data),
+		.clock0(clk));
 `else
 	// Simulation
+	logic[DATA_WIDTH - 1:0] data[SIZE];
+
 	always_ff @(posedge clk)
 	begin
 		if (wr_en)
-			data[wr_addr] <= wr_data;	
+		begin
+			if (ENABLE_BYTE_LANES)
+			begin
+				for (int i = 0; i < BYTE_LANES; i++)
+					if (wr_byte_enable[i])
+						data[wr_addr][i * 8+:8] <= wr_data[i * i+:8];	
+			end
+			else
+				data[wr_addr] <= wr_data;	
+		end
 
 		if (wr_addr == rd1_addr && wr_en && rd1_en)
 			rd1_data <= wr_data;
