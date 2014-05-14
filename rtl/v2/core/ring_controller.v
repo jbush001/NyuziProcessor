b553530e7938c455aee7355a216ddbac866cea9d
Jeff Bush <jeffbush001@gmail.com>
2014-05-13 20:49:29 -0700

    Fix bus hang

--- rtl/v2/core/ring_controller.v
+++ rtl/v2/core/ring_controller.v
@@ -23,6 +23,11 @@
 // The ring controller processes packets passing through the ring and
 // inserts messages where appropriate.  It sets control signals to update L1
 // caches. Packets flow through a three stage pipeline.
+// Stage 1: Issue address to tag RAM to snoop if the data is present.
+//   Also check if there is a pending miss for the packet address
+// Stage 2: Update tag memory and issue read request to data memory if there
+//   is a writeback
+// Stage 3: Issue signals to update data memory.
 //
 
 module ring_controller
@@ -58,31 +63,28 @@ module ring_controller
 	input thread_idx_t                     dd_cache_miss_thread_idx,
 	input logic[`CACHE_LINE_BITS - 1:0]    dd_ddata_read_data);
 
-	ring_packet_t packet1;
-	ring_packet_t packet2;
-	ring_packet_t packet_out_nxt;
-	logic request_ready;
-	scalar_t request_address;
-	logic request_ack;
-	logic dcache_miss_pending1;
-	logic dcache_miss_pending2;
-	logic[`THREADS_PER_CORE - 1:0] dcache_miss_entry1;
-	logic[`THREADS_PER_CORE - 1:0] dcache_miss_entry2;
-	logic[`L1D_WAYS - 1:0] snoop_hit_way_oh;
-	pending_miss_state_t pending_miss_state1;
-	logic dcache_request_ready;
-	scalar_t dcache_request_address;
-	logic dcache_request_store;
-	logic dcache_request_ack;
-	l1d_addr_t cache_addr1;
-	l1d_addr_t cache_addr2;
-	l1d_way_idx_t snoop_hit_way_idx;
+	ring_packet_t rc1_packet;
+	logic rc1_dcache_miss_pending;
+	logic[`THREADS_PER_CORE - 1:0] rc1_dcache_miss_entry;
+	logic[`L1D_WAYS - 1:0] rc1_snoop_hit_way_oh;
+	logic[`L1D_WAYS - 1:0] rc1_fill_way_oh;	
+	l1d_way_idx_t rc1_fill_way_idx;
+	l1d_addr_t rc1_cache_addr;
+	pending_miss_state_t rc1_pending_miss_state;
+	logic rc2_dcache_miss_pending;
+	ring_packet_t rc2_packet;
+	logic[`THREADS_PER_CORE - 1:0] rc2_dcache_miss_entry;
+	l1d_addr_t rc2_cache_addr;
+	l1d_way_idx_t rc1_snoop_hit_way_idx;
 	logic dcache_snoop_hit;
-	l1d_way_idx_t fill_way_idx1;
-	l1d_way_idx_t fill_way_idx2;
-	logic[`L1D_WAYS - 1:0] fill_way_oh1;	
-	logic need_writeback2;
-	scalar_t evicted_line_addr2;
+	l1d_way_idx_t rc2_fill_way_idx;
+	logic rc2_need_writeback;
+	scalar_t rc2_evicted_line_addr;
+	ring_packet_t packet_out_nxt;
+	logic dcache_miss_ready;
+	scalar_t dcache_miss_address;
+	logic dcache_miss_store;
+	logic dcache_miss_ack;
 		
 	l1_miss_queue dcache_miss_queue(
 		// Enqueue new requests
@@ -95,20 +97,20 @@ module ring_controller
 		.snoop_en(packet_in.valid),
 		.snoop_addr(packet_in.address),
 		.snoop_hit(dcache_snoop_hit),
-		.snoop_hit_entry(dcache_miss_entry1),
-		.snoop_state(pending_miss_state1),
+		.snoop_hit_entry(rc1_dcache_miss_entry),
+		.snoop_state(rc1_pending_miss_state),
 
 		// Wake threads when a transaction is complete
-		.wake_en(packet2.valid && dcache_miss_pending2),
-		.wake_entry(dcache_miss_entry2),
+		.wake_en(rc2_packet.valid && rc2_dcache_miss_pending),
+		.wake_entry(rc2_dcache_miss_entry),
 
 		// Insert new requests into ring
-		.request_ready(dcache_request_ready),
-		.request_address(dcache_request_address),
-		.request_store(dcache_request_store),      
-		.request_ack(dcache_request_ack),
+		.request_ready(dcache_miss_ready),
+		.request_address(dcache_miss_address),
+		.request_store(dcache_miss_store),      
+		.request_ack(dcache_miss_ack),
 		.*);
-	
+
 	//////////////////////////////////////////////////////////////////////////////
 	// Stage 1.  Issue snoop request to L1 tags and check miss queue.
 	//////////////////////////////////////////////////////////////////////////////
@@ -118,63 +120,65 @@ module ring_controller
 	always_ff @(posedge clk, posedge reset)
 	begin
 		if (reset)
-			packet1 <= 0;
+			rc1_packet <= 0;
 		else
-			packet1 <= packet_in;
+			rc1_packet <= packet_in;
 	end
-	
-	//////////////////////////////////////////////////////////////////////////////
-	// Stage 2. Update the tag and read an old line if one is to be evicted
-	//////////////////////////////////////////////////////////////////////////////
-	assign cache_addr1 = packet1.address;
-	assign dcache_miss_pending1 = dcache_snoop_hit && packet1.valid;
+
+	assign rc1_cache_addr = rc1_packet.address;
+	assign rc1_dcache_miss_pending = dcache_snoop_hit && rc1_packet.valid;
 
 	genvar way_idx;
 	generate
 		for (way_idx = 0; way_idx < `L1D_WAYS; way_idx++)
 		begin
-			assign snoop_hit_way_oh[way_idx] = dt_snoop_tag[way_idx] == cache_addr1.tag 
+			assign rc1_snoop_hit_way_oh[way_idx] = dt_snoop_tag[way_idx] == rc1_cache_addr.tag 
 				&& dt_snoop_state[way_idx] != CL_STATE_INVALID;
 		end
 	endgenerate
 
 	one_hot_to_index #(.NUM_SIGNALS(`L1D_WAYS)) convert_snoop_hit(
-		.index(snoop_hit_way_idx),
-		.one_hot(snoop_hit_way_oh));
+		.index(rc1_snoop_hit_way_idx),
+		.one_hot(rc1_snoop_hit_way_oh));
 
-	assign fill_way_idx1 = |snoop_hit_way_oh ? snoop_hit_way_idx : dt_snoop_lru;
+	assign rc1_fill_way_idx = |rc1_snoop_hit_way_oh ? rc1_snoop_hit_way_idx : dt_snoop_lru;
 
 	index_to_one_hot #(.NUM_SIGNALS(`L1D_WAYS)) convert_tag_update(
-		.index(fill_way_idx1),
-		.one_hot(fill_way_oh1));
+		.index(rc1_fill_way_idx),
+		.one_hot(rc1_fill_way_oh));
+	
+	//////////////////////////////////////////////////////////////////////////////
+	// Stage 2. Update the tag and read an old line if one is to be evicted
+	//////////////////////////////////////////////////////////////////////////////
 
-	assign rc_dtag_update_en_oh = fill_way_oh1 && {`L1D_WAYS{packet1.valid}};
-	assign rc_dtag_update_tag = cache_addr1.tag;	
-	assign rc_dtag_update_set = cache_addr1.set_idx;
-	assign rc_dtag_update_state = pending_miss_state1 == PM_READ_PENDING ? CL_STATE_SHARED
+	assign rc_dtag_update_en_oh = rc1_fill_way_oh && {`L1D_WAYS{rc1_packet.valid}};
+	assign rc_dtag_update_tag = rc1_cache_addr.tag;	
+	assign rc_dtag_update_set = rc1_cache_addr.set_idx;
+	assign rc_dtag_update_state = rc1_pending_miss_state == PM_READ_PENDING ? CL_STATE_SHARED
 		: CL_STATE_MODIFIED;
 
-	assign rc_ddata_read_en = packet1.valid && dcache_miss_pending1;
-	assign rc_ddata_read_set = cache_addr1.set_idx;
-	assign rc_ddata_read_way = fill_way_idx1;
+	assign rc_ddata_read_en = rc1_packet.valid && rc1_dcache_miss_pending;
+	assign rc_ddata_read_set = rc1_cache_addr.set_idx;
+	assign rc_ddata_read_way = rc1_fill_way_idx;
 	
 	always_ff @(posedge clk, posedge reset)
 	begin
 		if (reset)
 		begin
-			fill_way_idx2 <= 0;
-			packet2 <= 0;
-			dcache_miss_entry2 <= 0;
-			dcache_miss_pending2 <= 0;
+			rc2_fill_way_idx <= 0;
+			rc2_packet <= 0;
+			rc2_dcache_miss_entry <= 0;
+			rc2_dcache_miss_pending <= 0;
 		end
 		else
 		begin
-			fill_way_idx2 <= fill_way_idx1;
-			packet2 <= packet1;
-			dcache_miss_entry2 <= dcache_miss_entry1;
-			dcache_miss_pending2 <= dcache_miss_pending1;
-			need_writeback2 <= dt_snoop_state[snoop_hit_way_idx] == CL_STATE_MODIFIED;
-			evicted_line_addr2 <= {dt_snoop_tag[snoop_hit_way_idx], cache_addr1.set_idx, {`CACHE_LINE_OFFSET_WIDTH{1'b0}}};
+			rc2_fill_way_idx <= rc1_fill_way_idx;
+			rc2_packet <= rc1_packet;
+			rc2_dcache_miss_entry <= rc1_dcache_miss_entry;
+			rc2_dcache_miss_pending <= rc1_dcache_miss_pending;
+			rc2_need_writeback <= dt_snoop_state[rc1_snoop_hit_way_idx] == CL_STATE_MODIFIED
+				&& rc1_packet.valid;
+			rc2_evicted_line_addr <= {dt_snoop_tag[rc1_snoop_hit_way_idx], rc1_cache_addr.set_idx, {`CACHE_LINE_OFFSET_WIDTH{1'b0}}};
 		end
 	end
 	
@@ -183,56 +187,63 @@ module ring_controller
 	// If there is an empty ring slot and a request is pending, issue it now.
 	// Wake up threads if necessary.
 	//////////////////////////////////////////////////////////////////////////////
+
+	assign rc2_cache_addr = rc2_packet.address;
+	assign rc_ddata_update_en = rc2_packet.valid && rc2_dcache_miss_pending && rc2_packet.ack;
+	assign rc_ddata_update_way = rc2_fill_way_idx;	
+	assign rc_ddata_update_set = rc2_cache_addr.set_idx;
+	assign rc_ddata_update_data = rc2_packet.data;
+
 	always_comb
 	begin
-		dcache_request_ack = 0;
-		if (packet2.valid && (packet2.dest_node != NODE_ID || !packet2.ack))
-			packet_out_nxt = packet2;	// Forward packet on (or retry nacked packet)
-		else if (need_writeback2)
+		dcache_miss_ack = 0;
+		if (rc2_packet.valid)
 		begin
-			// Insert L2 write ack packet
-			packet_out_nxt.valid = 1;
-			packet_out_nxt.packet_type = PKT_L2_WRITEBACK;
-			packet_out_nxt.ack = 0;
-			packet_out_nxt.l2_miss = 0;
-			packet_out_nxt.dest_node = 0;	// XXX L2 node ID
-			packet_out_nxt.address = evicted_line_addr2;
-			packet_out_nxt.data = dd_ddata_read_data;
+			if (rc2_packet.dest_node != NODE_ID || !rc2_packet.ack)
+			begin
+				// Forward packet for another node on (or retry nacked packet)
+				assert(!rc2_need_writeback);
+				packet_out_nxt = rc2_packet;	
+			end
+			else if (rc2_need_writeback)
+			begin
+				// Insert L2 writeback packet
+				packet_out_nxt.valid = 1;
+				packet_out_nxt.packet_type = PKT_L2_WRITEBACK;
+				packet_out_nxt.ack = 0;
+				packet_out_nxt.l2_miss = 0;
+				packet_out_nxt.dest_node = 0;	// XXX L2 node ID
+				packet_out_nxt.address = rc2_evicted_line_addr;
+				packet_out_nxt.data = dd_ddata_read_data;
+			end
+			else
+			begin
+				// To avoid starvation, a node that consumes a response from the ring
+				// leaves that slot empty (unless an L2 writeback is necessary)
+				packet_out_nxt = 0;
+			end
 		end
-		else if (dcache_request_ready)
+		else if (dcache_miss_ready)
 		begin
 			// Inject request packet into ring
-			dcache_request_ack = 1;
+			dcache_miss_ack = 1;
 			packet_out_nxt.valid = 1;
-			packet_out_nxt.packet_type = dcache_request_store ? PKT_WRITE_INVALIDATE : PKT_READ_SHARED;
+			packet_out_nxt.packet_type = dcache_miss_store ? PKT_WRITE_INVALIDATE : PKT_READ_SHARED;
 			packet_out_nxt.ack = 0;
 			packet_out_nxt.l2_miss = 0;
 			packet_out_nxt.dest_node = NODE_ID;
-			packet_out_nxt.address = dcache_request_address;
+			packet_out_nxt.address = dcache_miss_address;
 		end
 		else
 			packet_out_nxt = 0;	// Empty packet
 	end
-
-	assign cache_addr2 = packet2.address;
-	assign rc_ddata_update_en = packet2.valid && dcache_miss_pending2 && packet2.ack;
-	assign rc_ddata_update_way = fill_way_idx2;	
-	assign rc_ddata_update_set = cache_addr2.set_idx;
-	assign rc_ddata_update_data = packet2.data;
-	
-	// XXX need to cancel entry in miss queue (probably should wake threads as side effect)
-	// XXX should there be an identifier for the miss queue entry?
 	
 	always_ff @(posedge clk, posedge reset)
 	begin
 		if (reset)
-		begin
 			packet_out <= 0;
-		end
 		else
-		begin
 			packet_out <= packet_out_nxt;
-		end
 	end
 endmodule
 
