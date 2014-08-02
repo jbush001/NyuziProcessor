b40be04f3fd849149149c9ecd7343b5bcdbc8c6f
Jeff Bush <jeffbush001@gmail.com>
2014-08-01 18:21:19 -0700

    Make instruction selection easier to parameterize.
    Add stubbed version of generate_device_io. Causes assertion currently, need to debug.

--- tests/cosimulation/generate_random.py
+++ tests/cosimulation/generate_random.py
@@ -25,6 +25,7 @@
 # v1, s1 - Computed address registers.  Guaranteed to be 64 byte aligned and in private memory segment.
 # v2, s2 - Base registers for private data segment (read/write, per thread)
 # v3-v8, s3-s8 - Operation registers
+# s9 - pointer into register mapped IO space (0xffff0000)
 #
 # Memory map:
 #  00000 start of code (strand0, 1, 2, 3), shared data segment (read only)
@@ -41,6 +42,9 @@
 import random
 import sys
 import argparse
+
+def generate_arith_reg():
+	return random.randint(3, 8)
 
 FP_FORMS = [
 	('s', 's', 's', ''),
@@ -89,6 +93,40 @@ UNARY_OPS = [
 	'move'
 ]
 
+
+def generate_binary_arith(file):
+	mnemonic = random.choice(BINARY_OPS)
+	if mnemonic == 'shuffle':
+		typed = 'v'
+		typea = 'v'
+		typeb = 'v'
+		suffix = '' if random.randint(0, 1) == 0 else '_mask'
+	elif mnemonic == 'getlane':
+		typed = 's'
+		typea = 'v'
+		typeb = 's' if random.randint(0, 1) == 0 else 'i'
+		suffix = ''
+	elif mnemonic[-2:] == '_f':
+		typed, typea, typeb, suffix = random.choice(FP_FORMS)
+	else:
+		typed, typea, typeb, suffix = random.choice(INT_FORMS)
+
+	dest = generate_arith_reg()
+	rega = generate_arith_reg()
+	regb = generate_arith_reg()
+	maskreg = generate_arith_reg()
+	opstr = '\t\t' + mnemonic + suffix + ' ' + typed + str(dest) + ', '
+	if suffix != '':
+		opstr += 's' + str(maskreg)	+ ', ' # Add mask register
+
+	opstr += typea + str(rega) + ', '
+	if typeb == 'i':
+		opstr += str(random.randint(0, 0x1ff))	# Immediate value
+	else:
+	 	opstr += typeb + str(regb)
+
+	file.write(opstr + '\n')
+
 COMPARE_FORMS = [
 	('v', 'v'),
 	('v', 's'),
@@ -112,6 +150,21 @@ COMPARE_OPS = [
 	'le_f'
 ]
 
+def generate_compare(file):
+	typea, typeb = random.choice(COMPARE_FORMS)
+	dest = generate_arith_reg()
+	rega = generate_arith_reg()
+	regb = generate_arith_reg()
+	opsuffix = random.choice(COMPARE_OPS)
+	opstr = '\t\t' + 'cmp' + opsuffix + ' s' + str(dest) + ', '
+	opstr += typea + str(rega) + ', '
+	if random.randint(0, 1) == 0 and opsuffix[-2:] != '_f':
+		opstr += str(random.randint(0, 0x1ff))	# Immediate value
+	else:
+	 	opstr += typeb + str(regb)
+		
+	file.write(opstr + '\n')
+
 LOAD_OPS = [
 	('_32', 4),
 	('_s16', 2),
@@ -126,6 +179,51 @@ STORE_OPS = [
 	('_8', 1)
 ]
 
+def generate_memory_access(file):
+	opType = random.randint(0, 2)
+
+	# v0/s0 represent the shared segment, which is read only
+	# v1/s1 represent the private segment, which is read/write
+	ptrReg = random.randint(0, 1)
+
+	opstr = 'load' if ptrReg == 0 or random.randint(0, 1) else 'store'
+
+	if opType == 0:
+		# Block vector
+		offset = random.randint(0, 16) * 64
+		opstr += '_v v' + str(generate_arith_reg()) + ', ' + str(offset) + '(s' + str(ptrReg) + ')'
+	elif opType == 1:
+		# Scatter/gather
+		offset = random.randint(0, 16) * 4
+		if opstr == 'load':
+			opstr += '_gath'
+		else:
+			opstr += '_scat'
+
+		maskType = random.randint(0, 1)
+		if maskType == 1:
+			opstr += '_mask'
+
+		opstr += ' v' + str(generate_arith_reg()) 
+		if maskType != 0:
+			opstr += ', s' + str(generate_arith_reg())
+	
+		opstr += ', ' + str(offset) + '(v' + str(ptrReg) + ')'
+	else:
+		# Scalar
+		if opstr == 'load':
+			suffix, align = random.choice(LOAD_OPS)
+		else:
+			suffix, align = random.choice(STORE_OPS)
+
+		offset = random.randint(0, 16) * align
+		opstr += suffix + ' s' + str(generate_arith_reg()) + ', ' + str(offset) + '(s' + str(ptrReg) + ')'
+
+	file.write('\t\t' + opstr + '\n')
+
+def generate_device_io(file):
+	file.write('\t\tstore_32 s' + str(generate_arith_reg())  + ', (s9)\n')
+
 BRANCH_TYPES = [
 	('bfalse', True),
 	('btrue', True),
@@ -135,8 +233,28 @@ BRANCH_TYPES = [
 	('goto', False)
 ]
 
-ARITH_REG_LOW = 3
-ARITH_REG_HIGH = 8
+def generate_branch(file):
+	# Branch
+	branchType, isCond = random.choice(BRANCH_TYPES)
+	if isCond:
+		file.write('\t\t' + branchType + ' s' + str(generate_arith_reg()) + ', ' + str(random.randint(1, 6)) + 'f\n')
+	else:
+		file.write('\t\t' + branchType + ' ' + str(random.randint(1, 6)) + 'f\n')
+
+def generate_computed_pointer(file):
+	if random.randint(0, 1) == 0:
+		file.write('\t\tadd_i s1, s2, ' + str(random.randint(0, 16) * 64) + '\n')
+	else:
+		file.write('\t\tadd_i v1, v2, ' + str(random.randint(0, 16) * 64) + '\n')
+
+generate_funcs = [
+	(0.1, generate_computed_pointer),
+	(0.5, generate_binary_arith),
+	(0.1, generate_compare),
+	(0.2, generate_memory_access),
+#	(0.01, generate_device_io),
+	(1.0, generate_branch),
+]
 
 def generate_test(filename, numInstructions):
 	file = open(filename, 'w')
@@ -161,6 +279,8 @@ _start:			move s1, 15
 				; Zero out shared base registers
 				move v0, 0
 				move s0, 0
+				
+				load_32 s9, device_ptr
 
 				;;;  Fill private memory with a random pattern  ;;;;;;;;;;;
 				move s3, s2	; Base Address
@@ -208,7 +328,8 @@ branch_addrs: 	.long start_strand0, start_strand1, start_strand2, start_strand3
 fill_length: 	.long 0x1000 / 4
 generator_a: 	.long 1103515245
 generator_c: 	.long 12345
-	''')
+device_ptr:		.long 0xffff0004
+''')
 
 	for strand in range(4):
 		file.write('start_strand%d: ' % strand)
@@ -216,113 +337,13 @@ generator_c: 	.long 12345
 		for x in range(numInstructions):
 			file.write(str(labelIdx + 1) + ': ')
 			labelIdx = (labelIdx + 1) % 6
-		
-			if random.randint(0, 7) == 0:
-				# Computed pointer
-				if random.randint(0, 1) == 0:
-					file.write('\t\tadd_i s1, s2, ' + str(random.randint(0, 16) * 64) + '\n')
-				else:
-					file.write('\t\tadd_i v1, v2, ' + str(random.randint(0, 16) * 64) + '\n')
-				
-				continue
-
 			instType = random.random()
-			if instType < 0.5:
-				# Arithmetic
-				mnemonic = random.choice(BINARY_OPS)
-				if mnemonic == 'shuffle':
-					typed = 'v'
-					typea = 'v'
-					typeb = 'v'
-					suffix = '' if random.randint(0, 1) == 0 else '_mask'
-				elif mnemonic == 'getlane':
-					typed = 's'
-					typea = 'v'
-					typeb = 's' if random.randint(0, 1) == 0 else 'i'
-					suffix = ''
-				elif mnemonic[-2:] == '_f':
-					typed, typea, typeb, suffix = random.choice(FP_FORMS)
-				else:
-					typed, typea, typeb, suffix = random.choice(INT_FORMS)
-
-				dest = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				rega = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				regb = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				maskreg = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				opstr = '\t\t' + mnemonic + suffix + ' ' + typed + str(dest) + ', '
-				if suffix != '':
-					opstr += 's' + str(maskreg)	+ ', ' # Add mask register
-		
-				opstr += typea + str(rega) + ', '
-				if typeb == 'i':
-					opstr += str(random.randint(0, 0x1ff))	# Immediate value
-				else:
-				 	opstr += typeb + str(regb)
-
-				file.write(opstr + '\n')
-			elif instType < 0.6:
-				# Compare op
-				typea, typeb = random.choice(COMPARE_FORMS)
-				dest = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				rega = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				regb = random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)
-				opsuffix = random.choice(COMPARE_OPS)
-				opstr = '\t\t' + 'cmp' + opsuffix + ' s' + str(dest) + ', '
-				opstr += typea + str(rega) + ', '
-				if random.randint(0, 1) == 0 and opsuffix[-2:] != '_f':
-					opstr += str(random.randint(0, 0x1ff))	# Immediate value
-				else:
-				 	opstr += typeb + str(regb)
-					
-				file.write(opstr + '\n')
-			elif instType < 0.9:
-				# Memory
-				opType = random.randint(0, 2)
-
-				# v0/s0 represent the shared segment, which is read only
-				# v1/s1 represent the private segment, which is read/write
-				ptrReg = random.randint(0, 1)
-				opstr = 'load' if ptrReg == 0 or random.randint(0, 1) else 'store'
-			
-				if opType == 0:
-					# Block vector
-					offset = random.randint(0, 16) * 64
-					opstr += '_v v' + str(random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)) + ', ' + str(offset) + '(s' + str(ptrReg) + ')'
-				elif opType == 1:
-					# Scatter/gather
-					offset = random.randint(0, 16) * 4
-					if opstr == 'load':
-						opstr += '_gath'
-					else:
-						opstr += '_scat'
-
-					maskType = random.randint(0, 1)
-					if maskType == 1:
-						opstr += '_mask'
-	
-					opstr += ' v' + str(random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)) 
-					if maskType != 0:
-						opstr += ', s' + str(random.randint(ARITH_REG_LOW, ARITH_REG_HIGH))
-				
-					opstr += ', ' + str(offset) + '(v' + str(ptrReg) + ')'
-	 			else:
-					# Scalar
-					if opstr == 'load':
-						suffix, align = random.choice(LOAD_OPS)
-					else:
-						suffix, align = random.choice(STORE_OPS)
-
-					offset = random.randint(0, 16) * align
-					opstr += suffix + ' s' + str(random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)) + ', ' + str(offset) + '(s' + str(ptrReg) + ')'
-			
-				file.write('\t\t' + opstr + '\n')
-			else:
-				# Branch
-				branchType, isCond = random.choice(BRANCH_TYPES)
-				if isCond:
-					file.write('\t\t' + branchType + ' s' + str(random.randint(ARITH_REG_LOW, ARITH_REG_HIGH)) + ', ' + str(random.randint(1, 6)) + 'f\n')
-				else:
-					file.write('\t\t' + branchType + ' ' + str(random.randint(1, 6)) + 'f\n')
+			cumulProb = 0.0
+			for prob, func in generate_funcs:
+				cumulProb += prob
+				if instType < cumulProb:
+					func(file)
+					break
 			
 		file.write('''
 		1: nop
