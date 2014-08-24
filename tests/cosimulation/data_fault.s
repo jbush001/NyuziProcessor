bc3c9d4b6aa95e07cb6cd5f059c89defe782ff62
Jeff Bush <jeffbush001@gmail.com>
2014-08-24 16:33:21 -0700

    Add more fault types

--- tests/cosimulation/data_fault.s
+++ tests/cosimulation/data_fault.s
@@ -29,18 +29,28 @@
 			    .align	4
 			    .type	main,@function
 _start:		    lea s0, fault_handler
-			    setcr s0, 1				# Set fault handler address
+			    setcr s0, 1			# Set fault handler address
 			    lea s1, testvar1
 			    add_i s1, s1, 1
-			    load_32 s2, (s1)
-			    move s10, 1
-			    setcr s0, 29		; Halt
+			    load_32 s2, (s1)	# Invalid word alignment, load
+				move s10, 1
+			    store_32 s2, (s1)	# Invalid word alignment, store
+				move s10, 2
+			    load_s16 s2, (s1)	# Invalid short alignment, load
+				move s10, 3
+			    load_u16 s2, (s1)	# Invalid unsigned short alignment, load
+				move s10, 4
+			    store_16 s2, (s1)	# Invalid short alignment, store
+				move s10, 5
+				load_v v2, (s1)		# Invalid vector alignment, load
+				move s10, 6
+				store_v v2, (s1)	# Invalid vector alignment, store
+			    setcr s0, 29		# Halt
 1: 		        goto 1b
 
-fault_handler: 	getcr s11, 2
-				getcr s12, 3
-				setcr s0, 29		; Halt
-1: 			    goto 1b
+fault_handler: 	getcr s11, 2		# Fault address
+				getcr s12, 3		# Reason
+				add_i pc, s11, 4	# Jump back to next instruction
 
 			   .align 4
 testvar1: 	   .long 0x1234abcd, 0, 0, 0
