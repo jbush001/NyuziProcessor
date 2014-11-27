8cd2dac8fe76c4533c30270611fc873a580c978a
Jeff Bush <jeffbush001@gmail.com>
2014-11-27 11:49:09 -0800

    Extend vararg test to use a few more types

--- tests/compiler/vararg.cpp
+++ tests/compiler/vararg.cpp
@@ -17,25 +17,35 @@
 // 
 
 #include <stdio.h>
+#include <stdarg.h>
 
-void varArgFunc(int numParams, ...)
+void varArgFunc(int foo, ...)
 {
 	__builtin_va_list ap;
-	__builtin_va_start(ap, numParams);
-
-	for (int i = 0; i < numParams; i++)
-		printf("0x%08x", __builtin_va_arg(ap, int));
-	
+	__builtin_va_start(ap, foo);
+	printf("foo = 0x%08x\n", foo);
+	printf("0. 0x%08x\n", __builtin_va_arg(ap, int));
+	printf("1. 0x%08x\n", __builtin_va_arg(ap, unsigned int));
+	printf("2. 0x%08x\n", __builtin_va_arg(ap, short));
+	printf("3. 0x%08x\n", __builtin_va_arg(ap, unsigned short));
+	printf("4. 0x%08x\n", __builtin_va_arg(ap, char));
+	printf("5. 0x%08x\n", __builtin_va_arg(ap, unsigned char));
+	printf("6. %s\n", __builtin_va_arg(ap, const char*));
 	__builtin_va_end(ap);
 }
 
 int main()
 {
-	varArgFunc(4, 0xaaaaaaaa, 0xbbbbbbbb, 0xcccccccc, 0xdddddddd);
+	varArgFunc(0x3659487d, 0x4f5b256, 0xacb0f292, 0x1f4d9c8d, 0xd5fc06d3, 
+		0xb201c748, 0xb3f71cc2, "test string");
 
-	// CHECK: 0xaaaaaaaa
-	// CHECK: 0xbbbbbbbb
-	// CHECK: 0xcccccccc
-	// CHECK: 0xdddddddd
+	// CHECK: foo = 0x3659487d
+	// CHECK: 0. 0x04f5b256
+	// CHECK: 1. 0xacb0f292
+	// CHECK: 2. 0xffff9c8d
+	// CHECK: 3. 0x000006d3
+	// CHECK: 4. 0x00000048
+	// CHECK: 5. 0x000000c2
+	// CHECK: 6. test string
 }
  
