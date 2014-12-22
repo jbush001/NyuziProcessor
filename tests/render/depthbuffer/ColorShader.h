c7bc1e49ffe31d49921736ed5538053b5aad9429
Jeff Bush <jeffbush001@gmail.com>
2014-12-22 09:41:32 -0800

    render tests: add depth buffer test

---
+++ tests/render/depthbuffer/ColorShader.h
@@ -0,0 +1,76 @@
+// 
+// Copyright (C) 2011-2014 Jeff Bush
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+// 
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA  02110-1301, USA.
+// 
+
+
+#ifndef __COLOR_SHADER
+#define __COLOR_SHADER
+
+#define TOON_SHADING 0
+
+#include "VertexShader.h"
+#include "PixelShader.h"
+
+using namespace render;
+
+//
+// The Color shader interpolates vertex normals across the surface of the triangle
+// and computes the dot product at each pixel
+//
+class ColorVertexShader : public render::VertexShader
+{
+public:
+	ColorVertexShader()
+		:	VertexShader(7, 8)
+	{
+	}
+
+	void shadeVertices(vecf16_t *outParams, const vecf16_t *inAttribs, const void *_uniforms,
+        int mask) const override
+	{
+		// Position
+		outParams[kParamX] = inAttribs[0];
+		outParams[kParamY] = inAttribs[1];
+		outParams[kParamZ] = inAttribs[2];
+		outParams[kParamW] = splatf(1.0);
+
+		// Color
+		outParams[4] = inAttribs[3];
+		outParams[5] = inAttribs[4];
+		outParams[6] = inAttribs[5];
+		outParams[7] = inAttribs[6];
+	}
+};
+
+class ColorPixelShader : public render::PixelShader
+{
+public:
+	ColorPixelShader(render::RenderTarget *target)
+		:	PixelShader(target)
+	{
+	}
+	
+	virtual void shadePixels(const vecf16_t inParams[16], vecf16_t outColor[4],
+		const void *_uniforms, unsigned short mask) const override
+	{
+		for (int i = 0; i < 4; i++)
+			outColor[i] = inParams[i];
+	}
+};
+
+#endif
---
+++ tests/render/depthbuffer/main.cpp
@@ -0,0 +1,78 @@
+// 
+// Copyright (C) 2011-2014 Jeff Bush
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+// 
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA  02110-1301, USA.
+// 
+
+
+#include <math.h>
+#include <schedule.h>
+#include <stdlib.h>
+#include <Matrix.h>
+#include <Rasterizer.h>
+#include <RenderTarget.h>
+#include <RenderContext.h>
+#include "ColorShader.h"
+
+using namespace render;
+
+const int kFbWidth = 640;
+const int kFbHeight = 480;
+
+static float kTriangleVertices[] = {
+	// 1st triangle
+	 0.0, -0.9,  5.0,     1.0, 0.0, 0.0, 1.0, 
+	-0.9,  0.7,  1.0,     1.0, 0.0, 0.0, 1.0,  
+	 0.9,  0.7,  10.0,     1.0, 0.0, 0.0, 1.0, 
+
+	// 2nd triangle
+ 	 0.0,  0.9, 5.0,      0.0, 1.0, 0.0, 1.0,  
+ 	 0.9, -0.7, 1.0,      0.0, 1.0, 0.0, 1.0,  
+ 	-0.9, -0.7, 10.0,      0.0, 1.0, 0.0, 1.0,  
+};
+
+static int kTriangleIndices[] = { 0, 1, 2, 3, 4, 5 };
+
+void *operator new(size_t size, void *p)
+{
+	return p;
+}
+	
+int main()
+{
+	RenderTarget *renderTarget = new RenderTarget();
+	Surface *colorBuffer = new (memalign(64, sizeof(Surface))) Surface(kFbWidth, kFbHeight, (void*) 0x200000);
+	Surface *zBuffer = new (memalign(64, sizeof(Surface))) Surface(kFbWidth, kFbHeight);
+	renderTarget->setColorBuffer(colorBuffer);
+	renderTarget->setZBuffer(zBuffer);
+	RenderContext *context = new RenderContext(renderTarget);
+	
+	VertexShader *vertexShader = new (memalign(64, sizeof(ColorVertexShader))) ColorVertexShader();
+	PixelShader *pixelShader = new ColorPixelShader(renderTarget);
+	pixelShader->enableZBuffer(true);
+
+	context->bindShader(vertexShader, pixelShader);
+	context->bindUniforms(nullptr);
+
+	Matrix projectionMatrix = Matrix::getProjectionMatrix(kFbWidth, kFbHeight);
+	Matrix modelViewMatrix;
+	Matrix rotationMatrix;
+	context->bindGeometry(kTriangleVertices, 6, kTriangleIndices, 6);
+	context->renderFrame();
+	modelViewMatrix = modelViewMatrix * rotationMatrix;
+	
+	return 0;
+}
