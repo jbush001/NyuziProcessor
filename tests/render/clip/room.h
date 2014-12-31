6583450f5d99847fff4a99cc24c560b0d2ba03c5
Jeff Bush <jeffbush001@gmail.com>
2014-12-31 09:45:01 -0800

    clip test: Reorder vertices so all clip cases are hit

--- tests/render/clip/room.h
+++ tests/render/clip/room.h
@@ -58,17 +58,20 @@ const float kRoomVertices[] = {
 };
 
 const int kNumRoomIndices = 36;
+
+// The order of indices is specifically chosen to hit all
+// clip cases.
 const int kRoomIndices[] = {
-	0, 1, 2, 
-	2, 3, 0,
-	4, 5, 6,
-	6, 7, 4,
-	8, 9, 10,
-	10, 11, 8,
-	12, 13, 14,
-	14, 15, 12,
-	16, 17, 18,
-	18, 19, 16,
-	20, 21, 22,
-	22, 23, 20
+	0, 1, 2, 		// clip mask 6
+	2, 3, 0,		// clip mask 1
+	4, 5, 6,		// clip mask 0 (completely visible)
+	6, 7, 4,		// clip mask 0
+	8, 9, 10,		// clip mask 7 (completely hidden)
+	10, 11, 8,		// clip mask 7
+	12, 13, 14,		// clip mask 6
+	14, 15, 12,		// clip mask 1
+	17, 18, 16,		// clip mask 5
+	19, 16,	18,		// clip mask 2
+	20, 21, 22,		// clip mask 4
+	22, 23, 20		// clip mask 3
 };
