9e16659c36a5f45806bb618142460ace0779a594
Jeff Bush <jeffbush001@gmail.com>
2015-01-27 08:29:53 -0800

    librender: simplify lookat

--- software/librender/Matrix.h
+++ software/librender/Matrix.h
@@ -239,13 +239,13 @@ public:
 		Vec3 y = x.crossProduct(z).normalized();
 
 		const float cameraValues[4][4] = {
-			{ x[0], x[1], x[2], 0 },
-			{ y[0], y[1], y[2], 0 },
-			{ -z[0], -z[1], -z[2], 0 },
+			{ x[0], x[1], x[2], -location[0] },
+			{ y[0], y[1], y[2], -location[1] },
+			{ -z[0], -z[1], -z[2], -location[2] },
 			{ 0, 0, 0, 1 }
 		};
 
-		return Matrix(cameraValues) * getTranslationMatrix(-location[0], -location[1], -location[2]);		
+		return Matrix(cameraValues);		
 	}
 
 private:
