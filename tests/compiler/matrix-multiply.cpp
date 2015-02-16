89b94184b76c63a71427fabd6b7ef2baa4f7ca4f
Jeff Bush <jeffbush001@gmail.com>
2015-02-16 08:29:25 -0800

    Better shuffle test

---
+++ tests/compiler/matrix-multiply.cpp
@@ -0,0 +1,75 @@
+// 
+// Copyright (C) 2011-2014 Jeff Bush
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// 
+
+// This tests vector addition, multiplication, and shuffles
+
+#include <stdint.h>
+#include <stdio.h>
+
+const veci16_t kShuffleA = { 0, 0, 0, 0, 4, 4, 4, 4, 8, 8, 8, 8, 12, 12, 12, 12 };
+const veci16_t kShuffleB = { 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 };
+const veci16_t kShuffleC = { 1, 1, 1, 1, 5, 5, 5, 5, 9, 9, 9, 9, 13, 13, 13, 13 };
+const veci16_t kShuffleD = { 4, 5, 6, 7, 4, 5, 6, 7, 4, 5, 6, 7, 4, 5, 6, 7 };
+const veci16_t kShuffleE = { 2, 2, 2, 2, 6, 6, 6, 6, 10, 10, 10, 10, 14, 14, 14, 14 };
+const veci16_t kShuffleF = { 8, 9, 10, 11, 8, 9, 10, 11, 8, 9, 10, 11, 8, 9, 10, 11 };
+const veci16_t kShuffleG = { 3, 3, 3, 3, 7, 7, 7, 7, 11, 11, 11, 11, 15, 15, 15, 15 };
+const veci16_t kShuffleH = { 12, 13, 14, 15, 12, 13, 14, 15, 12, 13, 14, 15, 12, 13, 14, 15 };
+
+vecf16_t multiplyMatrix(vecf16_t mat1, vecf16_t mat2)
+{
+	vecf16_t result = __builtin_nyuzi_shufflef(mat1, kShuffleA) * __builtin_nyuzi_shufflef(mat2, kShuffleB);
+	result += __builtin_nyuzi_shufflef(mat1, kShuffleC) * __builtin_nyuzi_shufflef(mat2, kShuffleD);
+	result += __builtin_nyuzi_shufflef(mat1, kShuffleE) * __builtin_nyuzi_shufflef(mat2, kShuffleF);
+	result += __builtin_nyuzi_shufflef(mat1, kShuffleG) * __builtin_nyuzi_shufflef(mat2, kShuffleH);
+	return result;
+}
+
+void printMatrix(vecf16_t value)
+{
+	for (int row = 0; row < 4; row++)
+	{
+		for (int col = 0; col < 4; col++)
+			printf("%g ", value[row * 4 + col]);
+
+		printf("\n");
+	}
+}
+
+const vecf16_t kTestMat1 = { 
+	1, 2, 7, 3,
+	0, 3, -1, 1,
+	3, 4, 2, -1,
+	-2, 0, 9, 2
+};
+
+const vecf16_t kTestMat2 = { 
+	3, 6, -2, 4,
+	0, 3, 3, 1,
+	7, 3, 1, 4,
+	0, -3, 3, 2
+};
+
+int main()
+{
+	printMatrix(multiplyMatrix(kTestMat1, kTestMat2));
+
+	// CHECK: 52.0 24.0 20.0 40.0
+	// CHECK: -7.0 3.0 11.0 1.0
+	// CHECK: 23.0 39.0 5.0 22.0
+	// CHECK: 57.0 9.0 19.0 32.0
+}
