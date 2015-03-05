893ecd8e42616e8bd76988f5c4605f4ff931adf5
Jeff Bush <jeffbush001@gmail.com>
2015-03-05 08:00:42 -0800

    Add Dhrystone benchmark

---
+++ benchmarks/dhrystone/runtest.sh
@@ -0,0 +1,34 @@
+# 
+# Copyright (C) 2011-2014 Jeff Bush
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# 
+
+COMPILER_DIR=/usr/local/llvm-nyuzi/bin
+VERILATOR=../../bin/verilator_model
+CC=$COMPILER_DIR/clang
+ELF2HEX=$COMPILER_DIR/elf2hex
+HEXFILE=WORK/program.hex
+ELFFILE=WORK/program.elf
+
+mkdir -p WORK
+
+# Build
+$CC -O3 -o $ELFFILE dry.c ../../software/libc/crt0.o ../../software/libc/libc.a
+$ELF2HEX -o $HEXFILE $ELFFILE
+
+# Run, collect results
+echo "running"
+$VERILATOR +bin=WORK/program.hex
