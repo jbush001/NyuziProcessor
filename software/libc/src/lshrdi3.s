f722dc46e6734d61e428232462a0ee1a2d40d671
Jeff Bush <jeffbush001@gmail.com>
2015-03-15 08:32:29 -0700

    Switch to a more permissive license

--- software/libc/src/lshrdi3.s
+++ software/libc/src/lshrdi3.s
@@ -1,21 +1,19 @@
 # 
-# Copyright (C) 2011-2014 Jeff Bush
+# Copyright 2011-2015 Jeff Bush
 # 
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 # 
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 # 
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-# Boston, MA  02110-1301, USA.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # 
+
 
 #
 # Logical shift right a 64 bit integer. The param to be shifted is in [s1, s0]
