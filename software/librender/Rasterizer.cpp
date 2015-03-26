ad394a9bb7c62549bc4a86de8eba182ed7c464e3
Jeff Bush <jeffbush001@gmail.com>
2015-03-25 22:28:08 -0700

    librender: Use more immutable variables in rasterizer

--- software/librender/Rasterizer.cpp
+++ software/librender/Rasterizer.cpp
@@ -96,45 +96,34 @@ void setupEdge(int tileLeft, int tileTop, int x1, int y1,
 	outRejectStepMatrix = xRejectStepValues - yRejectStepValues;
 }
 
+// Workhorse of rasterization.  Recursively subdivides tile into 4x4 grids.
 void subdivideTile( 
 	ShaderFiller &filler,
-	int acceptCornerValue1, 
-	int acceptCornerValue2, 
-	int acceptCornerValue3,
-	int rejectCornerValue1, 
-	int rejectCornerValue2,
-	int rejectCornerValue3,
-	veci16_t acceptStep1, 
-	veci16_t acceptStep2, 
-	veci16_t acceptStep3, 
-	veci16_t rejectStep1, 
-	veci16_t rejectStep2, 
-	veci16_t rejectStep3, 
-	int tileSize,
-	int tileLeft,
-	int tileTop,
-	int clipRight,
-	int clipBottom)
+	const int acceptCornerValue1, 
+	const int acceptCornerValue2, 
+	const int acceptCornerValue3,
+	const int rejectCornerValue1, 
+	const int rejectCornerValue2,
+	const int rejectCornerValue3,
+	const veci16_t acceptStep1, 
+	const veci16_t acceptStep2, 
+	const veci16_t acceptStep3, 
+	const veci16_t rejectStep1, 
+	const veci16_t rejectStep2, 
+	const veci16_t rejectStep3, 
+	const int tileSize,
+	const int tileLeft,
+	const int tileTop,
+	const int clipRight,
+	const int clipBottom)
 {
-	veci16_t acceptEdgeValue1;
-	veci16_t acceptEdgeValue2;
-	veci16_t acceptEdgeValue3;
-	veci16_t rejectEdgeValue1;
-	veci16_t rejectEdgeValue2;
-	veci16_t rejectEdgeValue3;
-	int trivialAcceptMask;
-	int trivialRejectMask;
-	int recurseMask;
-	int index;
-	int x, y;
-
 	// Compute accept masks
-	acceptEdgeValue1 = acceptStep1 + splati(acceptCornerValue1);
-	trivialAcceptMask = __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue1, splati(0));
-	acceptEdgeValue2 = acceptStep2 + splati(acceptCornerValue2);
-	trivialAcceptMask &= __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue2, splati(0));
-	acceptEdgeValue3 = acceptStep3 + splati(acceptCornerValue3);
-	trivialAcceptMask &= __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue3, splati(0));
+	const veci16_t acceptEdgeValue1 = acceptStep1 + splati(acceptCornerValue1);
+	const veci16_t acceptEdgeValue2 = acceptStep2 + splati(acceptCornerValue2);
+	const veci16_t acceptEdgeValue3 = acceptStep3 + splati(acceptCornerValue3);
+	const int trivialAcceptMask = __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue1, splati(0))
+		& __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue2, splati(0))
+		& __builtin_nyuzi_mask_cmpi_sle(acceptEdgeValue3, splati(0));
 
 	if (tileSize == 4)
 	{
@@ -143,23 +132,21 @@ void subdivideTile(
 		return;
 	}
 
-	// Reduce tile size for sub blocks
-	tileSize = tileSize / 4;
+	const int subTileSize = tileSize / 4;
 
 	// Process all trivially accepted blocks
 	if (trivialAcceptMask != 0)
 	{
-		int index;
 		int currentMask = trivialAcceptMask;
 	
 		while (currentMask)
 		{
-			index = __builtin_clz(currentMask) - 16;
+			const int index = __builtin_clz(currentMask) - 16;
 			currentMask &= ~(0x8000 >> index);
-			int subTileLeft = tileLeft + tileSize * (index & 3);
-			int subTileTop = tileTop + tileSize * (index >> 2);
-			int right = min(tileSize, clipRight - subTileLeft);
-			int bottom = min(tileSize, clipBottom - subTileTop);
+			const int subTileLeft = tileLeft + subTileSize * (index & 3);
+			const int subTileTop = tileTop + subTileSize * (index >> 2);
+			const int right = min(subTileSize, clipRight - subTileLeft);
+			const int bottom = min(subTileSize, clipBottom - subTileTop);
 			for (int y = 0; y < bottom; y += 4)
 			{
 				for (int x = 0; x < right; x += 4)
@@ -169,32 +156,32 @@ void subdivideTile(
 	}
 
 	// Compute reject masks
-	rejectEdgeValue1 = rejectStep1 + splati(rejectCornerValue1);
-	trivialRejectMask = __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue1, splati(0));
-	rejectEdgeValue2 = rejectStep2 + splati(rejectCornerValue2);
-	trivialRejectMask |= __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue2, splati(0));
-	rejectEdgeValue3 = rejectStep3 + splati(rejectCornerValue3);
-	trivialRejectMask |= __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue3, splati(0));
+	const veci16_t rejectEdgeValue1 = rejectStep1 + splati(rejectCornerValue1);
+	const veci16_t rejectEdgeValue2 = rejectStep2 + splati(rejectCornerValue2);
+	const veci16_t rejectEdgeValue3 = rejectStep3 + splati(rejectCornerValue3);
+	const int trivialRejectMask = __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue1, splati(0))
+		| __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue2, splati(0))
+		| __builtin_nyuzi_mask_cmpi_sgt(rejectEdgeValue3, splati(0));
 
-	recurseMask = (trivialAcceptMask | trivialRejectMask) ^ 0xffff;
+	int recurseMask = (trivialAcceptMask | trivialRejectMask) ^ 0xffff;
 	if (recurseMask)
 	{
 		// Divide each step matrix by 4
-		acceptStep1 = acceptStep1 >> splati(2);	
-		acceptStep2 = acceptStep2 >> splati(2);
-		acceptStep3 = acceptStep3 >> splati(2);
-		rejectStep1 = rejectStep1 >> splati(2);
-		rejectStep2 = rejectStep2 >> splati(2);
-		rejectStep3 = rejectStep3 >> splati(2);
+		const veci16_t subAcceptStep1 = acceptStep1 >> splati(2);	
+		const veci16_t subAcceptStep2 = acceptStep2 >> splati(2);
+		const veci16_t subAcceptStep3 = acceptStep3 >> splati(2);
+		const veci16_t subRejectStep1 = rejectStep1 >> splati(2);
+		const veci16_t subRejectStep2 = rejectStep2 >> splati(2);
+		const veci16_t subRejectStep3 = rejectStep3 >> splati(2);
 
 		// Recurse into blocks that are neither trivially rejected or accepted.
 		// They are partially overlapped and need to be further subdivided.
 		while (recurseMask)
 		{
-			index = __builtin_clz(recurseMask) - 16;
+			const int index = __builtin_clz(recurseMask) - 16;
 			recurseMask &= ~(0x8000 >> index);
-			x = tileLeft + tileSize * (index & 3);
-			y = tileTop + tileSize * (index >> 2);
+			const int x = tileLeft + subTileSize * (index & 3);
+			const int y = tileTop + subTileSize * (index >> 2);
 			if (x >= clipRight || y >= clipBottom)
 				continue;	// Clip tiles that are outside viewport
 
@@ -206,13 +193,13 @@ void subdivideTile(
 				rejectEdgeValue1[index],
 				rejectEdgeValue2[index],
 				rejectEdgeValue3[index],
-				acceptStep1,
-				acceptStep2,
-				acceptStep3,
-				rejectStep1,
-				rejectStep2,
-				rejectStep3,
-				tileSize,
+				subAcceptStep1,
+				subAcceptStep2,
+				subAcceptStep3,
+				subRejectStep1,
+				subRejectStep2,
+				subRejectStep3,
+				subTileSize,
 				x, 
 				y,
 				clipRight,
