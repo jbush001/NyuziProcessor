b34de53b40c831a1f3c54399e5c5f2ebf243cc59
Jeff Bush <jeffbush001@gmail.com>
2015-04-19 10:50:49 -0700

    render tests: add basic triangle test

---
+++ tests/render/triangle/ColorShader.h
@@ -0,0 +1,57 @@
+// 
+// Copyright 2011-2015 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+
+#pragma once
+
+#include <Shader.h>
+
+using namespace librender;
+
+class ColorShader : public Shader
+{
+public:
+	ColorShader()
+		:	Shader(7, 8)
+	{
+	}
+
+	void shadeVertices(vecf16_t outParams[], const vecf16_t inAttribs[], const void *,
+        int) const override
+	{
+		// Position
+		outParams[kParamX] = inAttribs[0];
+		outParams[kParamY] = inAttribs[1];
+		outParams[kParamZ] = inAttribs[2];
+		outParams[kParamW] = splatf(1.0);
+
+		// Color
+		outParams[4] = inAttribs[3];
+		outParams[5] = inAttribs[4];
+		outParams[6] = inAttribs[5];
+		outParams[7] = inAttribs[6];
+	}
+
+	void shadePixels(vecf16_t outColor[4], const vecf16_t inParams[16], 
+		const void *, const Texture * const [kMaxTextures],
+		unsigned short) const override
+	{
+		outColor[0] = inParams[0];
+		outColor[1] = inParams[1];
+		outColor[2] = inParams[2];
+		outColor[3] = inParams[3];
+	}
+};
---
+++ tests/render/triangle/main.cpp
@@ -0,0 +1,65 @@
+// 
+// Copyright 2011-2015 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+
+//
+// Validates alpha blending
+//
+
+#include <math.h>
+#include <schedule.h>
+#include <stdlib.h>
+#include <Matrix.h>
+#include <RenderTarget.h>
+#include <RenderContext.h>
+#include "ColorShader.h"
+
+using namespace librender;
+
+const int kFbWidth = 640;
+const int kFbHeight = 480;
+
+static float kTriangleVertices[] = {
+	// Position            Color (RGBA)
+	 0.0,  -0.9, -1.0,     1.0, 0.0, 0.0, 1.0,
+	 0.9, 0.9, -1.0,       0.0, 1.0, 0.0, 1.0,
+	 -0.9, 0.9, -1.0,      0.0, 0.0, 1.0, 1.0,
+};
+
+static int kTriangleIndices[] = { 0, 1, 2 };
+
+// All threads start execution here.
+int main()
+{
+	if (__builtin_nyuzi_read_control_reg(0) != 0)
+		workerThread();
+
+	// Start worker threads
+	__builtin_nyuzi_write_control_reg(30, 0xffffffff);
+
+	const RenderBuffer vertexBuffer(kTriangleVertices, 3, 7 * sizeof(float));
+	const RenderBuffer indexBuffer(kTriangleIndices, 3, 4);
+	RenderContext *context = new RenderContext();
+	RenderTarget *renderTarget = new RenderTarget();
+	Surface *colorBuffer = new Surface(kFbWidth, kFbHeight, (void*) 0x200000);
+	renderTarget->setColorBuffer(colorBuffer);
+	context->bindTarget(renderTarget);
+	context->bindShader(new ColorShader());
+	context->bindGeometry(&vertexBuffer, &indexBuffer);
+	context->submitDrawCommand();
+	context->finish();
+	return 0;
+}
