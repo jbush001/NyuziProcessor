2fb663c28998f8bb35b2cbbb7ee4516f940e2ff3
Jeff Bush <jeffbush001@gmail.com>
2015-05-18 10:54:53 -0700

    quakeview: remove debug code

--- software/apps/quakeview/main.cpp
+++ software/apps/quakeview/main.cpp
@@ -25,87 +25,6 @@
 
 using namespace librender;
 
-//#define DISPLAY_ATLAS 1
-
-namespace 
-{
-
-void makeWrappingTexture(PakFile *file, int textureId, RenderBuffer &vertices, RenderBuffer &indices)
-{
-	float left;
-	float bottom;
-	float width;
-	float height;
-	
-	if (textureId < 0)
-	{
-		left = 0;
-		bottom = 0;
-		width = 1;
-		height = 1;
-	}
-	else
-		file->getTextureLocation(textureId, left, bottom, width, height);
-	
-	printf("atlas entry is %g %g %g %g\n", left, bottom, width, height);
-	
-	float *vdata = new float[kTotalAttrs * 4];
-	float *vptr = vdata;
-	*vptr++ = -1.0;
-	*vptr++ = 1.0;
-	*vptr++ = -1.0;
-	*vptr++ = left;
-	*vptr++ = bottom;
-	*vptr++ = width;
-	*vptr++ = height;
-	*vptr++ = 0.0;
-	*vptr++ = 4.0;
-
-	*vptr++ = -1.0;
-	*vptr++ = -1.0;
-	*vptr++ = -1.0;
-	*vptr++ = left;
-	*vptr++ = bottom;
-	*vptr++ = width;
-	*vptr++ = height;
-	*vptr++ = 0.0;
-	*vptr++ = 0.0;
-
-	*vptr++ = 1.0;
-	*vptr++ = -1.0;
-	*vptr++ = -1.0;
-	*vptr++ = left;
-	*vptr++ = bottom;
-	*vptr++ = width;
-	*vptr++ = height;
-	*vptr++ = 4.0;
-	*vptr++ = 0.0;
-
-	*vptr++ = 1.0;
-	*vptr++ = 1.0;
-	*vptr++ = -1.0;
-	*vptr++ = left;
-	*vptr++ = bottom;
-	*vptr++ = width;
-	*vptr++ = height;
-	*vptr++ = 4.0;
-	*vptr++ = 4.0;
-
-	vertices.setData(vdata, 4, kTotalAttrs * sizeof(float));
-	
-	int *idata = new int[6];
-	idata[0] = 0;
-	idata[1] = 1;
-	idata[2] = 2;
-	idata[3] = 2;
-	idata[4] = 3;
-	idata[5] = 0;
-	
-	indices.setData(idata, 6, sizeof(int));
-}
-
-}
-
 RenderBspNode *findNode(RenderBspNode *head, float x, float y, float z)
 {
 	RenderBspNode *node = head;
@@ -131,7 +50,6 @@ public:
 			fNumLeaves(numLeaves),
 			fCurrentLeaf(0)
 	{
-		printf("index %d numLeaves %d\n", index, numLeaves);
 	}
 	
 	int nextNode()
@@ -211,25 +129,13 @@ int main()
 	for (int frame = 0; ; frame++)
 	{
 		RenderBspNode *currentNode = findNode(root, cameraPos[0], cameraPos[1], cameraPos[2]);
-		printf("currentNode is %d PVS index %d\n", currentNode->leafIndex, currentNode->pvsIndex);
 		PvsIterator visible(pak.getPvsList(), currentNode->pvsIndex, pak.getNumLeaves());
 		
-#if DISPLAY_ATLAS	
-		context->bindUniforms(&uniforms, sizeof(uniforms));
-#else	
 		Matrix modelViewMatrix = Matrix::lookAt(cameraPos, cameraPos + Vec3(cos(frame * 3.14 / 10), 
 			sin(frame * 3.14 / 10), 0), Vec3(0, 0, 1));
 		uniforms.fMVPMatrix = projectionMatrix * modelViewMatrix;
 		context->bindUniforms(&uniforms, sizeof(uniforms));
-#endif
 
-#if DISPLAY_ATLAS
-		RenderBuffer vertices;
-		RenderBuffer indices;
-		makeWrappingTexture(&pak, frame, vertices, indices);
-		context->bindGeometry(&vertices, &indices);
-		context->submitDrawCommand();
-#else
 		int renderLeaf;
 		while ((renderLeaf = visible.nextNode()) != -1)
 		{
@@ -239,10 +145,10 @@ int main()
 			context->bindGeometry(vertexBuf, indexBuf);
 			context->submitDrawCommand();
 		}
-#endif
+
 		int startInstructions = __builtin_nyuzi_read_control_reg(6);
 		context->finish();
-		printf("rendered frame in %d instructions\n", __builtin_nyuzi_read_control_reg(6) 
+		printf("rendered frame in %d instructions.\n", __builtin_nyuzi_read_control_reg(6) 
 			- startInstructions);
 	}
 }
