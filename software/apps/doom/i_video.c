ab3208a125b7abf17553ebfc5b7ac050ad7cb7b2
Jeff Bush <jeffbush001@gmail.com>
2015-05-18 16:28:41 -0700

    move keyboard driver into libos

--- software/apps/doom/i_video.c
+++ software/apps/doom/i_video.c
@@ -25,6 +25,7 @@ static const char
 rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
 
 #include <stdint.h>
+#include <keyboard.h>
 #include "doomstat.h"
 #include "i_system.h"
 #include "v_video.h"
@@ -34,8 +35,6 @@ rcsid[] = "$Id: i_x.c,v 1.6 1997/02/03 22:45:10 b1 Exp $";
 #include "doomdef.h"
 
 unsigned int gPalette[256];
-
-static volatile unsigned int * const REGISTERS = (volatile unsigned int*) 0xffff0000;
 
 void I_ShutdownGraphics(void)
 {
@@ -52,66 +51,92 @@ void I_GetEvent(void)
 {
 }
 
-// PS/2 scancodes, set 2
-const unsigned char kNormalScancodeTable[] = {
-	0, KEY_F9, 0, KEY_F5, KEY_F3, KEY_F1, KEY_F2, KEY_F12, 0, KEY_F10, KEY_F8, KEY_F6, KEY_F4, KEY_TAB, 
-	'`', 0, 0, KEY_LALT, KEY_LSHIFT, 0, 0, 'q', '1', 0, 0, 0, 'z', 's', 'a', 'w', '2', 0, 0, 'c', 'x', 
-	'd', 'e', '4', '3', 0, 0, ' ', 'v', 'f', 't', 'r', '5', 0, 0, 'n', 'b', 'h', 'g', 'y', '6', 0, 0, 0, 
-	'm', 'j', 'u', '7', '8', 0, 0, 0, 'k', 'i', 'o', '0', '9', 0, 0, 0, 0, 'l', 0, 'p', KEY_MINUS, 0, 
-	0, 0, 0, 0, 0, '=', 0, 0, 0, KEY_RSHIFT, KEY_ENTER, 0, 0, '\\', 0, 0, 0, 0, 0, 0, 0, 0, KEY_BACKSPACE, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, KEY_ESCAPE, 0, KEY_F11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	KEY_F7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
-
-const unsigned char kExtendedScancodeTable[] = {
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, KEY_RALT, 0, 0, KEY_RCTRL, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, KEY_LEFTARROW, 0, 0, 0, 
-	0, 0, 0, KEY_DOWNARROW, 0, KEY_RIGHTARROW, KEY_UPARROW, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0	
-};
-
-static int isExtendedCode = 0;
-static int isRelease = 0;
-
 //
 // I_StartTic
 //
 void I_StartTic (void)
 {
 	// Read keyboard
-	while (REGISTERS[0x38 / 4])
+	unsigned int code = pollKeyboard();
+	if (code != 0xffffffff)
 	{
-		unsigned int code = REGISTERS[0x3c / 4];
-		if (code == 0xe0)
-			isExtendedCode = 1;
-		else if (code == 0xf0)
-			isRelease = 1;
-		else
+		event_t event;
+		
+		switch (code & 0xffff)
 		{
-			event_t event;
-			if (isExtendedCode)
-				event.data1 = kExtendedScancodeTable[code];
-			else
-				event.data1 = kNormalScancodeTable[code];
-
-			if (isRelease)
-				event.type = ev_keyup;
-			else
-				event.type = ev_keydown;
-			
-			D_PostEvent(&event);
-			isExtendedCode = 0;
-			isRelease = 0;
+			case KBD_F1:
+				event.data1 = KEY_F1;
+				break;
+			case KBD_F2:
+				event.data1 = KEY_F2;
+				break;
+			case KBD_F3:
+				event.data1 = KEY_F3;
+				break;
+			case KBD_F4:
+				event.data1 = KEY_F4;
+				break;
+			case KBD_F5:
+				event.data1 = KEY_F5;
+				break;
+			case KBD_F6:
+				event.data1 = KEY_F6;
+				break;
+			case KBD_F7:
+				event.data1 = KEY_F7;
+				break;
+			case KBD_F8:
+				event.data1 = KEY_F8;
+				break;
+			case KBD_F9:
+				event.data1 = KEY_F9;
+				break;
+			case KBD_RIGHTARROW:
+				event.data1 = KEY_RIGHTARROW;
+				break;
+			case KBD_LEFTARROW:
+				event.data1 = KEY_LEFTARROW;
+				break;
+			case KBD_UPARROW:
+				event.data1 = KEY_UPARROW;
+				break;
+			case KBD_DOWNARROW:
+				event.data1 = KEY_DOWNARROW;
+				break;
+			case KBD_RSHIFT:
+				event.data1 = KEY_RSHIFT;
+				break;
+			case KBD_LSHIFT:
+				event.data1 = KEY_LSHIFT;
+				break;
+			case KBD_RALT:
+				event.data1 = KEY_RALT;
+				break;
+			case KBD_LALT:
+				event.data1 = KEY_LALT;
+				break;
+			case '\x08':
+				event.data1 = KEY_BACKSPACE;
+				break;
+			case '\x27':
+				event.data1 = KEY_ESCAPE;
+				break;
+			case '\n':
+				event.data1 = KEY_ENTER;
+				break;
+			case '\t':
+				event.data1 = KEY_TAB;
+				break;
+			default:
+				event.data1 = code & 0xff;
 		}
+
+		if (code & KBD_PRESSED)
+			event.type = ev_keydown;
+		else
+			event.type = ev_keyup;
+			
+		D_PostEvent(&event);		
 	}
 }
 
@@ -124,6 +149,7 @@ void I_UpdateNoBlit (void)
 	// what is this?
 }
 
+static volatile unsigned int * const REGISTERS = (volatile unsigned int*) 0xffff0000;
 static unsigned int lastCycleCount = 0;
 static unsigned int lastTimeUs = 0;
 static int frameCount = 0;
---
+++ software/libs/libos/keyboard.h
@@ -0,0 +1,55 @@
+// 
+// Copyright 2015 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KBD_PRESSED 0x00080000
+	
+#define KBD_F1 128
+#define KBD_F2 129
+#define KBD_F3 130
+#define KBD_F4 131
+#define KBD_F5 132
+#define KBD_F6 133
+#define KBD_F7 134
+#define KBD_F8 135
+#define KBD_F9 136
+#define KBD_F10 137
+#define KBD_F11 138
+#define KBD_F12 139
+#define KBD_RIGHTARROW 140
+#define KBD_LEFTARROW 141
+#define KBD_UPARROW 142
+#define KBD_DOWNARROW 143
+#define KBD_RSHIFT 144
+#define KBD_LSHIFT 145
+#define KBD_RALT 146
+#define KBD_LALT 147
+#define KBD_RCTRL 148
+#define KBD_LCTRL 149
+
+// Return -1 if no key is pressed
+unsigned int pollKeyboard();
+
+
+#ifdef __cplusplus
+}
+#endif
