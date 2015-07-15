aa2e2b0a0da56e4c2a3d63e21e764f702563253d
Pipat Methavanitpong <pipat1010@gmail.com>
2015-07-15 12:03:38 +0900

    Add uart test program (OE only)

---
+++ tests/misc/uart/uart.c
@@ -0,0 +1,80 @@
+// 
+// Copyright 2011-2015 Jeff Bush
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+// UART Rx Overrun Error behavior test
+// An external uart has its Tx connected to Rx itself. 
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+static volatile unsigned int * const REGISTERS = (volatile unsigned int*) 0xffff0000;
+#define REG_UART_EXT_STATUS	0x118 / 4
+#define REG_UART_EXT_RX		0x11c / 4
+#define REG_UART_EXT_TX		0x120 / 4
+
+void writeUartExt(char ch)
+{
+	while ((REGISTERS[REG_UART_EXT_STATUS] & 0x5) == 0)
+		;	// Wait for space
+	
+	REGISTERS[REG_UART_EXT_TX] = ch;	
+}
+
+unsigned char readUartExt()
+{
+	while ((REGISTERS[REG_UART_EXT_STATUS] & 0x6) == 0)
+		;	// Wait for space
+	
+	return REGISTERS[REG_UART_EXT_RX];	
+}
+
+void flushUartExtRx()
+{
+	char c;
+	while(REGISTERS[REG_UART_EXT_STATUS] & 0x6)
+	{
+		c = REGISTERS[REG_UART_EXT_RX];
+	}
+}
+
+int main()
+{
+	const char* word[2] = { "AWESOME", "A quick brown fox jumps over the lazy dog."};
+	int word_len[2] = { strlen(word[0]), strlen(word[1]) };
+	bool isOverrunError = false;
+
+	for (int i = 0; i < 2; i++)
+	{
+		printf("Sending \"%s\" (len=%d) to an external uart\n", word[i], word_len[i]);
+		for (int j = 0; j < word_len[i]; j++)
+			writeUartExt(word[i][j]);
+		
+		isOverrunError = (REGISTERS[REG_UART_EXT_STATUS] & 0x1) ? true : false;
+		if (word_len[i] < 16 && isOverrunError)
+		{
+			printf("Overrun Error ... FAIL\n");
+			return 1;
+		}
+		printf("Overrun Error ... PASS\n\n");
+		flushUartExtRx();
+	}
+
+	printf("END\n");
+
+	return 0;
+}
