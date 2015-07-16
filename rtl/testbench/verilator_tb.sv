jbush001/NyuziProcessor

commit ec3314520bfdc2a21083499e94d680175f8064a7

rtl: fix testbench bug where serial status address was wrong
This was masked because when an unknown address was passed,
0xffffffff was returned. I modified that to return a random
value to flush out similar bugs in the future.

rtl/testbench/verilator_tb.sv
@@ -367,7 +367,7 @@ module verilator_tb(
 		if (io_read_en)
 		begin
 			case (io_address)
-				'h16: io_read_data <= 1;	// Serial status 
+				'h18: io_read_data <= 1;	// Serial status 
 				'h38,
 				'h3c: io_read_data <= ps2_read_data;
 				
@@ -378,7 +378,7 @@ module verilator_tb(
 					io_read_data <= spi_read_data;
 				end
 				
-				default: io_read_data <= 32'hffffffff;
+				default: io_read_data <= $random();
 			endcase
 		end
 

commit a65296d5322d100a251f89b705bc404019967ff7

Fix cosimulation failure for generated random instrutions
Introduced by ec3314520bfdc2a21083499e94d680175f8064a7, which randomized
the value returned for unknown register reads. Cosimulation was relying
on this returning 'hffffffff.

rtl/testbench/verilator_tb.sv
@@ -366,7 +366,10 @@ module verilator_tb(
 
 		if (io_read_en)
 		begin
+			$display("io_read %x\n", io_address);
 			case (io_address)
+				'h4,
+				'h8: io_read_data <= 32'hffffffff;	// Hack for cosimulation tests
 				'h18: io_read_data <= 1;	// Serial status 
 				'h38,
 				'h3c: io_read_data <= ps2_read_data;