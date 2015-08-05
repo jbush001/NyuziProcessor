18c7abdc9f9b5bfcd24853aeb5e6367c0c6dc320
Jeff Bush <jeffbush001@gmail.com>
2015-08-05 08:45:17 -0700

    Fix indents

--- software/apps/plasma/plasma.cpp
+++ software/apps/plasma/plasma.cpp
@@ -30,18 +30,18 @@ const int kNumThreads = 4;
 
 inline vecf16_t splatf(float f)
 {
-    return __builtin_nyuzi_makevectorf(f);
+	return __builtin_nyuzi_makevectorf(f);
 }
 
 inline veci16_t splati(unsigned int i)
 {
-    return __builtin_nyuzi_makevectori(i);
+	return __builtin_nyuzi_makevectori(i);
 }
 
 vecf16_t fmodv(vecf16_t val1, vecf16_t val2)
 {
-    veci16_t whole = __builtin_convertvector(val1 / val2, veci16_t);
-    return val1 - __builtin_convertvector(whole, vecf16_t) * val2;
+	veci16_t whole = __builtin_convertvector(val1 / val2, veci16_t);
+	return val1 - __builtin_convertvector(whole, vecf16_t) * val2;
 }
 
 //
@@ -52,58 +52,58 @@ vecf16_t fmodv(vecf16_t val1, vecf16_t val2)
 const int kNumTerms = 6;
 
 const double denominators[] = {
-    -0.166666666666667f,  // 1 / 3!
-    0.008333333333333f,   // 1 / 5!
-    -0.000198412698413f,  // 1 / 7!
-    0.000002755731922f,	  // 1 / 9!
-    -2.50521084e-8f,      // 1 / 11!
-    1.6059044e-10f        // 1 / 13!
+	-0.166666666666667f,  // 1 / 3!
+	0.008333333333333f,   // 1 / 5!
+	-0.000198412698413f,  // 1 / 7!
+	0.000002755731922f,	  // 1 / 9!
+	-2.50521084e-8f,      // 1 / 11!
+	1.6059044e-10f        // 1 / 13!
 };
 
 vecf16_t fsinv(vecf16_t angle)
 {
-    // The approximation begins to diverge past 0-pi/2. To prevent
-    // discontinuities, mirror or flip this function for each portion of
+	// The approximation begins to diverge past 0-pi/2. To prevent
+	// discontinuities, mirror or flip this function for each portion of
 	// the result
-    angle = fmodv(angle, splatf(M_PI * 2));
+	angle = fmodv(angle, splatf(M_PI * 2));
 
-    int resultSign = __builtin_nyuzi_mask_cmpf_lt(angle, splatf(0.0));
+	int resultSign = __builtin_nyuzi_mask_cmpf_lt(angle, splatf(0.0));
 
-    angle = ((veci16_t)angle) & splati(0x7fffffff);	// fabs
+	angle = ((veci16_t)angle) & splati(0x7fffffff);	// fabs
 
-    int cmp1 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI * 3 / 2));
-    angle = __builtin_nyuzi_vector_mixf(cmp1, splatf(M_PI * 2) - angle, angle);
-    resultSign ^= cmp1;
+	int cmp1 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI * 3 / 2));
+	angle = __builtin_nyuzi_vector_mixf(cmp1, splatf(M_PI * 2) - angle, angle);
+	resultSign ^= cmp1;
 
-    int cmp2 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI));
-    int mask2 = cmp2 & ~cmp1;
-    angle = __builtin_nyuzi_vector_mixf(mask2, angle - splatf(M_PI), angle);
-    resultSign ^= mask2;
+	int cmp2 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI));
+	int mask2 = cmp2 & ~cmp1;
+	angle = __builtin_nyuzi_vector_mixf(mask2, angle - splatf(M_PI), angle);
+	resultSign ^= mask2;
 
-    int cmp3 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI / 2));
+	int cmp3 = __builtin_nyuzi_mask_cmpf_gt(angle, splatf(M_PI / 2));
 	int mask3 = cmp3 & ~(cmp1 | cmp2);
-    angle = __builtin_nyuzi_vector_mixf(mask3, splatf(M_PI) - angle, angle);
+	angle = __builtin_nyuzi_vector_mixf(mask3, splatf(M_PI) - angle, angle);
 
-    vecf16_t angleSquared = angle * angle;
-    vecf16_t numerator = angle;
-    vecf16_t result = angle;
+	vecf16_t angleSquared = angle * angle;
+	vecf16_t numerator = angle;
+	vecf16_t result = angle;
 
-    for (int i = 0; i < kNumTerms; i++)
-    {
-        numerator *= angleSquared;
-        result += numerator * splatf(denominators[i]);
-    }
+	for (int i = 0; i < kNumTerms; i++)
+	{
+		numerator *= angleSquared;
+		result += numerator * splatf(denominators[i]);
+	}
 
-    return __builtin_nyuzi_vector_mixf(resultSign, -result, result);
+	return __builtin_nyuzi_vector_mixf(resultSign, -result, result);
 }
 
 inline vecf16_t sqrtfv(vecf16_t value)
 {
-    vecf16_t guess = value;
-    for (int iteration = 0; iteration < 6; iteration++)
-        guess = ((value / guess) + guess) / __builtin_nyuzi_makevectorf(2.0f);
+	vecf16_t guess = value;
+	for (int iteration = 0; iteration < 6; iteration++)
+		guess = ((value / guess) + guess) / __builtin_nyuzi_makevectorf(2.0f);
 
-    return guess;
+	return guess;
 }
 
 volatile int gFrameNum = 0;
@@ -113,44 +113,44 @@ Barrier<4> gFrameBarrier;
 // All threads start here
 int main()
 {
-    __builtin_nyuzi_write_control_reg(30, 0xffffffff); // Start all threads
+	__builtin_nyuzi_write_control_reg(30, 0xffffffff); // Start all threads
 
-    int myThreadId = __builtin_nyuzi_read_control_reg(0);
-    for (;;)
-    {
-        for (int y = myThreadId; y < kScreenHeight; y += kNumThreads)
-        {
-            veci16_t *ptr = kFrameBufferAddress + y * kScreenWidth / 16;
-            for (int x = 0; x < kScreenWidth; x += 16)
-            {
-                vecf16_t xv = (splatf((float) x) + kXOffsets) / splatf(kScreenWidth / 10);
-                vecf16_t yv = splatf((float) y) / splatf(kScreenHeight / 10);
-                vecf16_t tv = splatf((float) gFrameNum / 15);
+	int myThreadId = __builtin_nyuzi_read_control_reg(0);
+	for (;;)
+	{
+		for (int y = myThreadId; y < kScreenHeight; y += kNumThreads)
+		{
+			veci16_t *ptr = kFrameBufferAddress + y * kScreenWidth / 16;
+			for (int x = 0; x < kScreenWidth; x += 16)
+			{
+				vecf16_t xv = (splatf((float) x) + kXOffsets) / splatf(kScreenWidth / 10);
+				vecf16_t yv = splatf((float) y) / splatf(kScreenHeight / 10);
+				vecf16_t tv = splatf((float) gFrameNum / 15);
 
-                vecf16_t fintensity = splatf(0.0);
-                fintensity += fsinv(xv);
-                fintensity += fsinv(yv + tv * splatf(0.5));
-                fintensity += fsinv((xv + yv * splatf(0.3) - tv) * splatf(0.5));
-                fintensity += fsinv(sqrtfv(xv * xv + yv * yv + splatf(1.0)) + tv);
+				vecf16_t fintensity = splatf(0.0);
+				fintensity += fsinv(xv);
+				fintensity += fsinv(yv + tv * splatf(0.5));
+				fintensity += fsinv((xv + yv * splatf(0.3) - tv) * splatf(0.5));
+				fintensity += fsinv(sqrtfv(xv * xv + yv * yv + splatf(1.0)) + tv);
 
 				vecf16_t redf = fsinv(fintensity * splatf(M_PI));
 				vecf16_t greenf = fsinv(fintensity * splatf(M_PI) + splatf(M_PI));
-               	veci16_t redi =  __builtin_convertvector(redf * splatf(60) + splatf(128),
-                                 veci16_t);
+				veci16_t redi =  __builtin_convertvector(redf * splatf(60) + splatf(128),
+				                 veci16_t);
 				veci16_t greeni =  __builtin_convertvector(greenf * splatf(68) + splatf(128),
-                                   veci16_t);
+				                   veci16_t);
 
-                *ptr = (redi << __builtin_nyuzi_makevectori(8)) | greeni;
-                asm("dflush %0" : : "s" (ptr));
-                ptr++;
-            }
-        }
+				*ptr = (redi << __builtin_nyuzi_makevectori(8)) | greeni;
+				asm("dflush %0" : : "s" (ptr));
+				ptr++;
+			}
+		}
 
-        if (myThreadId == 0)
-            __sync_fetch_and_add(&gFrameNum, 1);
+		if (myThreadId == 0)
+			__sync_fetch_and_add(&gFrameNum, 1);
 
-        gFrameBarrier.wait();
-    }
+		gFrameBarrier.wait();
+	}
 
-    return 0;
+	return 0;
 }
