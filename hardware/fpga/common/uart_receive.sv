jbush001/NyuziProcessor

commit aa66e0ca61e5840c3c226d628602c983e386308b

uart: simplify receive logic
While talking with Pipat, I realized having both a bad divisor and sample
count was redundant. Just use one counter.

I've repurposed Pipat's overrun test as a general UART test to validate
this change, adding a few additional checks for correct data.

hardware/fpga/common/uart_receive.sv
@@ -34,17 +34,15 @@ module uart_receive
 
 	receive_state_t state_ff;
 	receive_state_t state_nxt;
-	logic[3:0] sample_count_ff;
-	logic[3:0] sample_count_nxt;
+	logic[11:0] sample_count_ff;
+	logic[11:0] sample_count_nxt;
 	logic[7:0] shift_register;	
 	logic[3:0] bit_count_ff;
 	logic[3:0] bit_count_nxt;
 	logic do_shift;
-	logic[10:0] clock_divider;
 	logic rx_sync;
 	logic sample_enable;
 
-	assign sample_enable = clock_divider == 0;
 	assign rx_char = shift_register;
 
 	synchronizer #(.RESET_STATE(1)) rx_synchronizer(
@@ -67,7 +65,11 @@ module uart_receive
 				if (!rx_sync)
 				begin
 					state_nxt = STATE_READ_CHARACTER;
-					sample_count_nxt = 11;	// Scan to middle of first bit
+					
+					// We are at the beginning of the start bit. Next
+					// sample point is in middle of first data bit.
+					// Set divider to 1.5 times bit duration.
+					sample_count_nxt = (BAUD_DIVIDE * 3 / 2) - 1;
 				end
 			end
 
@@ -75,7 +77,7 @@ module uart_receive
 			begin
 				if (sample_count_ff == 0)
 				begin
-					sample_count_nxt = 7;
+					sample_count_nxt = BAUD_DIVIDE - 1;
 					if (bit_count_ff == 8)
 					begin
 						state_nxt = STATE_WAIT_START;
@@ -101,10 +103,9 @@ module uart_receive
 			state_ff <= STATE_WAIT_START;
 			/*AUTORESET*/
 			// Beginning of autoreset for uninitialized flops
-			bit_count_ff <= 4'h0;
-			clock_divider <= 11'h0;
-			sample_count_ff <= 4'h0;
-			shift_register <= 8'h0;
+			bit_count_ff <= '0;
+			sample_count_ff <= '0;
+			shift_register <= '0;
 			// End of automatics
 		end
 		else
@@ -113,19 +114,13 @@ module uart_receive
 			sample_count_ff <= sample_count_nxt;
 			bit_count_ff <= bit_count_nxt;
 			if (do_shift)
-			begin
 				shift_register <= { rx_sync, shift_register[7:1] };
-			end
-
-			if (clock_divider == 0)
-				clock_divider <= BAUD_DIVIDE - 1;
-			else
-				clock_divider <= clock_divider - 1;
 		end
 	end
 endmodule
 
 // Local Variables:
 // verilog-typedef-regexp:"_t$"
+// verilog-auto-reset-widths:unbased
 // End:
 

tests/misc/uart/uart.c
@@ -1,0 +1,98 @@
+// 
+// Copyright 2015 Pipat Methavanitpong
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+/// Check UART Overrun bit to assert and deassert properly
+/// Recommend to turn on UART Overrun print in
+///     hardware/fpga/common/uart.sv
+/// to check contents in its FIFO
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define CHECK(cond) do { if (!(cond)) { printf("FAIL: %d: %s\n", __LINE__, \
+	#cond); abort(); } } while(0)
+
+const int kMaxTimeout = 1000000;
+const int kMaxFifoDepth = 7;
+volatile unsigned int * const LOOPBACK_UART = (volatile unsigned int*) 0xFFFF0100;
+
+enum UartRegs
+{
+	kStatus = 0,
+	kRx = 1,
+	kTx = 2
+};
+
+
+void writeLoopbackUart(char ch) 
+{
+	int timeout;
+	LOOPBACK_UART[kTx] = ch;
+	while ((LOOPBACK_UART[kStatus] & 1) == 0)	// Wait for transmit to finish
+		CHECK(++timeout < kMaxTimeout);
+
+	printf("write %02x\n", ch);
+}
+
+int readLoopbackUart(void)
+{
+	char result;
+	int timeout = 0;
+	while ((LOOPBACK_UART[kStatus] & 2) == 0)
+		CHECK(++timeout < kMaxTimeout);
+	
+	result = LOOPBACK_UART[kRx];
+	printf("read %02x\n", result);
+	return result;
+}
+
+int main () 
+{
+	int fifoCount;
+	int i;
+	char txChar = 1;
+	char rxChar = 1;
+	int readCount;
+	
+	for (fifoCount = 1; fifoCount < kMaxFifoDepth + 3; fifoCount++)
+	{
+		for (i = 0; i < fifoCount; i++)
+		{
+			writeLoopbackUart(txChar++);
+			
+			// Ensure the overrun bit is set if we've filled the FIFO,
+			// not set if we have not
+			if (i >= kMaxFifoDepth)
+				CHECK((LOOPBACK_UART[kStatus] & 4) != 0);
+			else
+				CHECK((LOOPBACK_UART[kStatus] & 4) == 0);
+		}
+		
+		// Indicate dropped characters
+		if (fifoCount > kMaxFifoDepth)
+			rxChar += fifoCount - kMaxFifoDepth;
+		
+		readCount = fifoCount;
+		if (readCount > kMaxFifoDepth)
+			readCount = kMaxFifoDepth;
+		
+		for (i = 0; i < readCount; i++)
+			CHECK(readLoopbackUart() == rxChar++);
+	}
+
+	printf("PASS\n");
+	return 0;
+}

hardware/fpga/common/uart.sv
@@ -76,25 +76,25 @@ module uart
 	
 	assign tx_enable = io_write_en && io_address == TX_REG;
 
-	uart_transmit #(.BAUD_DIVIDE(BAUD_DIVIDE * 8)) uart_transmit(
+	uart_transmit #(.BAUD_DIVIDE(BAUD_DIVIDE)) uart_transmit(
 		.tx_char(io_write_data[7:0]),
 		/*AUTOINST*/
-		// Outputs
-		.tx_ready		(tx_ready),
-		.uart_tx		(uart_tx),
-		// Inputs
-		.clk			(clk),
-		.reset			(reset),
-		.tx_enable		(tx_enable));
+								 // Outputs
+								 .tx_ready		(tx_ready),
+								 .uart_tx		(uart_tx),
+								 // Inputs
+								 .clk			(clk),
+								 .reset			(reset),
+								 .tx_enable		(tx_enable));
 
 	uart_receive #(.BAUD_DIVIDE(BAUD_DIVIDE)) uart_receive(/*AUTOINST*/
-		// Outputs
-		.rx_char		(rx_char[7:0]),
-		.rx_char_valid	(rx_char_valid),
-		// Inputs
-		.clk			(clk),
-		.reset			(reset),
-		.uart_rx		(uart_rx));
+							       // Outputs
+							       .rx_char		(rx_char[7:0]),
+							       .rx_char_valid	(rx_char_valid),
+							       // Inputs
+							       .clk		(clk),
+							       .reset		(reset),
+							       .uart_rx		(uart_rx));
 						     
 	// XXX detect and flag uart_rx overflow
 	assign rx_fifo_read = io_address == RX_REG && io_read_en;

hardware/testbench/verilator_tb.sv
@@ -152,7 +152,7 @@ module verilator_tb(
 
 	logic[31:0] loopback_uart_read_data;
 	wire loopback_uart_line;
-	uart #(.BASE_ADDRESS('h100)) loopback_uart(
+	uart #(.BASE_ADDRESS('h100), .BAUD_DIVIDE(8)) loopback_uart(
 		.io_read_data(loopback_uart_read_data),
 		.uart_tx(loopback_uart_line),
 		.uart_rx(loopback_uart_line),

tests/misc/uart/runtest.py
@@ -20,9 +20,10 @@ import sys
 sys.path.insert(0, '../..')
 import test_harness
 
-test_harness.compile_test('overrun.c')
+test_harness.compile_test('uart.c')
 result = test_harness.run_verilator()
 if result.find('PASS') == -1:
+	print result
 	print 'FAIL'
 	sys.exit(1)
 else:

tests/misc/uart_overrun/overrun.c
@@ -1,75 +1,0 @@
-// 
-// Copyright 2015 Pipat Methavanitpong
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
-
-/// Check UART Overrun bit to assert and deassert properly
-/// Recommend to turn on UART Overrun print in
-///     hardware/fpga/common/uart.sv
-/// to check contents in its FIFO
-
-#include <stdio.h>
-#include <unistd.h>
-
-volatile unsigned int * const LOOPBACK_UART = (volatile unsigned int*) 0xFFFF0100;
-
-enum UartRegs
-{
-	kStatus = 0,
-	kRx = 1,
-	kTx = 2
-};
-
-void writeLoopbackUart(char ch) {
-	while ((LOOPBACK_UART[kStatus] & 1) == 0)	// Wait for ready
-		;
-	
-	LOOPBACK_UART[kTx] = ch;
-}
-
-// A UART has 7-character capacity
-const char * text = "abcdefghij";
-const int text_len = 10;
-
-int main () {
-	printf("====UART Overrun Test====\n");
-	printf("String: %s\n", text);
-	int i, j, k;
-	for (i = 1; i <= 7; i++) {
-		// Fill Rx FIFO until full and overrun
-		while ((LOOPBACK_UART[kStatus] & 4) == 0) {
-			for (j = 0; j < text_len; j++) {
-				writeLoopbackUart(text[j]);
-			}
-		}
-		// Dequeue for i characters
-		printf("%dDequeue: ", i);
-		for (k = 1; k <= i; k++) {
-			while ((LOOPBACK_UART[kStatus] & 2) == 0)
-				;
-			printf("%c", LOOPBACK_UART[kRx]);
-		}
-		printf("\n");
-
-		// Check if Overrun bit is deasserted
-		if ((LOOPBACK_UART[kStatus] & 4) != 0)
-		{
-			printf("%d:FAIL\n", i);
-			exit(1);
-		}
-	}
-
-	printf("PASS\n");
-	return 0;
-}

commit b5f56e4f8d08a39bb8604084d8c1bc80133b2eba

uart: fix regression introduced by aa66e0ca61e5840c3c226d628602c983e386308b
Disused sample_enable signal was still enabling sample_count decrement
Also fixed unitilized timeout in UART test harness and make timeout smaller

hardware/fpga/common/uart_receive.sv
@@ -41,7 +41,6 @@ module uart_receive
 	logic[3:0] bit_count_nxt;
 	logic do_shift;
 	logic rx_sync;
-	logic sample_enable;
 
 	assign rx_char = shift_register;
 
@@ -90,7 +89,7 @@ module uart_receive
 						bit_count_nxt = bit_count_ff + 1;
 					end
 				end
-				else if (sample_enable)
+				else
 					sample_count_nxt = sample_count_ff - 1;
 			end
 		endcase

tests/misc/uart/uart.c
@@ -25,7 +25,7 @@
 #define CHECK(cond) do { if (!(cond)) { printf("FAIL: %d: %s\n", __LINE__, \
 	#cond); abort(); } } while(0)
 
-const int kMaxTimeout = 1000000;
+const int kMaxTimeout = 10000;
 const int kMaxFifoDepth = 7;
 volatile unsigned int * const LOOPBACK_UART = (volatile unsigned int*) 0xFFFF0100;
 
@@ -39,7 +39,7 @@ enum UartRegs
 
 void writeLoopbackUart(char ch) 
 {
-	int timeout;
+	int timeout = 0;
 	LOOPBACK_UART[kTx] = ch;
 	while ((LOOPBACK_UART[kStatus] & 1) == 0)	// Wait for transmit to finish
 		CHECK(++timeout < kMaxTimeout);
@@ -81,7 +81,7 @@ int main ()
 				CHECK((LOOPBACK_UART[kStatus] & 4) == 0);
 		}
 		
-		// Indicate dropped characters
+		// Account for dropped characters
 		if (fifoCount > kMaxFifoDepth)
 			rxChar += fifoCount - kMaxFifoDepth;
 		