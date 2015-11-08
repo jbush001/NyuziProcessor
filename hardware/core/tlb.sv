jbush001/NyuziProcessor

commit c2b464788c1fae054fbafe3dbc91fc5c96ed4d1e

hardware: cleanup
- Constrain and make explicit contract for the value of SRAM output when
read_en is not asserted. It now is undefined. I assign X to it in this
case, which Verilator will randomize. This should catch if hardware is using
the value without having asserted read_en.
- Use case statement for way generation in cache_lru
- Rename variables in control_registers to be more consistent

hardware/core/tlb.sv
@@ -105,8 +105,10 @@ module tlb
 				begin
 					if (way_update_oh[way_idx] && tlb_read_en && update_set_idx == request_set_idx)
 						way_valid <= update_valid;  // Bypass
-					else
+					else if (lookup_en)
 						way_valid <= entry_valid[request_set_idx];
+					else
+						way_valid <= 0;
 						
 					if (invalidate_all_en)
 					begin

hardware/core/cache_lru.sv
@@ -114,80 +114,86 @@ module cache_lru
 		.*);
 	
 	generate
-		if (NUM_WAYS == 1)
-		begin
-			assign fill_way = 0;
-			assign update_flags = 0;
-		end
-		else if (NUM_WAYS == 2)
-		begin
-			assign fill_way = !lru_flags[0];
-			assign update_flags[0] = !new_mru;
-		end
-		else if (NUM_WAYS == 4)
-		begin
-			always_comb
+		case (NUM_WAYS)
+			1:
 			begin
-				casez (lru_flags)
-					3'b00?: fill_way = 0;
-					3'b10?: fill_way = 1;
-					3'b?10: fill_way = 2;
-					3'b?11: fill_way = 3;
-					default: fill_way = '0;
-				endcase
+				assign fill_way = 0;
+				assign update_flags = 0;
 			end
 
-			always_comb
+			2:
 			begin
-				case (new_mru)
-					2'd0: update_flags = { 2'b11, lru_flags[0] };
-					2'd1: update_flags = { 2'b01, lru_flags[0] };
-					2'd2: update_flags = { lru_flags[2], 2'b01 };
-					2'd3: update_flags = { lru_flags[2], 2'b00 };
-					default: update_flags = '0;
-				endcase
-			end
-		end
-		else if (NUM_WAYS == 8)
-		begin
-			always_comb
-			begin
-				casez (lru_flags)
-					7'b00?0???: fill_way = 0;
-					7'b10?0???: fill_way = 1;
-					7'b?100???: fill_way = 2;
-					7'b?110???: fill_way = 3;
-					7'b???100?: fill_way = 4;
-					7'b???110?: fill_way = 5;
-					7'b???1?10: fill_way = 6;
-					7'b???1?11: fill_way = 7;
-					default: fill_way = '0;
-				endcase
+				assign fill_way = !lru_flags[0];
+				assign update_flags[0] = !new_mru;
 			end
 
-			always_comb
+			4:
 			begin
-				case (new_mru)
-					3'd0: update_flags = { 2'b11, lru_flags[5], 1'b1, lru_flags[2:0] };
-					3'd1: update_flags = { 2'b01, lru_flags[5], 1'b1, lru_flags[2:0] };
-					3'd2: update_flags = { lru_flags[6], 3'b011, lru_flags[2:0] };
-					3'd3: update_flags = { lru_flags[6], 3'b001, lru_flags[2:0] };
-					3'd4: update_flags = { lru_flags[6:4], 3'b011, lru_flags[0] };
-					3'd5: update_flags = { lru_flags[6:4], 3'b010, lru_flags[0] };
-					3'd6: update_flags = { lru_flags[6:4], 2'b00, lru_flags[1], 1'b1 }; 
-					3'd7: update_flags = { lru_flags[6:4], 2'b00, lru_flags[1], 1'b0 };
-					default: update_flags = '0;
-				endcase
+				always_comb
+				begin
+					casez (lru_flags)
+						3'b00?: fill_way = 0;
+						3'b10?: fill_way = 1;
+						3'b?10: fill_way = 2;
+						3'b?11: fill_way = 3;
+						default: fill_way = '0;
+					endcase
+				end
+
+				always_comb
+				begin
+					case (new_mru)
+						2'd0: update_flags = { 2'b11, lru_flags[0] };
+						2'd1: update_flags = { 2'b01, lru_flags[0] };
+						2'd2: update_flags = { lru_flags[2], 2'b01 };
+						2'd3: update_flags = { lru_flags[2], 2'b00 };
+						default: update_flags = '0;
+					endcase
+				end
 			end
-		end
-		else
-		begin
-			initial
+
+			8:
 			begin
-				$display("%m invalid number of ways");
-				$finish;
+				always_comb
+				begin
+					casez (lru_flags)
+						7'b00?0???: fill_way = 0;
+						7'b10?0???: fill_way = 1;
+						7'b?100???: fill_way = 2;
+						7'b?110???: fill_way = 3;
+						7'b???100?: fill_way = 4;
+						7'b???110?: fill_way = 5;
+						7'b???1?10: fill_way = 6;
+						7'b???1?11: fill_way = 7;
+						default: fill_way = '0;
+					endcase
+				end
+
+				always_comb
+				begin
+					case (new_mru)
+						3'd0: update_flags = { 2'b11, lru_flags[5], 1'b1, lru_flags[2:0] };
+						3'd1: update_flags = { 2'b01, lru_flags[5], 1'b1, lru_flags[2:0] };
+						3'd2: update_flags = { lru_flags[6], 3'b011, lru_flags[2:0] };
+						3'd3: update_flags = { lru_flags[6], 3'b001, lru_flags[2:0] };
+						3'd4: update_flags = { lru_flags[6:4], 3'b011, lru_flags[0] };
+						3'd5: update_flags = { lru_flags[6:4], 3'b010, lru_flags[0] };
+						3'd6: update_flags = { lru_flags[6:4], 2'b00, lru_flags[1], 1'b1 }; 
+						3'd7: update_flags = { lru_flags[6:4], 2'b00, lru_flags[1], 1'b0 };
+						default: update_flags = '0;
+					endcase
+				end
 			end
-		end
+
+			default:
+			begin
+				initial
+				begin
+					$display("%m invalid number of ways");
+					$finish;
+				end
+			end
+		endcase
 	endgenerate
 
 	always_ff @(posedge clk, posedge reset)

hardware/core/control_registers.sv
@@ -34,6 +34,15 @@ module control_registers
 	input                                   ix_is_eret,
 	input thread_idx_t                      ix_thread_idx,
 	
+	// From dcache_data_stage 
+	// dd_xxx signals are unregistered. dt_thread_idx represents thread going into
+	// dcache_data_stage)
+	input thread_idx_t                      dt_thread_idx,
+	input                                   dd_creg_write_en,
+	input                                   dd_creg_read_en,
+	input control_register_t                dd_creg_index,
+	input scalar_t                          dd_creg_write_val,
+
 	// From writeback_stage
 	input                                   wb_fault,
 	input fault_reason_t                    wb_fault_reason,
@@ -41,15 +50,6 @@ module control_registers
 	input scalar_t                          wb_fault_access_vaddr,
 	input thread_idx_t                      wb_fault_thread_idx,
 	input subcycle_t                        wb_fault_subcycle,
-	
-	// From dcache_data_stage 
-	// dd_XXX signals are unregistered. dt_thread_idx represents thread going into
-	// dcache_data_stage)
-	input thread_idx_t                      dt_thread_idx,
-	input                                   dd_creg_write_en,
-	input                                   dd_creg_read_en,
-	input control_register_t                dd_creg_index,
-	input scalar_t                          dd_creg_write_val,
 
 	// To writeback_stage
 	output scalar_t                         cr_creg_read_val,
@@ -60,10 +60,10 @@ module control_registers
 	
 	scalar_t fault_access_addr[`THREADS_PER_CORE];
 	fault_reason_t fault_reason[`THREADS_PER_CORE];
-	logic prev_int_flag[`THREADS_PER_CORE];
-	logic prev_mmu_enable[`THREADS_PER_CORE];
-	scalar_t cycle_count;
+	logic interrupt_en_saved[`THREADS_PER_CORE];
+	logic mmu_en_saved[`THREADS_PER_CORE];
 	scalar_t scratchpad[`THREADS_PER_CORE * 2];
+	scalar_t cycle_count;
 
 	always_ff @(posedge clk, posedge reset)
 	begin
@@ -73,10 +73,10 @@ module control_registers
 			begin
 				fault_reason[i] <= FR_RESET;
 				cr_eret_address[i] <= 0;
-				prev_int_flag[i] <= 0;
+				interrupt_en_saved[i] <= 0;
 				fault_access_addr[i] <= '0;
 				cr_mmu_en[i] <= 0;
-				prev_mmu_enable[i] <= 0;
+				mmu_en_saved[i] <= 0;
 				cr_eret_subcycle[i] <= 0;
 			end
 
@@ -113,15 +113,15 @@ module control_registers
 					cr_mmu_en[wb_fault_thread_idx] <= 0;
 
 				// Copy current flags to prev flags
-				prev_int_flag[wb_fault_thread_idx] <= cr_interrupt_en[wb_fault_thread_idx];
-				prev_mmu_enable[wb_fault_thread_idx] <= cr_mmu_en[wb_fault_thread_idx];
+				interrupt_en_saved[wb_fault_thread_idx] <= cr_interrupt_en[wb_fault_thread_idx];
+				mmu_en_saved[wb_fault_thread_idx] <= cr_mmu_en[wb_fault_thread_idx];
 				cr_eret_subcycle[wb_fault_thread_idx] <= wb_fault_subcycle;
 			end
 			else if (ix_is_eret)
 			begin	
 				// Copy from prev flags to current flags
-				cr_interrupt_en[ix_thread_idx] <= prev_int_flag[ix_thread_idx];	
-				cr_mmu_en[ix_thread_idx] <= prev_mmu_enable[ix_thread_idx];
+				cr_interrupt_en[ix_thread_idx] <= interrupt_en_saved[ix_thread_idx];	
+				cr_mmu_en[ix_thread_idx] <= mmu_en_saved[ix_thread_idx];
 			end
 
 			//
@@ -138,8 +138,8 @@ module control_registers
 
 					CR_SAVED_FLAGS:
 					begin
-						prev_mmu_enable[dt_thread_idx] <= dd_creg_write_val[1];
-						prev_int_flag[dt_thread_idx] <= dd_creg_write_val[0];
+						mmu_en_saved[dt_thread_idx] <= dd_creg_write_val[1];
+						interrupt_en_saved[dt_thread_idx] <= dd_creg_write_val[0];
 					end
 
 					CR_FAULT_PC:         cr_eret_address[dt_thread_idx] <= dd_creg_write_val;
@@ -161,7 +161,7 @@ module control_registers
 				case (dd_creg_index)
 					CR_FLAGS:
 					begin
-						cr_creg_read_val <= scalar_t'({ 
+						cr_creg_read_val <= scalar_t'({
 							cr_mmu_en[dt_thread_idx],
 							cr_interrupt_en[dt_thread_idx] 
 						});
@@ -169,13 +169,13 @@ module control_registers
 
 					CR_SAVED_FLAGS:
 					begin
-						cr_creg_read_val <= scalar_t'({ 
-							prev_mmu_enable[dt_thread_idx],
-							prev_int_flag[dt_thread_idx]
+						cr_creg_read_val <= scalar_t'({
+							mmu_en_saved[dt_thread_idx],
+							interrupt_en_saved[dt_thread_idx]
 						});
 					end
 
-					CR_THREAD_ID:        cr_creg_read_val <= scalar_t'({ CORE_ID, dt_thread_idx });
+					CR_THREAD_ID:        cr_creg_read_val <= scalar_t'({CORE_ID, dt_thread_idx});
 					CR_FAULT_PC:         cr_creg_read_val <= cr_eret_address[dt_thread_idx];
 					CR_FAULT_REASON:     cr_creg_read_val <= scalar_t'(fault_reason[dt_thread_idx]);
 					CR_FAULT_HANDLER:    cr_creg_read_val <= cr_fault_handler;

hardware/core/l2_cache_interface.sv
@@ -348,7 +348,8 @@ module l2_cache_interface
 		else
 		begin
 			// Make sure more than one snoop way isn't a hit
-			assert($onehot0(snoop_hit_way_oh));
+			assert(!response_stage2.valid || response_stage2.cache_type != CT_DCACHE 
+				|| $onehot0(snoop_hit_way_oh));
 
 			// Ensure only one dequeue type is set
 			assert(!sq_dequeue_ready || $onehot0({sq_dequeue_flush, sq_dequeue_iinvalidate, 

hardware/core/l2_cache_read.sv
@@ -236,7 +236,7 @@ module l2_cache_read(
 			assert(!l2t_is_l2_fill || !cache_hit);
 			
 			// Make sure there isn't a hit on more than one way
-			assert($onehot0(hit_way_oh));
+			assert(!l2t_request.valid || $onehot0(hit_way_oh));
 
 			l2r_request <= l2t_request;
 			l2r_cache_hit <= cache_hit;

hardware/core/nyuzi.sv
@@ -38,11 +38,14 @@ module nyuzi
 	input scalar_t        io_read_data);
 
 	l2req_packet_t l2i_request[`NUM_CORES];
-	l2rsp_packet_t l2_response;
 	ioreq_packet_t io_request[`NUM_CORES];
-	iorsp_packet_t ia_response;
 	logic[`TOTAL_THREADS - 1:0] ny_thread_enable;
 	logic[`TOTAL_PERF_EVENTS - 1:0] perf_events;
+
+	// XXX AUTOLOGIC not generating these
+	l2rsp_packet_t l2_response;
+	iorsp_packet_t ia_response;
+
 	
 	/*AUTOLOGIC*/
 	// Beginning of automatic wires (for undeclared instantiated-module outputs)

hardware/core/sram_1r1w.sv
@@ -18,14 +18,14 @@
 
 //
 // Block SRAM with 1 read port and 1 write port. 
-// Reads and writes are performed synchronously, with the value for a read 
-// appearing on the next clock edge after the address is asserted. 
-// The READ_DURING_WRITE parameter determine what happens if a read and a write 
-// are performed to the same address in the same cycle:
-//  "NEW_DATA" this will return the newly written data ("read-after-write").
-//  "DONT_CARE" The results are undefined. This can be used to improve clock 
-//  speed.
-//
+// Reads and writes are performed synchronously. The read value appears 
+// on the next clock edge after the address and readx_en are asserted
+// If read_en is not asserted, the value of read_data is undefined during
+// the next cycle. The READ_DURING_WRITE parameter determines what happens 
+// if a read and a write are performed to the same address in the same cycle:
+//  - "NEW_DATA" this will return the newly written data ("read-after-write").
+//  - "DONT_CARE" The results are undefined. This can be used to improve clock 
+//    speed.
 // This does not clear memory contents on reset.
 //
 
@@ -114,6 +114,8 @@ module sram_1r1w
 		end
 		else if (read_en)
 			read_data <= data[read_addr];
+		else
+			read_data <= {DATA_WIDTH{1'bx}};
 	end
 
 	initial

hardware/core/sram_2r1w.sv
@@ -16,14 +16,14 @@
 
 //
 // Block SRAM with 2 read ports and 1 write port. 
-// Reads and writes are performed synchronously, with the value for a read 
-// appearing on the next clock edge after the address is asserted. 
-// The READ_DURING_WRITE parameter determine what happens if a read and a write 
-// are performed to the same address in the same cycle:
-//  "NEW_DATA" this will return the newly written data ("read-after-write").
-//  "DONT_CARE" The results are undefined. This can be used to improve clock 
-//  speed.
-//
+// Reads and writes are performed synchronously. The read value appears 
+// on the next clock edge after the address and readx_en are asserted
+// If readx_en is not asserted, the value of readx_data is undefined during
+// the next cycle. The READ_DURING_WRITE parameter determines what happens 
+// if a read and a write are performed to the same address in the same cycle:
+//  - "NEW_DATA" this will return the newly written data ("read-after-write").
+//  - "DONT_CARE" The results are undefined. This can be used to improve clock 
+//    speed.
 // This does not clear memory contents on reset.
 //
 
@@ -142,6 +142,8 @@ module sram_2r1w
 		end
 		else if (read1_en)
 			read1_data <= data[read1_addr];
+		else
+			read1_data <= {DATA_WIDTH{1'bx}};
 
 		if (write_addr == read2_addr && write_en && read2_en)
 		begin
@@ -152,6 +154,8 @@ module sram_2r1w
 		end
 		else if (read2_en)
 			read2_data <= data[read2_addr];
+		else
+			read2_data <= {DATA_WIDTH{1'bx}};
 	end
 
 	initial

commit a9926d417d462c47f068f5dd6b40f6a43328f5ba

hardware: Fix bug introduced by c2b464788c1fae054fbafe3dbc91fc5c96ed4d1e
Need to set valid if TLB is read for any reason, not just if a lookup
is requested.

hardware/core/tlb.sv
@@ -105,7 +105,7 @@ module tlb
 				begin
 					if (way_update_oh[way_idx] && tlb_read_en && update_set_idx == request_set_idx)
 						way_valid <= update_valid;  // Bypass
-					else if (lookup_en)
+					else if (tlb_read_en)
 						way_valid <= entry_valid[request_set_idx];
 					else
 						way_valid <= 0;