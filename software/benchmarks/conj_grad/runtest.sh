101d7969ddaed712c5337ead8f49abf2b60adc0d
Jeff Bush <jeffbush001@gmail.com>
2015-12-31 07:37:21 -0800

    fix build 2: forgot to update a few other makefiles

--- software/benchmarks/conj_grad/runtest.sh
+++ software/benchmarks/conj_grad/runtest.sh
@@ -1,18 +1,18 @@
-# 
+#
 # Copyright 2011-2015 Jeff Bush
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 
 
 BINDIR=../../../bin
@@ -29,7 +29,7 @@ function compileAndRun {
 	# Build
 	gcc time.c -o WORK/time
 	WORK/time		# time(0) is stored in the return value
-	$CC -DSEED=$? -O3 -o $ELFFILE $1 ../../libs/libc/crt0.o ../../libs/libc/libc.a ../../libs/libos/libos.a -I../../../software/libs/libc/include
+	$CC -DSEED=$? -O3 -o $ELFFILE $1 ../../libs/libc/crt0.o ../../libs/libc/libc.a ../../libs/libos/libos.a ../../libs/compiler-rt/compiler-rt.a -I../../../software/libs/libc/include
 	$ELF2HEX -o $HEXFILE $ELFFILE
 
 	# Run, collect results
