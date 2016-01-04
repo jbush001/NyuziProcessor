38246deec300ca794575ec0b46ed803503394a27
Jeff Bush <jeffbush001@gmail.com>
2016-01-03 16:59:27 -0800

    Refactor VGA microcode compiler
    [ci skip]

--- software/libs/libos/vga.c
+++ software/libs/libos/vga.c
@@ -17,22 +17,64 @@
 #include "registers.h"
 #include "vga.h"
 
-#define nelems(x) (sizeof(x) / sizeof(x[0]))
-#define IN_VIS 1
-#define NEW_FRAME 2
-#define HSYNC 4
-#define VSYNC 8
-#define CTL_LOOP (1 << 18)
+// Microinstruction format:
+//    op (1)  0: load counter 1: loop
+//    counter value or branch dest (13)
+//    vsync (1)
+//    hsync (1)
+//    new_frame (1)
+//    in_visible (1)
 
-// Initiaze counter with value
-#define INITCNT(counter, value) (((counter) << 17) | ((value) << 4))
+#define OP_LOAD 0
+#define OP_LOOP 1
+#define HCOUNT 0
+#define VCOUNT 1
+#define F_VISIBLE 1
+#define F_NEW_FRAME 2
+#define F_HSYNC 4
+#define F_VSYNC 8
 
-// Decrement counter and jump to target PC if it is not yet zero
-#define LOOP(counter, target) (CTL_LOOP | ((counter) << 17) | ((target) << 4))
+static int gVLoopTop;
+static int gCurrentPc;
+static int gSyncFlags;
+
+static void emitOp(int opcode, int counterIndex, int value)
+{
+    REGISTERS[REG_VGA_MICROCODE] = (opcode << 18) | (counterIndex << 17)
+                                   | (value << 4) | gSyncFlags;
+    gCurrentPc++;
+}
+
+static void emitScanline(int hfp, int hs, int hbp, int hres, int visible)
+{
+    // Horizontal Front porch
+    emitOp(OP_LOAD, HCOUNT, hfp - 1);
+    emitOp(OP_LOOP, HCOUNT, gCurrentPc);
+
+    // Horizontal Sync pulse
+    gSyncFlags ^= F_HSYNC;
+    emitOp(OP_LOAD, HCOUNT, hs - 1);
+    emitOp(OP_LOOP, HCOUNT, gCurrentPc);
+    gSyncFlags ^= F_HSYNC;
+
+    // Horizontal Back porch
+    emitOp(OP_LOAD, HCOUNT, hbp - 1);
+    emitOp(OP_LOOP, HCOUNT, gCurrentPc);
+
+    // Scanline
+    if (visible)
+        gSyncFlags ^= F_VISIBLE;
+
+    emitOp(OP_LOAD, HCOUNT, hres - 1);
+    emitOp(OP_LOOP, HCOUNT, gCurrentPc);
+    if (visible)
+        gSyncFlags ^= F_VISIBLE;
+}
 
 //
-// Load microcode into microsequencer to generate synchronization signals
-// for a given resolution
+// Compile microcode to generate synchronization signals for resolution and
+// load into microsequencer.
+//
 // - hpf: horizontal front porch length (pixel clocks)
 // - hs: horizontal sync pulse length
 // - hbp: horizontal back porch length
@@ -44,71 +86,40 @@
 // - vpol: vertical polarity
 // - vres: vertical resolution (pixels)
 //
-void loadMicrocode(int hfp, int hs, int hbp, int hpol, int hres,
-                   int vfp, int vs, int vbp, int vpol, int vres)
+static void compileMicrocode(int hfp, int hs, int hbp, int hpol, int hres,
+                             int vfp, int vs, int vbp, int vpol, int vres)
 {
-    int hsync = (vpol ? 0 : VSYNC) | (hpol ? HSYNC : 0);
-    int vsync = (vpol ? VSYNC : 0) | (hpol ? 0 : HSYNC);
-    int hsync_vsync = (vpol ? VSYNC : 0) | (hpol ? HSYNC : 0);
-    int nosync = (vpol ? 0 : VSYNC) | (hpol ? 0 : HSYNC);
-    int i;
-    unsigned int UPGM[] =
-    {
-        // Vertical front porch
-        INITCNT(1, vfp) | nosync,
-        INITCNT(0, hfp - 1) | nosync,  // Horizontal Front porch
-        LOOP(0, 2) | nosync,
-        INITCNT(0, hs - 1) | hsync,    // Horizontal Sync pulse
-        LOOP(0, 4) | hsync,
-        INITCNT(0, hbp - 1) | nosync,  // Horizontal Back porch
-        LOOP(0, 6) | nosync,
-        INITCNT(0, hres - 1) | nosync, // Scanline
-        LOOP(0, 8) | nosync,
-        LOOP(1, 1) | nosync,
-
-        // Vertical sync (negative)
-        INITCNT(1, vs) | vsync,
-        INITCNT(0, hfp - 2) | vsync,      // Horizontal Front porch
-        LOOP(0, 12) | vsync,
-        INITCNT(0, hs - 1) | hsync_vsync, // Horizontal Sync pulse
-        LOOP(0, 14) | hsync_vsync,
-        INITCNT(0, hbp - 1) | vsync,      // Horizontal Back porch
-        LOOP(0, 16) | vsync,
-        INITCNT(0, hres - 1) | vsync,     // Scanline
-        LOOP(0, 18) | vsync,
-        LOOP(1, 11) | vsync,
-
-        // Vertical back porch
-        INITCNT(1, vbp) | nosync,
-        INITCNT(0, hfp - 1) | nosync,  // Horizontal Front porch
-        LOOP(0, 22) | nosync,
-        INITCNT(0, hs - 1) | hsync,    // Horizontal Sync pulse
-        LOOP(0, 24) | hsync,
-        INITCNT(0, hbp - 1) | nosync,  // Horizontal Back porch
-        LOOP(0, 26) | nosync,
-        INITCNT(0, hres - 1) | nosync, // Scanline
-        LOOP(0, 28) | nosync,
-        LOOP(1, 21) | nosync,
-
-        // Visible area
-        INITCNT(1, vres) | nosync,
-        INITCNT(0, hfp - 1) | nosync,   // Horizontal Front porch
-        LOOP(0, 32) | nosync,
-        INITCNT(0, hs - 1) | hsync,     // Horizontal Sync pulse
-        LOOP(0, 34) | hsync,
-        INITCNT(0, hbp - 1) | nosync,   // Horizontal Back porch
-        LOOP(0, 36) | nosync,
-        INITCNT(0, hres - 1) | nosync | IN_VIS,  // Visible area
-        LOOP(0, 38) | nosync | IN_VIS,
-        LOOP(1, 31) | nosync,
-        NEW_FRAME | nosync
-    };
-    int length = nelems(UPGM);
+    gSyncFlags = (vpol ? 0 : F_VSYNC) | (hpol ? 0 : F_HSYNC);
 
     // Must disable sequencer to load new program into it
     REGISTERS[REG_VGA_ENABLE] = 0;
-    for (i = 0; i < length; i++)
-        REGISTERS[REG_VGA_MICROCODE] = UPGM[i];
+
+    // Vertical front porch
+    emitOp(OP_LOAD, VCOUNT, vfp);
+    gVLoopTop = gCurrentPc;
+    emitScanline(hfp, hs, hbp, hres, 0);
+    emitOp(OP_LOOP, VCOUNT, gVLoopTop);
+
+    // Vertical sync
+    gSyncFlags ^= F_VSYNC;
+    emitOp(OP_LOAD, VCOUNT, vs);
+    gVLoopTop = gCurrentPc;
+    emitScanline(hfp, hs, hbp, hres, 0);
+    emitOp(OP_LOOP, VCOUNT, gVLoopTop);
+    gSyncFlags ^= F_VSYNC;
+
+    // Vertical back porch
+    emitOp(OP_LOAD, VCOUNT, vbp);
+    gVLoopTop = gCurrentPc;
+    emitScanline(hfp, hs, hbp, hres, 0);
+    emitOp(OP_LOOP, VCOUNT, gVLoopTop);
+
+    // Visible area
+    emitOp(OP_LOAD, VCOUNT, vres);
+    gVLoopTop = gCurrentPc;
+    emitScanline(hfp, hs, hbp, hres, 1);
+    emitOp(OP_LOOP, VCOUNT, gVLoopTop);
+    REGISTERS[REG_VGA_MICROCODE] = gSyncFlags | F_NEW_FRAME;
 
     REGISTERS[REG_VGA_BASE] = 0x200000;
     REGISTERS[REG_VGA_LENGTH] = hres * vres;
@@ -120,11 +131,11 @@ int initVGA(VGAMode mode)
     switch (mode)
     {
         case VGA_MODE_640x480:
-            loadMicrocode(16, 96, 48, 0, 640, 10, 2, 33, 0, 480);
+            compileMicrocode(16, 96, 48, 0, 640, 10, 2, 33, 0, 480);
             break;
 
         case VGA_MODE_640x400:
-            loadMicrocode(16, 96, 48, 0, 640, 12, 2, 35, 1, 400);
+            compileMicrocode(16, 96, 48, 0, 640, 12, 2, 35, 1, 400);
             break;
 
         default:
