288f422580262db82c6775745d5aae54dbbfb41b
Jeff Bush <jeffbush001@gmail.com>
2016-03-19 11:05:13 -0700

    hardware: minor cleanup in sim_sdram

--- hardware/testbench/sim_sdram.sv
+++ hardware/testbench/sim_sdram.sv
@@ -32,8 +32,8 @@ module sim_sdram
     input                       dram_cs_n,
     input                       dram_ras_n,
     input                       dram_cas_n,
-    input                       dram_we_n,        // Write enable
-    input[1:0]                  dram_ba,         // Bank select
+    input                       dram_we_n,      // Write enable
+    input[1:0]                  dram_ba,        // Bank select
     input[12:0]                 dram_addr,
     inout[DATA_WIDTH - 1:0]     dram_dq);
 
@@ -63,13 +63,13 @@ module sim_sdram
     logic[$clog2(MEM_SIZE) - 1:0] burst_address;
     logic[DATA_WIDTH - 1:0] output_reg;
     logic[2:0] cas_delay;
-    logic command_enable;
-    logic req_load_mode;
-    logic req_auto_refresh;
-    logic req_precharge;
-    logic req_activate;
-    logic req_write_burst;
-    logic req_read_burst;
+    logic cmd_en;
+    logic cmd_load_mode;
+    logic cmd_auto_refresh;
+    logic cmd_precharge;
+    logic cmd_activate;
+    logic cmd_write_burst;
+    logic cmd_read_burst;
 
     initial
     begin
@@ -83,20 +83,20 @@ module sim_sdram
         cke_ff <= dram_cke;
 
     // Decode command
-    assign command_enable = cke_ff & ~dram_cs_n;
-    assign req_load_mode = command_enable & ~dram_ras_n & ~dram_cas_n & ~dram_we_n;
-    assign req_auto_refresh = command_enable & ~dram_ras_n & ~dram_cas_n & dram_we_n;
-    assign req_precharge = command_enable & ~dram_ras_n & dram_cas_n & ~dram_we_n;
-    assign req_activate = command_enable & ~dram_ras_n & dram_cas_n & dram_we_n;
-    assign req_write_burst = command_enable & dram_ras_n & ~dram_cas_n & ~dram_we_n;
-    assign req_read_burst = command_enable & dram_ras_n & ~dram_cas_n & dram_we_n;
+    assign cmd_en = cke_ff && !dram_cs_n;
+    assign cmd_load_mode = cmd_en && !dram_ras_n && !dram_cas_n && !dram_we_n;
+    assign cmd_auto_refresh = cmd_en && !dram_ras_n && !dram_cas_n && dram_we_n;
+    assign cmd_precharge = cmd_en && !dram_ras_n && dram_cas_n && !dram_we_n;
+    assign cmd_activate = cmd_en && !dram_ras_n && dram_cas_n && dram_we_n;
+    assign cmd_write_burst = cmd_en && dram_ras_n && !dram_cas_n && !dram_we_n;
+    assign cmd_read_burst = cmd_en && dram_ras_n && !dram_cas_n && dram_we_n;
 
     // Burst count
     always_ff @(posedge dram_clk)
     begin
-        if (req_write_burst)
+        if (cmd_write_burst)
             burst_count_ff <= 1;    // Count the first transfer, which has already occurred
-        else if (req_read_burst)
+        else if (cmd_read_burst)
             burst_count_ff <= 0;
         else if (burst_active && cke_ff && (burst_w || burst_read_delay_count === 0))
             burst_count_ff <= burst_count_ff + 1;
@@ -105,7 +105,7 @@ module sim_sdram
     // Bank active
     always_ff @(posedge dram_clk)
     begin
-        if (req_precharge)
+        if (cmd_precharge)
         begin
             if (dram_addr[10])
             begin
@@ -124,7 +124,7 @@ module sim_sdram
 
             initialized <= 1;
         end
-        else if (req_activate)
+        else if (cmd_activate)
         begin
             // Check for attempt to activate bank that is already active
             assert(!bank_active[dram_ba]);
@@ -143,7 +143,7 @@ module sim_sdram
     // Mode register
     always_ff @(posedge dram_clk)
     begin
-        if (req_load_mode)
+        if (cmd_load_mode)
         begin
 `ifdef SDRAM_DEBUG
             $display("latching mode %x", dram_addr[9:0]);
@@ -155,7 +155,7 @@ module sim_sdram
     // Burst read delay count
     always_ff @(posedge dram_clk)
     begin
-        if (req_read_burst)
+        if (cmd_read_burst)
             burst_read_delay_count <= cas_delay - 1; // Note: there is one extra cycle of latency in read
         else if (burst_active && cke_ff && ~burst_w)
         begin
@@ -167,7 +167,7 @@ module sim_sdram
     // Burst active
     always_ff @(posedge dram_clk)
     begin
-        if (req_write_burst || req_read_burst)
+        if (cmd_write_burst || cmd_read_burst)
             burst_active <= 1'b1;
         else if (burst_count_ff >= burst_length - 1 && burst_active)
             burst_active <= 1'b0; // Burst is complete
@@ -175,7 +175,7 @@ module sim_sdram
 
     always_ff @(posedge dram_clk)
     begin
-        if (req_write_burst || req_read_burst)
+        if (cmd_write_burst || cmd_read_burst)
         begin
             // Bank must be active to start burst
             assert(bank_active[dram_ba]);
@@ -185,15 +185,15 @@ module sim_sdram
 
 `ifdef SDRAM_DEBUG
             $display("start %s transfer bank %d row %d column %d",
-                req_write_burst ? "write" : "read", dram_ba,
+                cmd_write_burst ? "write" : "read", dram_ba,
                 bank_active_row[dram_ba], dram_addr[COL_ADDR_WIDTH - 1:0]);
 `endif
-            burst_w <= req_write_burst;
+            burst_w <= cmd_write_burst;
             burst_bank <= dram_ba;
             burst_auto_precharge <= dram_addr[10];
             burst_column_address <= $size(burst_column_address)'(dram_addr[COL_ADDR_WIDTH - 1:0]);
         end
-        else if (req_auto_refresh)
+        else if (cmd_auto_refresh)
         begin
             // Do not auto refresh with open rows
             assert(bank_active == 0);
@@ -208,7 +208,7 @@ module sim_sdram
     begin
         // Fail if not refreshed
         assert(refresh_delay < MAX_REFRESH_INTERVAL);
-        if (req_auto_refresh)
+        if (cmd_auto_refresh)
             refresh_delay <= 0;
         else if (initialized)
             refresh_delay <= refresh_delay + 1;
@@ -220,13 +220,13 @@ module sim_sdram
     begin
         if (burst_active && cke_ff && burst_w)
             memory[burst_address] <= dram_dq;    // Write
-        else if (req_write_burst)
+        else if (cmd_write_burst)
             memory[{bank_active_row[dram_ba], dram_ba, dram_addr[COL_ADDR_WIDTH - 1:0]}] <= dram_dq;    // Latch first word
 
 `ifndef VERILATOR
         // Check if data is still high-z. This doesn't work on verilator, because
         // it doesn't support Z or X.
-        if ((burst_active && cke_ff && burst_w) || req_write_burst)
+        if ((burst_active && cke_ff && burst_w) || cmd_write_burst)
         begin
             if ((dram_dq ^ dram_dq) !== 0)
             begin
@@ -238,26 +238,26 @@ module sim_sdram
 `endif
 
 `ifdef SDRAM_DEBUG
-    if ((burst_active && cke_ff && burst_w) || req_write_burst)
+    if ((burst_active && cke_ff && burst_w) || cmd_write_burst)
         $display(" write %08x", dram_dq);
-    else if (burst_active && !burst_w && !req_write_burst)
+    else if (burst_active && !burst_w && !cmd_write_burst)
         $display(" read %08x", dram_dq);
 `endif
     end
 
     // RAM read
     assign output_reg = memory[burst_address];
-    assign dram_dq = (burst_w || req_write_burst) ? {DATA_WIDTH{1'hZ}} : output_reg;
+    assign dram_dq = (burst_w || cmd_write_burst) ? {DATA_WIDTH{1'hZ}} : output_reg;
 
     // Make sure client is respecting CAS latency.
     always_ff @(posedge dram_clk)
     begin
-        if (req_activate)
+        if (cmd_activate)
             bank_cas_delay[dram_ba] <= cas_delay - 2;
 
         for (int bank = 0; bank < NUM_BANKS; bank++)
         begin
-            if (bank_cas_delay[bank] > 0 && (dram_ba != $clog2(NUM_BANKS)'(bank) || ~req_activate))
+            if (bank_cas_delay[bank] > 0 && (dram_ba != $clog2(NUM_BANKS)'(bank) || ~cmd_activate))
                 bank_cas_delay[bank] <= bank_cas_delay[bank] - 1;
         end
     end
