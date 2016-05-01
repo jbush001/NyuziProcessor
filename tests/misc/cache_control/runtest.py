98f41a9ac58cd9659155bce7c1adfb43b5f60bd2
Jeff Bush <jeffbush001@gmail.com>
2016-04-30 18:48:31 -0700

    Fix dflush test
    - Translate from C to assembly
    - If the output file was truncated, this would flag false positive. Add
    more stringent checking.

--- tests/misc/cache_control/runtest.py
+++ tests/misc/cache_control/runtest.py
@@ -30,27 +30,24 @@ BASE_ADDRESS = 0x400000
 
 
 def dflush_test(name):
-    compile_test('dflush.c')
+    assemble_test('dflush.S')
     run_program(
         environment='verilator',
         dump_file='obj/vmem.bin',
         dump_base=BASE_ADDRESS,
         dump_length=0x40000)
     with open('obj/vmem.bin', 'rb') as f:
-        index = 0
-        while True:
+        for index in range(4096):
             val = f.read(4)
-            if len(val):
-                break
+            if len(val) < 4:
+                raise TestException('output file is truncated')
 
             numVal = ord(val[0]) | (ord(val[1]) << 8) | (
                 ord(val[2]) << 16) | (ord(val[3]) << 24)
             expected = 0x1f0e6231 + (index // 16)
             if numVal != expected:
-                raise TestException('FAIL: mismatch at' + hex(
-                    BASE_ADDRESS + (index * 4)) + 'want' + str(expected) + 'got' + str(numVal))
-
-            index += 1
+                raise TestException('FAIL: mismatch at ' + hex(
+                    BASE_ADDRESS + (index * 4)) + ' want ' + str(expected) + ' got ' + str(numVal))
 
 
 def dinvalidate_test(name):
