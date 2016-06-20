jbush001/NyuziProcessor

commit 81f9cfd15b157110d31a73b97351f45afe05171e

Add ability to run more complex programs as user space programs under the kernel
- Libos is now split into two variants: a bare metal version that runs standalone
and a kernel version that runs as a user space program and uses system calls
- The init_vga call has been modified to return a pointer to the base address
of the framebuffer region.
- The plasma and mandelbrot programs have been modified to run as kernel programs
using the krun target (plasma currently crashes in this configuration, which is
probably a bug somewhere)

software/kernel/vm_address_space.c
@@ -111,6 +111,7 @@ struct vm_area *create_area(struct vm_address_space *space, unsigned int address
 
     area->cache = cache;
     area->cache_offset = cache_offset;
+    area->cache_length = size;
     if (flags & AREA_WIRED)
     {
         for (fault_addr = area->low_address; fault_addr < area->high_address;
@@ -246,6 +247,7 @@ static int soft_fault(struct vm_address_space *space, const struct vm_area *area
     struct vm_cache *cache;
     int old_flags;
     int is_cow_page = 0;
+    int size_to_read;
 
     VM_DEBUG("soft fault va %08x %s\n", address, is_store ? "store" : "load");
 
@@ -266,7 +268,7 @@ static int soft_fault(struct vm_address_space *space, const struct vm_area *area
         if (source_page)
             break;
 
-        if (cache->file)
+        if (cache->file && cache_offset < area->cache_length)
         {
             VM_DEBUG("reading page from file\n");
 
@@ -280,8 +282,13 @@ static int soft_fault(struct vm_address_space *space, const struct vm_area *area
             unlock_vm_cache();
             restore_interrupts(old_flags);
 
+            if (area->cache_length - cache_offset < PAGE_SIZE)
+                size_to_read = area->cache_length - cache_offset;
+            else
+                size_to_read = PAGE_SIZE;
+
             got = read_file(cache->file, cache_offset,
-                            (void*) PA_TO_VA(page_to_pa(source_page)), PAGE_SIZE);
+                            (void*) PA_TO_VA(page_to_pa(source_page)), size_to_read);
             if (got < 0)
             {
                 kprintf("failed to read from file\n");
@@ -294,6 +301,13 @@ static int soft_fault(struct vm_address_space *space, const struct vm_area *area
 
                 unlock_vm_cache();
                 return 0;
+            }
+
+            // For BSS, clear out data past the end of the file
+            if (size_to_read < PAGE_SIZE)
+            {
+                memset((char*) PA_TO_VA(page_to_pa(source_page)) + size_to_read, 0,
+                       PAGE_SIZE - size_to_read);
             }
 
             disable_interrupts();

commit 35e7419829ed659c6b75f50ce86e46316346dbc2

kernel: fix regression
Change 81f9cfd15b157110d31a7 introduced a problem because it was comparing
the cache_length as an absolute file offset instead of area relative. This
broke the 'initdata' test.

software/kernel/vm_address_space.c
@@ -270,7 +270,7 @@ static int soft_fault(struct vm_address_space *space, const struct vm_area *area
         if (source_page)
             break;
 
-        if (cache->file && cache_offset < area->cache_length)
+        if (cache->file && address - area->low_address < area->cache_length)
         {
             VM_DEBUG("reading page from file\n");
 