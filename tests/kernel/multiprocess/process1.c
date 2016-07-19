6afa3799d87e5f9c3c13eb38fabaf4903119e340
Jeff Bush <jeffbush001@gmail.com>
2016-07-19 07:26:28 -0700

    tests: create stub kernel test with multiple processes
    Currently crashes when run in Verilog simulation
    
    [ci skip]

---
+++ tests/kernel/multiprocess/process1.c
@@ -0,0 +1,37 @@
+//
+// Copyright 2016 Jeff Bush
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+extern int __syscall(int n, int arg0, int arg1, int arg2, int arg3, int arg4);
+
+void exec(const char *path)
+{
+    __syscall(3, (int) path, 0, 0, 0, 0);
+}
+
+void printstr(const char *str, int length)
+{
+    __syscall(0, (int) str, length, 0, 0, 0);
+}
+
+int main()
+{
+    exec("program2.elf");
+
+    // XXX do work. Need to touch more heap data and make function calls so
+    // the stack is accessed.
+    while (1)
+        printstr("A", 1);
+}
---
+++ tests/kernel/multiprocess/process2.mk
@@ -0,0 +1,32 @@
+#
+# Copyright 2016 Jeff Bush
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TOPDIR=../../..
+
+include $(TOPDIR)/build/target.mk
+
+SRCS=process2.c start.s
+
+OBJS=$(SRCS_TO_OBJS)
+DEPS=$(SRCS_TO_DEPS)
+
+program2.elf: $(OBJS)
+	$(LD) -o program2.elf -Ttext=0x1000 $(OBJS)
+
+clean:
+	rm -f program2.elf
+
+-include $(DEPS)
