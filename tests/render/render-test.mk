fddc098984672dc860f1c2d1be08952431e46faa
Jeff Bush <jeffbush001@gmail.com>
2016-11-16 22:17:46 -0800

    minor comment updates

--- tests/render/render-test.mk
+++ tests/render/render-test.mk
@@ -38,25 +38,27 @@ program.lst: $(OBJ_DIR)/program.elf
 clean:
 	rm -rf $(OBJ_DIR)
 
-# Run in emulator
+# Run in emulator. Dump rendered framebuffer to a file 'output.png'.
 run: $(OBJ_DIR)/program.hex
 	@rm -f $(OBJ_DIR)/output.bin output.png
 	$(EMULATOR) -d $(OBJ_DIR)/output.bin,0x200000,0x12C000 $(OBJ_DIR)/program.hex
 	@convert -depth 8 -size 640x480 rgba:$(OBJ_DIR)/output.bin output.png
 
-# Run in verilator
+# Run in verilator. Dump rendered framebuffer to a file 'output.png'.
 verirun: $(OBJ_DIR)/program.hex
 	@rm -f $(OBJ_DIR)/output.bin output.png
 	$(VERILATOR) +memdumpfile=$(OBJ_DIR)/output.bin +memdumpbase=200000 +memdumplen=12C000 +bin=$(OBJ_DIR)/program.hex
 	@convert -depth 8 -size 640x480 rgba:$(OBJ_DIR)/output.bin output.png
 
-# Test (emulator only)
+# Test (emulator only). Run program and compare checksum of framebuffer to
+# value in Makefile. If they do not match, return an error.
 test: $(OBJ_DIR)/program.hex
 	@rm -f $(OBJ_DIR)/output.bin output.png
 	$(EMULATOR) -d $(OBJ_DIR)/output.bin,0x200000,0x12C000 $(OBJ_DIR)/program.hex
 	@shasum $(OBJ_DIR)/output.bin | awk '{if ($$1!=$(IMAGE_CHECKSUM)) {print "FAIL: bad checksum, expected " $(IMAGE_CHECKSUM) " got " $$1; exit 1}}'
 	@echo "PASS"
 
+# Compile and send to FPGA board over serial port.
 fpgarun: $(OBJ_DIR)/program.hex
 	$(SERIAL_BOOT) $(SERIAL_PORT) $(OBJ_DIR)/program.hex
 
