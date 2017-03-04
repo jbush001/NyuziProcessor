2d6e9a5cb8dd1c4b50f26ba3a7f0d4ff338e26cd
Jeff Bush <jeffbush001@gmail.com>
2017-03-03 20:34:21 -0800

    tests: make vector compare test more comprehensive

--- tests/compiler/vector_compare.cpp
+++ tests/compiler/vector_compare.cpp
@@ -18,41 +18,94 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// This test converts the result of a vector comparison to another vector
-// (the native format is a bitmask, the compiler synthesizes the conversion)
+// Test various forms of vector comparisons
 
-const veci16_t kVecA = { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4 };
-const veci16_t kVecB = { 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4 };
+const veci16_t kVecA = { -1, 0, 1, -1, 0, 1, -1, 0, 1, 0, 1, 2, 3, 4, 5, 6 };
+const veci16_t kVecB = {  -1, -1, -1, 0, 0, 0, 1, 1, 1, 6, 5, 4, 3, 2, 1, 0 };
+const vecf16_t kVecC = { -1, 0, 1, -1, 0, 1, -1, 0, 1, 0, 1, 2, 3, 4, 5, 6 };
+const vecf16_t kVecD = {  -1, -1, -1, 0, 0, 0, 1, 1, 1, 6, 5, 4, 3, 2, 1, 0 };
 
 void __attribute__ ((noinline)) printVector(veci16_t v)
 {
-	for (int lane = 0; lane < 16; lane++)
-		printf("%d ", v[lane]);
+    for (int lane = 0; lane < 16; lane++)
+        printf("%d ", v[lane]);
+
+    printf("\n");
+}
+
+void __attribute__ ((noinline)) compareVectors(veci16_t a, veci16_t b)
+{
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_ugt(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_uge(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_ult(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_ule(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_sgt(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_sge(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_slt(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_sle(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_eq(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpi_ne(a, b));
+}
+
+void __attribute__ ((noinline)) compareVectors(vecf16_t a, vecf16_t b)
+{
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_gt(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_ge(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_lt(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_le(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_eq(a, b));
+    printf("%04x\n", __builtin_nyuzi_mask_cmpf_ne(a, b));
 }
 
 int main()
 {
-	printVector(kVecA > kVecB);
-  // CHECK: 0 0 0 0
-  // CHECK: -1 0 0 0
-  // CHECK: -1 -1 0 0
-  // CHECK: -1 -1 -1 0
+    // The first four tests validate convertint the result of a vector
+    // comparison to another vector (the native format is a bitmask,
+    // the compiler synthesizes the conversion)
+    // XXX this doesn't test unsigned...
 
-	printVector(kVecA >= kVecB);
-  // CHECK: -1 0 0 0
-  // CHECK: -1 -1 0 0
-  // CHECK: -1 -1 -1 0
-  // CHECK: -1 -1 -1 -1
+    printVector(kVecA > kVecB);
+    // CHECK: 0 -1 -1
+    // CHECK: 0 0 -1
+    // CHECK: 0 0 0
+    // CHECK: 0 0 0 0 -1 -1 -1
 
-	printVector(kVecA < kVecB);
-  // CHECK: 0 -1 -1 -1
-  // CHECK: 0 0 -1 -1
-  // CHECK: 0 0 0 -1
-  // CHECK: 0 0 0 0
+    printVector(kVecA >= kVecB);
+    // CHECK: -1 -1 -1
+    // CHECK: 0 -1 -1
+    // CHECK: 0 0 -1
+    // CHECK: 0 0 0 -1 -1 -1 -1
 
-	printVector(kVecA <= kVecB);
-  // CHECK: -1 -1 -1 -1
-  // CHECK: 0 -1 -1 -1
-  // CHECK: 0 0 -1 -1
-  // CHECK: 0 0 0 -1
+    printVector(kVecA < kVecB);
+    // CHECK: 0 0 0
+    // CHECK: -1 0 0
+    // CHECK: -1 -1 0
+    // CHECK: -1 -1 -1 0 0 0 0
+
+    printVector(kVecA <= kVecB);
+    // CHECK: -1 0 0
+    // CHECK: -1 -1 0
+    // CHECK: -1 -1 -1
+    // CHECK: -1 -1 -1 -1 0 0 0
+
+    // Test all comparison builtins
+    compareVectors(kVecA, kVecB);
+    // CHECK: 1607
+    // CHECK: 9e8f
+    // CHECK: 6170
+    // CHECK: e9f8
+    // CHECK: 6407
+    // CHECK: ec8f
+    // CHECK: 1370
+    // CHECK: 9bf8
+    // CHECK: 8888
+    // CHECK: 7777
+
+    compareVectors(kVecC, kVecD);
+    // CHECK: 6407
+    // CHECK: ec8f
+    // CHECK: 1370
+    // CHECK: 9bf8
+    // CHECK: 8888
+    // CHECK: 7777
 }
