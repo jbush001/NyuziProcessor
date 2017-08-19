fbe25c7d429cb818eff869edd5bf0280d0e078bc
Jeff Bush <jeffbush001@gmail.com>
2017-08-19 08:43:49 -0700

    Fix a few lint warnings in printf tests

--- tests/libc/printf.c
+++ tests/libc/printf.c
@@ -4,19 +4,21 @@ int main()
 {
     printf("<%d>\n", 12345678); // CHECK: <12345678>
     printf("<%d>\n", -2);   // CHECK: <-2>
-    printf("<%u>\n", 452345), // CHECK: <452345>
-    printf("<%u>\n", -2);  // CHECK: <4294967294>
-    printf("<%Ld>\n", 37036629630LL); // CHECK: <37036629630>
-    printf("<%Ld>\n", -37036629630LL); // CHECK: <-37036629630>
-    printf("<%Lu>\n", 37036629630LL); // CHECK: <37036629630>
-    printf("<%Lu>\n", -37036629630LL); // CHECK: <18446744036672921986>
+    printf("<%u>\n", 452345u), // CHECK: <452345>
+    printf("<%u>\n", 4294967294u);  // CHECK: <4294967294>
+    printf("<%Ld>\n", 37036629630ll); // CHECK: <37036629630>
+    printf("<%Ld>\n", -37036629630ll); // CHECK: <-37036629630>
+    printf("<%Lu>\n", 37036629630ull); // CHECK: <37036629630>
+    printf("<%Lu>\n", 18446744036672921986ull); // CHECK: <18446744036672921986>
     printf("<%x>\n", 0x762); // CHECK: <762>
+    printf("<%Lx>\n", 0xf947483738473843ull); // CHECK: <f947483738473843>
+    printf("<%016Lx>\n", 0x123ull); // CHECK: <0000000000000123>
     printf("<%08d>\n", 1234); // CHECK: <00001234>
     printf("<%09x>\n", 0x847); // CHECK: <000000847>
     printf("<%8d>\n", 1234); // CHECK: <    1234>
     printf("<%9x>\n", 0x789); // CHECK: <      789>
     printf("<%s>\n", "foo");  // CHECK: <foo>
-    printf("<%.4s>\n", "poiuytre"); // CHECK: <poiu>
+    printf("<%.4s>\n", "abcdefgh"); // CHECK: <abcd>
     // XXX padding not supported
 
     printf("<%g>\n", 1.5); // CHECK: <1.5>
