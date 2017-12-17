eb6cfc96f97438c58e50d3474393f66c9d365527
Jeff Bush <jeffbush001@gmail.com>
2017-12-17 08:57:08 -0800

    Fix synthesis error
    Quartus apparently doesn't support unique0
    
    [ci skip]

--- hardware/core/nyuzi.sv
+++ hardware/core/nyuzi.sv
@@ -81,13 +81,16 @@ module nyuzi
         begin
             if (io_bus.write_en)
             begin
-                unique0 case (io_bus.address)
+                unique case (io_bus.address)
                     // Thread enable flag handling. This is limited to 32 threads.
                     'h100: // resume thread
                         thread_en <= thread_en | io_bus.write_data[TOTAL_THREADS - 1:0];
 
                     'h104: // halt thread
                         thread_en <= thread_en & ~io_bus.write_data[TOTAL_THREADS - 1:0];
+  
+                    default:
+                        ;
                 endcase
             end
         end
