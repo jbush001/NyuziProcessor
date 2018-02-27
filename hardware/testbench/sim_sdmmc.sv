jbush001/NyuziProcessor

commit 314ae29dc7b740783e53faa9fd94f3abd3c8ef1e

Add extra error checking to SIM SDMMC
[ci skip]

hardware/testbench/sim_sdmmc.sv
@@ -22,8 +22,8 @@
 
 module sim_sdmmc(
     input            sd_sclk,
-    input            sd_di,
     input            sd_cs_n,
+    input            sd_di,
     output logic     sd_do);
 
     localparam INIT_CLOCKS = 72;
@@ -111,7 +111,9 @@ module sim_sdmmc(
         case (current_state)
             STATE_INIT_WAIT_FOR_CLOCKS:
             begin
-                $display("command sent to SD card before initialized");
+                // Bug in this module: shouldn't process commands until
+                // initialization
+                assert(0);
                 $finish;
             end
 
@@ -133,6 +135,14 @@ module sim_sdmmc(
                     case (command[0] & 8'h3f)
                         CMD_GO_IDLE:
                         begin
+                            // Still in native SD mode, checksum needs to be correct
+                            assert(command[1] == 0);
+                            assert(command[2] == 0);
+                            assert(command[3] == 0);
+                            assert(command[4] == 0);
+                            assert(mosi_byte_nxt == 8'h95);
+
+                            // Reset the card
                             card_ready <= 1;
                             current_state <= STATE_RESULT;
                             command_result <= 1;
@@ -148,7 +158,7 @@ module sim_sdmmc(
                         begin
                             assert(card_ready);
                             current_state <= STATE_READ_CMD_RESPONSE;
-                            state_delay <= $random() & 'hf;    // Simulate random delay
+                            state_delay <= $random() & 'h7;    // Simulate random delay
                             command_result <= 1;
                             miso_byte <= 'hff;    // wait
                             transfer_count <= 0;
@@ -183,7 +193,7 @@ module sim_sdmmc(
                         begin
                             assert(card_ready);
                             current_state <= STATE_WRITE_CMD_RESPONSE;
-                            state_delay <= $random() & 'hf;    // Simulate random delay
+                            state_delay <= $random() & 'h7;    // Simulate random delay
                             command_result <= 1;
                             transfer_address <= {command[1], command[2], command[3], command[4]}
                                 * block_length;
@@ -214,7 +224,7 @@ module sim_sdmmc(
                 begin
                     current_state <= STATE_READ_DATA_TOKEN;
                     miso_byte <= 0; // Ready
-                    state_delay <= $random() & 'hf;    // Simulate random delay
+                    state_delay <= $random() & 'h7;    // Simulate random delay
                 end
                 else
                     state_delay <= state_delay - 1;
@@ -240,7 +250,7 @@ module sim_sdmmc(
                 state_delay <= state_delay - 1;
                 if (state_delay <= 2)
                 begin
-                    // Transmit 16 bit checksum (ignored)
+                    // 16 bit checksum (ignored)
                     miso_byte <= 'hff;
                 end
                 else
@@ -259,7 +269,7 @@ module sim_sdmmc(
                 begin
                     current_state <= STATE_WRITE_DATA_TOKEN;
                     miso_byte <= 0; // Ready
-                    state_delay <= $random() & 'hf;    // Simulate random delay
+                    state_delay <= $random() & 'h7;    // Simulate random delay
                 end
                 else
                 begin
@@ -317,10 +327,21 @@ module sim_sdmmc(
         endcase
     endtask
 
-    always @(posedge sd_sclk) // fix for multiple drivers on shift_count: initial statement not compatible with always_ff according to SystemVerilog standard
+    // This is 'always' instead of 'always_ff' because there is an initial
+    // statement to initialize variables, which is not compatible with
+    // always_ff according to SystemVerilog standard (was causing errors
+    // on some simulators otherwise).
+    always @(posedge sd_sclk)
     begin
-        if (sd_cs_n && current_state == STATE_INIT_WAIT_FOR_CLOCKS)
+        if (current_state == STATE_INIT_WAIT_FOR_CLOCKS)
         begin
+            if (!sd_di || !sd_cs_n)
+            begin
+                $display("Error in SD initialization sequence: DI %d CS %d",
+                    sd_di, sd_cs_n);
+                $finish;
+            end
+
             init_clock_count <= init_clock_count + 1;
             if (init_clock_count >= INIT_CLOCKS)
                 current_state <= STATE_IDLE;

commit fe56e4ea85758f024acd981e8e5cbb1af2046ab3

SD fixes
- I had incorrectly treated command numbers as hex instead of decimal
- Made command identifiers consistent with spec
- Fixed a bug introduced in 314ae29dc7b740783e53faa9fd94f3abd3c8ef1e, where spurious
  asserts could occur on initialization.

hardware/testbench/sim_sdmmc.sv
@@ -45,11 +45,11 @@ module sim_sdmmc(
     } sd_state_t;
 
     // SD commands
-    localparam CMD_GO_IDLE = 8'h00;
-    localparam CMD_SEND_OP_COND = 8'h01;
-    localparam CMD_SET_BLOCKLEN = 8'h16;
-    localparam CMD_READ_SINGLE_BLOCK = 8'h17;
-    localparam CMD_WRITE_SINGLE_BLOCK = 8'h24;
+    localparam CMD_GO_IDLE_STATE = 0;
+    localparam CMD_SEND_OP_COND = 1;
+    localparam CMD_SET_BLOCKLEN = 16;
+    localparam CMD_READ_SINGLE_BLOCK = 17;
+    localparam CMD_WRITE_SINGLE_BLOCK = 24;
 
     logic[1000:0] filename;
     int shift_count;
@@ -133,7 +133,7 @@ module sim_sdmmc(
                 if (command_length == 5)
                 begin
                     case (command[0] & 8'h3f)
-                        CMD_GO_IDLE:
+                        CMD_GO_IDLE_STATE:
                         begin
                             // Still in native SD mode, checksum needs to be correct
                             assert(command[1] == 0);
@@ -335,16 +335,15 @@ module sim_sdmmc(
     begin
         if (current_state == STATE_INIT_WAIT_FOR_CLOCKS)
         begin
-            if (!sd_di || !sd_cs_n)
+            // 6.4.1 "The host shall...start to supply at least 74 SD clocks
+            // to the SD card with keeping CMD [DI] line to high. In case of SPI
+            // mode, CS shall be held to high during 74 clock cycles."
+            if (sd_cs_n && sd_di)
             begin
-                $display("Error in SD initialization sequence: DI %d CS %d",
-                    sd_di, sd_cs_n);
-                $finish;
+                init_clock_count <= init_clock_count + 1;
+                if (init_clock_count >= INIT_CLOCKS)
+                    current_state <= STATE_IDLE;
             end
-
-            init_clock_count <= init_clock_count + 1;
-            if (init_clock_count >= INIT_CLOCKS)
-                current_state <= STATE_IDLE;
         end
         else if (!sd_cs_n)
         begin

software/kernel/sd_card.c
@@ -25,10 +25,10 @@
 
 enum sd_command
 {
-    SD_CMD_RESET = 0,
-    SD_CMD_INIT = 1,
-    SD_CMD_SET_BLOCK_LEN = 0x16,
-    SD_CMD_READ_BLOCK = 0x17
+    CMD_GO_IDLE_STATE = 0,
+    CMD_SEND_OP_COND = 1,
+    CMD_SET_BLOCKLEN = 16,
+    CMD_READ_SINGLE_BLOCK = 17
 };
 
 static spinlock_t sd_lock;
@@ -90,14 +90,14 @@ int init_sd_device()
     set_cs(0);
 
     // Reset the card
-    result = send_sd_command(SD_CMD_RESET, 0);
+    result = send_sd_command(CMD_GO_IDLE_STATE, 0);
     if (result != 1)
         return -1;
 
     // Poll until it is ready
     while (1)
     {
-        result = send_sd_command(SD_CMD_INIT, 0);
+        result = send_sd_command(CMD_SEND_OP_COND, 0);
         if (result == 0)
             break;
 
@@ -106,7 +106,7 @@ int init_sd_device()
     }
 
     // Configure the block size
-    result = send_sd_command(SD_CMD_SET_BLOCK_LEN, BLOCK_SIZE);
+    result = send_sd_command(CMD_SET_BLOCKLEN, BLOCK_SIZE);
     if (result != 0)
         return -1;
 
@@ -124,7 +124,7 @@ int read_sd_device(unsigned int block_address, void *ptr)
 
     old_flags = acquire_spinlock_int(&sd_lock);
 
-    result = send_sd_command(SD_CMD_READ_BLOCK, block_address);
+    result = send_sd_command(CMD_READ_SINGLE_BLOCK, block_address);
     if (result != 0)
         return -1;
 

software/libs/libos/bare-metal/sdmmc.c
@@ -27,11 +27,11 @@
 
 typedef enum
 {
-    SD_CMD_RESET = 0,
-    SD_CMD_INIT = 1,
-    SD_CMD_SET_BLOCK_LEN = 0x16,
-    SD_CMD_READ_SINGLE_BLOCK = 0x17,
-    SD_CMD_WRITE_SINGLE_BLOCK = 0x24
+    CMD_GO_IDLE_STATE = 0,
+    CMD_SEND_OP_COND = 1,
+    CMD_SET_BLOCK_LEN = 16,
+    CMD_READ_SINGLE_BLOCK = 17,
+    CMD_WRITE_SINGLE_BLOCK = 24
 } SDCommand;
 
 static void set_cs(int level)
@@ -91,7 +91,7 @@ int init_sdmmc_device(void)
 
     // Reset the card by sending CMD0 with CS low.
     set_cs(0);
-    result = send_sd_command(SD_CMD_RESET, 0);
+    result = send_sd_command(CMD_GO_IDLE_STATE, 0);
 
     // The card should have returned 01 to indicate it is in SPI mode.
     if (result != 1)
@@ -99,7 +99,7 @@ int init_sdmmc_device(void)
         if (result == 0xff)
             printf("init_sdmmc_device: timed out during reset\n");
         else
-            printf("init_sdmmc_device: SD_CMD_RESET failed: invalid response %02x\n", result);
+            printf("init_sdmmc_device: CMD_GO_IDLE_STATE failed: invalid response %02x\n", result);
 
         return -1;
     }
@@ -108,22 +108,22 @@ int init_sdmmc_device(void)
     // of milliseconds.
     while (1)
     {
-        result = send_sd_command(SD_CMD_INIT, 0);
+        result = send_sd_command(CMD_SEND_OP_COND , 0);
         if (result == 0)
             break;
 
         if (result != 1)
         {
-            printf("init_sdmmc_device: SD_CMD_INIT unexpected response %02x\n", result);
+            printf("init_sdmmc_device: CMD_SEND_OP_COND  unexpected response %02x\n", result);
             return -1;
         }
     }
 
     // Configure the block size
-    result = send_sd_command(SD_CMD_SET_BLOCK_LEN, SDMMC_BLOCK_SIZE);
+    result = send_sd_command(CMD_SET_BLOCK_LEN, SDMMC_BLOCK_SIZE);
     if (result != 0)
     {
-        printf("init_sdmmc_device: SD_CMD_SET_BLOCK_LEN unexpected response %02x\n", result);
+        printf("init_sdmmc_device: CMD_SET_BLOCK_LEN unexpected response %02x\n", result);
         return -1;
     }
 
@@ -138,10 +138,10 @@ int read_sdmmc_device(unsigned int block_address, void *ptr)
     int result;
     int data_timeout;
 
-    result = send_sd_command(SD_CMD_READ_SINGLE_BLOCK, block_address);
+    result = send_sd_command(CMD_READ_SINGLE_BLOCK, block_address);
     if (result != 0)
     {
-        printf("read_sdmmc_device: SD_CMD_READ_SINGLE_BLOCK unexpected response %02x\n", result);
+        printf("read_sdmmc_device: CMD_READ_SINGLE_BLOCK unexpected response %02x\n", result);
         return -1;
     }
 
@@ -170,10 +170,10 @@ int write_sdmmc_device(unsigned int block_address, void *ptr)
 {
     int result;
 
-    result = send_sd_command(SD_CMD_WRITE_SINGLE_BLOCK, block_address);
+    result = send_sd_command(CMD_WRITE_SINGLE_BLOCK, block_address);
     if (result != 0)
     {
-        printf("write_sdmmc_device: SD_CMD_WRITE_SINGLE_BLOCK unexpected response %02x\n", result);
+        printf("write_sdmmc_device: CMD_WRITE_SINGLE_BLOCK unexpected response %02x\n", result);
         return -1;
     }
 

tools/emulator/sdmmc.c
@@ -37,11 +37,11 @@
 // Commands
 enum sd_command
 {
-    CMD_GO_IDLE = 0x00,
-    CMD_SEND_OP_COND = 0x01,
-    CMD_SET_BLOCKLEN = 0x16,
-    CMD_READ_SINGLE_BLOCK = 0x17,
-    CMD_WRITE_SINGLE_BLOCK = 0x24
+    CMD_GO_IDLE_STATE = 0,
+    CMD_SEND_OP_COND = 1,
+    CMD_SET_BLOCKLEN = 16,
+    CMD_READ_SINGLE_BLOCK = 17,
+    CMD_WRITE_SINGLE_BLOCK = 24
 };
 
 enum sd_state
@@ -113,7 +113,7 @@ static void process_command(const uint8_t *command)
 {
     switch (command[0] & 0x3f)
     {
-        case CMD_GO_IDLE:
+        case CMD_GO_IDLE_STATE:
             // If a virtual block device wasn't specified, don't initialize
             if (block_fd > 0)
             {