2bd5dcd50921e3c81690de243ba5d580d179da12
Jeff Bush <jeffbush001@gmail.com>
2019-02-20 18:09:04 -0800

    Update to latest toolchain

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -29,13 +29,13 @@ TOOLCHAIN_DIR=/usr/local/llvm-nyuzi/
 
 rm -rf tmp/*
 mkdir -p tmp
-cp $TOOLCHAIN_DIR/bin/clang-8 tmp/
+cp $TOOLCHAIN_DIR/bin/clang-9 tmp/
 cp $TOOLCHAIN_DIR/bin/elf2hex tmp/
 cp $TOOLCHAIN_DIR/bin/lld tmp/
 cp $TOOLCHAIN_DIR/bin/llvm-ar tmp/
 cp $TOOLCHAIN_DIR/bin/llvm-ranlib tmp/
 cp $TOOLCHAIN_DIR/bin/llvm-objdump tmp/
-cp $TOOLCHAIN_DIR/lib/clang/8.0.0/lib/libclang_rt.builtins-nyuzi.a tmp/
+cp $TOOLCHAIN_DIR/lib/clang/9.0.0/lib/libclang_rt.builtins-nyuzi.a tmp/
 
 cp -R /usr/local/share/verilator tmp/share_verilator
 cp /usr/local/bin/verilator* tmp/
