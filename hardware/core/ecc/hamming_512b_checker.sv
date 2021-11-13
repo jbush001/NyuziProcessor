dcd1886c8ebfc081b2a4ac7fc7540578964be54d
Jooooc <joel.stv.9@gmail.com>
2021-11-13 22:16:33 +0100

    Solving hamming 512b checker bugs + full testbench

--- hardware/core/ecc/hamming_512b_checker.sv
+++ hardware/core/ecc/hamming_512b_checker.sv
@@ -9,16 +9,16 @@ module hamming_512b_checker(
     input hamming_512b_t              coded_word,
     output logic                      error, //1 = we detected one or more errors
     output logic                      corrected, //1 = the errors were corrected
-    output cache_line_data_t          correct_word); //The word after applying the corrections
+    output hamming_512b_t             correct_word_hamming, //The hamming code after applying the corrections
+    output cache_line_data_t          correct_word); //The word without hamming after applying the corrections
 
-    cache_line_data_t useful_data = {coded_word[521:512], coded_word[510:256], coded_word[254:128],
-                                     coded_word[126:64], coded_word[62:32], coded_word[30:16],
-                                     coded_word[14:8], coded_word[6:4], coded_word[2]};
+    cache_line_data_t useful_data;
     cache_line_data_t corrected_data;
+    hamming_512b_t corrected_data_hamming;
     hamming_512b_t useful_data_hamming;
     logic [9:0]syndrome;
-    logic [9:0]syn1;
-    logic [9:0]syn2;
+    logic [9:0]syn1; //From the input word
+    logic [9:0]syn2; //Encoding the useful data from the input word
     logic parity;
     logic [1:0]error_type; //00 = No error, 11 = Single Error Correctable, 
                            //10 = Double error no correctable, 01 = Only the parity bit is wrong                     
@@ -31,6 +31,9 @@ module hamming_512b_checker(
     );
 
     always_comb begin : error_detection
+        useful_data = {coded_word[521:512], coded_word[510:256], coded_word[254:128],
+                       coded_word[126:64], coded_word[62:32], coded_word[30:16],
+                       coded_word[14:8], coded_word[6:4], coded_word[2]};
         syn1 = {coded_word[511], coded_word[255], coded_word[127], coded_word[63], coded_word[31],
                 coded_word[15], coded_word[7], coded_word[3], coded_word[1], coded_word[0]};
         syn2 = {useful_data_hamming[511], useful_data_hamming[255], useful_data_hamming[127],
@@ -46,12 +49,16 @@ module hamming_512b_checker(
     int unsigned pos;
     always_comb begin : error_correction
         pos = syndrome;
-        corrected_data = useful_data;
-        corrected_data[pos] = ~(useful_data[pos]);
+        corrected_data_hamming = coded_word;
+        corrected_data_hamming[pos-1] = ~(coded_word[pos-1]);
+        corrected_data = {corrected_data_hamming[521:512], corrected_data_hamming[510:256], corrected_data_hamming[254:128],
+                          corrected_data_hamming[126:64], corrected_data_hamming[62:32], corrected_data_hamming[30:16],
+                          corrected_data_hamming[14:8], corrected_data_hamming[6:4], corrected_data_hamming[2]};
     end
 
     assign error = |error_type;
     assign corrected = &error_type;
-    assign corrected_word = (error_type == 2'b11) ? aux_data : useful_data;
+    assign correct_word = (error_type == 2'b11) ? corrected_data : useful_data;
+    assign correct_word_hamming = (error_type == 2'b11) ? corrected_data_hamming : useful_data_hamming;
 
 endmodule
--- hardware/testbench/ECC_tests/hamming_512b_checker_tb.sv
+++ hardware/testbench/ECC_tests/hamming_512b_checker_tb.sv
@@ -19,6 +19,7 @@ module hamming_checker_tb();
     end
 
     hamming_512b_t in_word;
+    hamming_512b_t out_word_hamming;
     logic out_error;
     logic out_corrected;
     cache_line_data_t out_word;
@@ -29,6 +30,7 @@ module hamming_checker_tb();
         .coded_word(in_word),
         .error(out_error),
         .corrected(out_corrected),
+        .correct_word_hamming(out_word_hamming),
         .correct_word(out_word)
     );
 
@@ -62,11 +64,11 @@ module hamming_checker_tb();
           $display("Error: %b  Corrected: %b", out_error, out_corrected);
         end
       end
-      //8 -> The 4th bit flipped, should be corrected to 0
-      in_word = 524'h8;
+      //10 -> The 5th bit flipped, should be corrected to 0
+      in_word = 524'h10;
       #20;
       if (out_word != 512'h0) begin
-        $error("Error correcting 8");
+        $error("Error correcting 10");
         $displayh("Value of in_word: %h", in_word);
         $displayh("Value of out_word: %h", out_word);
         $display("Error: %b  Corrected: %b", out_error, out_corrected);
@@ -83,28 +85,89 @@ module hamming_checker_tb();
           $display("Error: %b  Corrected: %b", out_error, out_corrected);
         end
       end
-      //C -> The 3rd and 4th bits flipped. Not correctable, just detectable
-      in_word = 524'hC;
+      //14 -> The 3rd and 5th bits flipped. Not correctable, just detectable
+      in_word = 524'h14;
       #20;
-      if (out_word == 512'h0) begin
+      if (!out_error) begin
+        $error("Error detecting an error in 14");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      if (out_error && out_corrected) begin
+        $error("Error correcting an error in C");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //2 -> The 2nd bit flipped. Only the parity bit is wrong
+      in_word = 524'h2;
+      #20;
+      if (out_word != 512'h0) begin
         $error("Error decoding 0");
         $displayh("Value of in_word: %h", in_word);
         $displayh("Value of out_word: %h", out_word);
         $display("Error: %b  Corrected: %b", out_error, out_corrected);
-        if (!out_error) begin
-          $error("Error in the error signal");
-          $displayh("Value of in_word: %h", in_word);
-          $displayh("Value of out_word: %h", out_word);
-          $display("Error: %b  Corrected: %b", out_error, out_corrected);
-        end
-        if (!out_corrected) begin
-          $error("Error in the corrected signal");
-          $displayh("Value of in_word: %h", in_word);
-          $displayh("Value of out_word: %h", out_word);
-          $display("Error: %b  Corrected: %b", out_error, out_corrected);
-        end
       end
-      in_word = 524'hC;
+      if (!out_error) begin
+        $error("Error detecting an error in 2");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //Correct encoding for 0x9
+      in_word = 524'h4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004C;
       #20;
+      if (out_word != 512'h9) begin
+        $error("Error decoding 9");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //Correct encoding for 0xF
+      in_word = 524'h4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007F;
+      #20;
+      if (out_word != 512'hF) begin
+        $error("Error decoding F");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //Correct encoding for 0x10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+      in_word = 524'h4408000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b;
+      #20;
+      if (out_word != 512'h10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000) begin
+        $error("Error decoding 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //Correct encoding for 0xAAAA
+      in_word = 524'h15AAD9;
+      #20;
+      if (out_word != 512'hAAAA) begin
+        $error("Error decoding AAAA");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //Correct encoding for 0x11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+      in_word = 524'h0442222222222222222222222222222222222222222222222222222222222222222111111111111111111111111111111110888888888888888c444444422221107;
+      #20;
+      if (out_word != 512'h11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111) begin
+        $error("Error decoding 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
+      //The overall parity bit flipped
+      in_word = 524'h40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+      #20;
+      if (!out_error || (out_error && out_corrected)) begin
+        $error("Error detecting an error in overall parity bit");
+        $displayh("Value of in_word: %h", in_word);
+        $displayh("Value of out_word: %h", out_word);
+        $display("Error: %b  Corrected: %b", out_error, out_corrected);
+      end
     end
 endmodule
